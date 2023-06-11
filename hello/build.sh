a42c776e0b4e35ae82d5f7f9e29c438c3c54fcfb
danielnachun <daniel.nachun@gmail.com>
2023-06-10 21:15:54 -0700

    fix recipe

--- hello/build.sh
+++ hello/build.sh
@@ -2,6 +2,10 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+if [[ "${target_platform}" == darwin-* ]]; then
+    LDFLAGS="-liconv"
+fi
+
 ./configure --disable-silent \
     --disable-dependency-tracking \
     --prefix=${PREFIX} 
