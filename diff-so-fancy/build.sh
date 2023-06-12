3c31a2b8c4bd539acc74bf6e9f7a1f830a0cc3fc
danielnachun <daniel.nachun@gmail.com>
2023-06-11 17:13:10 -0700

    add diff-so-fancy

--- diff-so-fancy/build.sh
+++ diff-so-fancy/build.sh
@@ -3,7 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 mkdir -p ${PREFIX}/bin 
-mkdir -p ${PREFIX}/libexec
-install -m 755 diff-so-fancy ${PREFIX}/libexec
-cp -r lib ${PREFIX}/libexec
+mkdir -p ${PREFIX}/libexec/diff-so-fancy
+install -m 755 diff-so-fancy ${PREFIX}/libexec/diff-so-fancy
+cp -r lib ${PREFIX}/libexec/diff-so-fancy
 ln -sf ${PREFIX}/libexec/diff-so-fancy ${PREFIX}/bin/diff-so-fancy
