b400c39ca13e48af7f9a832a188dc0e44109ff7e
danielnachun <daniel.nachun@gmail.com>
2023-06-11 19:55:12 -0700

    add dust

--- dust/build.sh
+++ dust/build.sh
@@ -9,9 +9,12 @@ cargo-bundle-licenses \
 # build statically linked binary with Rust
 cargo install --locked --root ${PREFIX} --path .
 
-install -Dm 644 completions/dust.bash ${PREFIX}/etc/bash_completion.d/dust.bash
-install -Dm 644 completions/dust.fish ${PREFIX}/share/fish/vendor_completions.d/dust.fish
-install -Dm 644 completions/_dust ${PREFIX}/share/zsh/site-functions/_dust
+mkdir -p ${PREFIX}/etc/bash_completion.d
+mkdir -p ${PREFIX}/share/fish/vendor_completions.d
+mkdir -p ${PREFIX}/share/zsh/site-functions
+install -m 644 completions/dust.bash ${PREFIX}/etc/bash_completion.d/dust.bash
+install -m 644 completions/dust.fish ${PREFIX}/share/fish/vendor_completions.d/dust.fish
+install -m 644 completions/_dust ${PREFIX}/share/zsh/site-functions/_dust
 
 # strip debug symbols
 "$STRIP" "$PREFIX/bin/${PKG_NAME}"
