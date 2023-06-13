0997c47ea48169bb9c9aca42d89a4318be7bc4cb
danielnachun <daniel.nachun@gmail.com>
2023-06-13 12:57:22 -0700

    add grep

---
+++ grep/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+./configure --disable-debug \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} \
+    --libdir=${PREFIX}/lib
+
+make
+make install
