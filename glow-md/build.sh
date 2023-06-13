e97354dca4683b928505f0f6065b29bc71367dd6
danielnachun <daniel.nachun@gmail.com>
2023-06-13 00:20:43 -0700

    fix mess

--- glow-md/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-export CGO_ENABLED=0
-export LDFLAGS="-s -w -X main.Version=${PKG_VERSION}"
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/glow -ldflags="${LDFLAGS}"
