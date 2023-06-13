e013079bbe2c445978e60279ac7d2afedbbad163
danielnachun <daniel.nachun@gmail.com>
2023-06-12 23:36:47 -0700

    add git-absorb

--- git-absorb/build.sh
+++ git-absorb/build.sh
@@ -9,11 +9,14 @@ cargo-bundle-licenses \
 # build statically linked binary with Rust
 cargo install --locked --root ${PREFIX} --path .
 
-install -Dd ${PREFIX}/etc/bash_completion.d ${PREFIX}/share/fish/vendor_completions.d ${PREFIX}/share/zsh/site-functions
-install -Dm 644 Documentation/git-absorb.1 ${PREFIX}/share/man/man1/git-absorb.1
+mkdir -p ${PREFIX}/etc/bash_completion.d 
+mkdir -p ${PREFIX}/share/fish/vendor_completions.d 
+mkdir -p ${PREFIX}/share/zsh/site-functions
+mkdir -p ${PREFIX}/share/man/man1
 git-absorb --gen-completions bash > ${PREFIX}/etc/bash_completion.d/git-absorb
 git-absorb --gen-completions fish > ${PREFIX}/share/fish/vendor_completions.d/git-absorb.fish
 git-absorb --gen-completions zsh > ${PREFIX}/share/zsh/site-functions/_git-absorb
+install -m 644 Documentation/git-absorb.1 ${PREFIX}/share/man/man1/git-absorb.1
 
 # strip debug symbols
 "$STRIP" "$PREFIX/bin/${PKG_NAME}"
