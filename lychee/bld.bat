c5e2f60e384352f2b7c43eb59dbbfe6e46fff50c
danielnachun <daniel.nachun@gmail.com>
2023-06-13 21:57:11 -0700

    add lychee

---
+++ lychee/bld.bat
@@ -0,0 +1,14 @@
+:: check licenses
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+:: build
+cargo install --locked --root "%PREFIX%" --path lychee-bin || goto :error
+
+:: remove extra build file
+del /F /Q "%PREFIX%\.crates.toml"
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
