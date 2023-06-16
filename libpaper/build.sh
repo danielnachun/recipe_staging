c07d51a625ef300773b7cf8a2afe48a1acfa3f5d
danielnachun <daniel.nachun@gmail.com>
2023-06-16 13:41:59 -0700

    add libpaper

---
+++ libpaper/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+./configure --disable-silent \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} \
+    --sysconfdir=${PREFIX}/etc \
+make install
+make test
