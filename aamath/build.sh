221497623e1f1cf553d590180ac01978168431e4
danielnachun <daniel.nachun@gmail.com>
2023-06-16 20:43:32 -0700

    add aamath

--- aamath/build.sh
+++ aamath/build.sh
@@ -2,6 +2,8 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+ln -sf ${BUILD_PREFIX}/bin/flex ${BUILD_PREFIX}/bin/lex
+
 make CXX=${CXX}
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/share/man/man1
