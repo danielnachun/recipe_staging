00b965394addd87a9de21f19b72916c6ebb5aab2
danielnachun <daniel.nachun@gmail.com>
2023-06-17 18:36:07 -0700

    add abcde and dependencies

--- smake/build.sh
+++ smake/build.sh
@@ -2,4 +2,4 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-make GMAKE_NOWARN=true INS_BASE=${PREFIX} INS_RBASE=${PREFIX} CFLAGS="${CFLAGS} -Wno-implicit-int" install
+make GMAKE_NOWARN=true INS_BASE=${PREFIX} INS_RBASE=${PREFIX} CFLAGS="${CFLAGS} -Wno-implicit-int" CC="${CC}" install
