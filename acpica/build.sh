3915c21d2a63ff0b94380d3cff639f88b7e44527
danielnachun <daniel.nachun@gmail.com>
2023-06-18 11:44:41 -0700

    add acpica

--- acpica/build.sh
+++ acpica/build.sh
@@ -2,5 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-make PREFIX=${PREFIX}
-make PREFIX=${PREFIX} install
+make PREFIX=${PREFIX} CFLAGS="${CFLAGS} -Wno-dangling-pointer"
+make PREFIX=${PREFIX} CFLAGS="${CFLAGS} -Wno-dangling-pointer" install
