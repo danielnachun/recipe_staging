4e504182cbba0abe6d1f5f724137e6ff1dce9fae
danielnachun <daniel.nachun@gmail.com>
2023-06-18 00:14:17 -0700

    add aces_container

--- aces_container/build.sh
+++ aces_container/build.sh
@@ -2,6 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cmake -S . -B build ${CMAKE_ARGS}
+
+cmake -S . -B build ${CMAKE_ARGS} -DCMAKE_CXX_FLAGS="-Wno-c++11-narrowing"
 cmake --build build
 cmake --install build
