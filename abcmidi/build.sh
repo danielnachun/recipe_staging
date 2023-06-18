541f584e67bc7878ee92c04b58f0a4aef8349ad2
danielnachun <daniel.nachun@gmail.com>
2023-06-17 19:50:36 -0700

    add abcmidi

---
+++ abcmidi/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+./configure --disable-silent \
+    --disable-debug \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} \
+    --mandir=${PREFIX}/share/man
+make install
