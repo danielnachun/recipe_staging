0d455289c2412e77fb06ccfa18fb55c8567ccb9f
danielnachun <daniel.nachun@gmail.com>
2023-06-17 19:14:09 -0700

    add abcde and dependencies

--- perl-musicbrainz-discid/0.06/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit -o pipefail -o xtrace
-
-perl Makefile.PL INSTALLDIRS=site
-make CCFLAGS="${CFLAGS}" OTHERLDFLAGS="${LDFLAGS} -ldiscid -L${CONDA_BUILD_SYSROOT}/usr/lib"
-make test
-make install
