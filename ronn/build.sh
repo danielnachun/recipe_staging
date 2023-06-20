5a5a21e1b0d12835c383999fc51502af4a7678d3
danielnachun <daniel.nachun@gmail.com>
2023-06-19 22:45:10 -0700

    add ronn

--- ronn/build.sh
+++ ronn/build.sh
@@ -7,9 +7,19 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/share/man/man1
 mkdir -p ${PREFIX}/share/man/man7
 
+cat << EOF > ${PREFIX}/etc/conda/activate.d/activate_ronn.sh
+#!/bin/sh
+GEM_HOME="${PREFIX}/libexec/ronn"
+EOF
+
+cat << EOF > ${PREFIX}/etc/conda/deactivate.d/deactivate_ronn.sh
+#!/bin/sh
+unset GEM_HOME
+EOF
+
 cat << EOF > ${PREFIX}/bin/ronn
-    #!/bin/sh
-    GEM_HOME="${PREFIX}/libexec/ronn" exec "${PREFIX}/libexec/ronn/bin/ronn" "$@"
+#!/bin/sh
+ exec "${PREFIX}/libexec/ronn/bin/ronn" "$@"
 EOF
 
 install -m 644 man/ronn.1 ${PREFIX}/share/man/man1/ronn.1
