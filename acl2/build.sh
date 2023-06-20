86724a66dffb160d88b072869a985d6bf9420cb2
danielnachun <daniel.nachun@gmail.com>
2023-06-19 22:22:59 -0700

    add acl2

--- acl2/build.sh
+++ acl2/build.sh
@@ -9,15 +9,27 @@ make LISP=${PREFIX}/bin/sbcl ACL2_PAR=p ACL2=${SRC_DIR}/saved_acl2p USE_QUICKLIS
 
 mkdir -p ${PREFIX}/libexec/acl2
 mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/etc/conda/activate.d/
+mkdir -p ${PREFIX}/etc/conda/deactivate.d/
 
 cp -r * ${PREFIX}/libexec/acl2
-cat << EOF > ${PREFIX}/bin/acl2
-    #!/bin/sh
-    export ACL2_SYSTEM_BOOKS='${PREFIX}/libexec/acl2/books'
-    ${PREFIX}/bin/sbcl --core '${PREFIX}/libexec/acl2/saved_acl2.core' --userinit /dev/null --eval '(acl2::sbcl-restart)'
+
+cat << EOF > ${PREFIX}/etc/conda/activate.d/activate_acl2.sh
+#!/bin/sh
+export ACL2_SYSTEM_BOOKS="${PREFIX}/libexec/acl2/books"
 EOF
+
+cat << EOF > ${PREFIX}/etc/conda/deactivate.d/deactivate_acl2.sh
+#!/bin/sh
+unset ACL2_SYSTEM_BOOKS
+EOF
+
+cat << EOF > ${PREFIX}/bin/acl2
+#!/bin/sh
+${PREFIX}/bin/sbcl --core "${PREFIX}/libexec/acl2/saved_acl2.core" --userinit /dev/null --eval '(acl2::sbcl-restart)'
+EOF
+
 cat << EOF > ${PREFIX}/bin/acl2p
-    #!/bin/sh
-    export ACL2_SYSTEM_BOOKS='${PREFIX}/libexec/acl2/books'
-    ${PREFIX}/bin/sbcl --core '${PREFIX}/libexec/acl2/saved_acl2p.core' --userinit /dev/null --eval '(acl2::sbcl-restart)'
+#!/bin/sh
+${PREFIX}/bin/sbcl --core "${PREFIX}/libexec/acl2/saved_acl2p.core" --userinit /dev/null --eval '(acl2::sbcl-restart)'
 EOF
