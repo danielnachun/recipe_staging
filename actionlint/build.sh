8718a27dff3cea0e3854f01f3cb4f764b6206f4c
danielnachun <daniel.nachun@gmail.com>
2023-06-20 19:37:23 -0700

    add actionlint

--- actionlint/build.sh
+++ actionlint/build.sh
@@ -7,4 +7,4 @@ export LDFLAGS="-s -w -X github.com/rhysd/actionlint.version=${PKG_VERSION}"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}" ./cmd/${PKG_NAME}
 ronn man/actionlint.1.ronn
 mkdir -p ${PREFIX}/share/man/man1
-install -m 644 man/actionlint.1 ${PREFIX}/share/man/man1/man/actionlint.1
+install -m 644 man/actionlint.1 ${PREFIX}/share/man/man1/actionlint.1
