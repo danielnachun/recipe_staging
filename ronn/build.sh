c029559567521c02a7bdb1ceb46475de557352cf
danielnachun <daniel.nachun@gmail.com>
2023-06-20 18:25:35 -0700

    add ronn

--- ronn/build.sh
+++ ronn/build.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env bash
 
-export GEM_HOME=${PREFIX}/libexec/ronn
-gem install -N -l -V --ignore-dependencies ronn-${PKG_VERSION}.gem
-mkdir -p ${PREFIX}/bin
+set -o xtrace -o nounset -o pipefail -o errexit
+
+gem install -N -l -V --ignore-dependencies ronn-${PKG_VERSION}.gem --bindir=${PREFIX}/bin
+gem unpack ronn-${PKG_VERSION}.gem
+
 mkdir -p ${PREFIX}/share/man/man1
 mkdir -p ${PREFIX}/share/man/man7
+ln -sf ${PREFIX}/share/rubygems/ronn-${PKG_VERSION}/man/man1/ronn.1 ${PREFIX}/share/man/man1
+ln -sf ${PREFIX}/share/rubygems/ronn-${PKG_VERSION}/man/man7/ronn.7 ${PREFIX}/share/man/man7
 
 cat << EOF > ${PREFIX}/etc/conda/activate.d/activate_ronn.sh
 #!/bin/sh
-GEM_HOME="${PREFIX}/libexec/ronn"
+GEM_HOME="${PREFIX}/share/rubygems"
 EOF
 
 cat << EOF > ${PREFIX}/etc/conda/deactivate.d/deactivate_ronn.sh
 #!/bin/sh
 unset GEM_HOME
 EOF
-
-cat << EOF > ${PREFIX}/bin/ronn
-#!/bin/sh
-exec "${PREFIX}/libexec/ronn/bin/ronn" "$@"
-EOF
-
-install -m 644 man/ronn.1 ${PREFIX}/share/man/man1/ronn.1
-install -m 644 man/ronn-format.7 ${PREFIX}/share/man/man7/ronn-format.7
