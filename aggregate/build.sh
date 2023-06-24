876a7b39cbd35cd9cb10c1e5f623388fd332f412
danielnachun <daniel.nachun@gmail.com>
2023-06-24 15:22:48 -0700

    add aggregate

--- aggregate/build.sh
+++ aggregate/build.sh
@@ -2,7 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-sed -i 's/$(prefix)/man/man1/$(prefix)/share/man/man1/g' Makefile.in
+sed -i 's?$(prefix)/man/man1?$(prefix)/share/man/man1?g' Makefile.in
 
 ./configure --disable-silent \
     --disable-debug \
