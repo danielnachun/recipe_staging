48b41a52bce035055f82c524aaf72c57f796fa8e
danielnachun <daniel.nachun@gmail.com>
2023-06-24 12:57:37 -0700

    add afio

---
+++ afio/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+make DESTDIR=${PREFIX}
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/share/man/man1
+
+install -m 755 afio ${PREFIX}/bin/afio
+install -m 644 afio.1 ${PREFIX}/share/man/man1/afio.1
+cp -r script* ${PREFIX}/share
