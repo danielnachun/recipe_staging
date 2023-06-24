dc45710bfa93b24be852bf0b214b9ca9f080d0d9
danielnachun <daniel.nachun@gmail.com>
2023-06-23 23:43:08 -0700

    add aespipe

--- aespipe/build.sh
+++ aespipe/build.sh
@@ -2,12 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-CFLAGS="-fPIC -std=gnu89"
-
 ./configure --disable-debug \
     --disable-dependency-tracking \
-    --prefix=${PREFIX} \
-    --enable-shared \
-    --mandir=${PREFIX}/share/man
-make install AR=${AR}
-make test AR=${AR}
+    --prefix=${PREFIX}
+make install
