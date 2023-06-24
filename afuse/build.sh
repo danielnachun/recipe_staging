9375eb03d4e7c0c75a6f400aea80ac61babe83d4
danielnachun <daniel.nachun@gmail.com>
2023-06-24 13:49:33 -0700

    add afuse

---
+++ afuse/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+./configure --disable-silent \
+    --disable-debug \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} 
+make install
