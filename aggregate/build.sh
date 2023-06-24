bcf0d62e3d90a27013145960c40f78b5ccea10b9
danielnachun <daniel.nachun@gmail.com>
2023-06-24 15:06:20 -0700

    add aggregate

---
+++ aggregate/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+sed -i 's/$(prefix)/man/man1/$(prefix)/share/man/man1/g' Makefile.in
+
+./configure --disable-silent \
+    --disable-debug \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} 
+make install
