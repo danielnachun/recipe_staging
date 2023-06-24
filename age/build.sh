7b0f03b8b52a0dd9d0a80a4498133d38a32255d3
danielnachun <daniel.nachun@gmail.com>
2023-06-24 14:37:30 -0700

    add age

--- age/build.sh
+++ age/build.sh
@@ -4,8 +4,10 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CGO_ENABLED=0
 export LDFLAGS="-s -w -X main.Version=v${PKG_VERSION}"
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/share/man/man1
+
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}" "filippo.io/age/cmd/..."
 
-mkdir -p ${PREFIX}/share/man/man1
 install -m 644 doc/age.1 ${PREFIX}/share/man/man1/age.1
 install -m 644 doc/age-keygen.1 ${PREFIX}/share/man/man1/age-keygen.1
