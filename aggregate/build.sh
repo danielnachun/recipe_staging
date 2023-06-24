540fca47ed02642165ccdc4666b4a51b64a91cca
danielnachun <daniel.nachun@gmail.com>
2023-06-24 16:25:39 -0700

    add aggregate

--- aggregate/build.sh
+++ aggregate/build.sh
@@ -3,9 +3,10 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 sed -i 's?$(prefix)/man/man1?$(prefix)/share/man/man1?g' Makefile.in
-
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/share/man/man1
+export CFLAGS="-Wno-implicit-int"
+
 ./configure --disable-silent \
     --disable-debug \
     --disable-dependency-tracking \
