125a603be0be29dcd0f0972b300b68ac14801c69
danielnachun <daniel.nachun@gmail.com>
2023-06-24 01:28:47 -0700

    add aescrypt

--- aescrypt/build.sh
+++ aescrypt/build.sh
@@ -2,10 +2,16 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+# Fix multiple definition error
+sed -i 's/int ROUNDS;/extern int ROUNDS;/' rijndael.h
+sed -i 's/#include "rijndael-boxes.dat"/&\nint ROUNDS;/' rijndael.c
+
 export CFLAGS="-Wno-implicit-int"
 ./configure --disable-silent \
     --disable-debug \
     --disable-dependency-tracking \
     --prefix=${PREFIX}
-make LIBS="-Wl,--allow-multiple-definition"
-make install
+make
+
+mkdir -p ${PREFIX}/bin
+install -m 755 aescrypt ${PREFIX}/bin/aescrypt
