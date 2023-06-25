6143533fecf2a6cfe255cc395091962c337ea97e
danielnachun <daniel.nachun@gmail.com>
2023-06-25 15:04:55 -0700

    add akita-cli

--- akira-cli/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-export CGO_ENABLED=0
-export LDFLAGS="-s -w"
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
