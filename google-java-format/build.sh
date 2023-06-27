50ab227a9a6769ee98497d15a6e1af05c7c6aef5
danielnachun <daniel.nachun@gmail.com>
2023-06-27 12:52:14 -0700

    add google-java-format

---
+++ google-java-format/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+mkdir release
+mvn install -DskipTests=true -Dmaven.repo.local=${SRC_DIR}
+
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+mkdir -p ${PREFIX}/bin
+
+cp com/google/googlejavaformat/google-java-format/${PKG_VERSION}/google-java-format-${PKG_VERSION}-all-deps.jar ${PREFIX}/libexec/${PKG_NAME}
+cat << EOF > ${PREFIX}/bin/google-java-format
+#!/bin/sh
+exec ${JAVA_HOME}/bin/java -jar ${PREFIX}/libexec/${PKG_NAME}/google-java-format-${PKG_VERSION}-all-deps.jar "\$@"
+EOF
