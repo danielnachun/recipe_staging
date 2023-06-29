04ca5af731d75867cfc4a35da4a4815f4dc7829a
danielnachun <daniel.nachun@gmail.com>
2023-06-28 20:54:27 -0700

    update rnaseqc

--- rnaseqc/build.sh
+++ rnaseqc/build.sh
@@ -3,9 +3,6 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 cd rnaseqc
-#sed -i "s?\$(CC)?-I${PREFIX}/include \$(INCLUDE_DIRS)?g" Makefile
-#$(STDLIB)
-#sed -i "s?\$(INCLUDE_DIRS)?-I${PREFIX}/include \$(INCLUDE_DIRS)?g" Makefile
 sed -i 's/CC/CXX/g' Makefile
 sed -i "s?-lz?-L${PREFIX}/lib -lz?g" SeqLib/bwa/Makefile
 sed -i "s?-lz?-L${PREFIX}/lib -lz?g" SeqLib/fermi-lite/Makefile
@@ -17,4 +14,5 @@ make CXX=${CXX} \
      CPPFLAGS="-I${PREFIX}/include" \
      LIBRARY_PATHS="-L${PREFIX}/lib"
 
-install -D -m 755 rnaseqc ${PREFIX}/bin
+mkdir -p ${PREFIX}/bin
+install -m 755 rnaseqc ${PREFIX}/bin
