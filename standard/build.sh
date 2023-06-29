c843597a1982679cc9046418a5daa0ace055ae08
danielnachun <daniel.nachun@gmail.com>
2023-06-29 14:55:16 -0700

    update standard

---
+++ standard/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}/bin
+gem install -N -l -V --ignore-dependencies --bindir "${PREFIX}/libexec/${PKG_NAME}/bin" ${PKG_NAME}-${PKG_VERSION}.gem
+gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
+
+mkdir -p ${PREFIX}/bin
+sed -i "s?\$bindir?${PREFIX}/bin?g" ${PREFIX}/libexec/${PKG_NAME}/bin/standardrb
+cat << EOF > ${PREFIX}/bin/standardrb
+#!/bin/sh
+GEM_HOME=${PREFIX}/share/rubygems exec ${PREFIX}/libexec/${PKG_NAME}/bin/standardrb "\$@"
+EOF
