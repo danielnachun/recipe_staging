918d52dc35099b98f1ab9d3dc8a56751cd0b6c5a
danielnachun <daniel.nachun@gmail.com>
2023-07-03 22:34:24 -0700

    add fsautocomplete

--- fsautocomplete/build.sh
+++ fsautocomplete/build.sh
@@ -2,9 +2,15 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+if [[ ${target_platform} =~ .*linux.* ]]; then
+    export LD_LIBRARY_PATH="${PREFIX}/lib:${CONDA_BUILD_SYSROOT}/usr/lib"
+fi
+
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+
 rm global.json
 dotnet tool restore 
 dotnet run --project build -t LocalRelease
-dotnet tool install --add-source bin/release_as_tool --tool-path ${DOTNET_ROOT}/tools fsautocomplete
+dotnet tool install --add-source bin/release_as_tool --tool-path ${PREFIX}/libexec/${PKG_NAME} fsautocomplete
 
-ln -sf ${DOTNET_ROOT}/tools/fsautocomplete ${PREFIX}/bin
+ln -sf ${PREFIX}/libexec/${PKG_NAME}/fsautocomplete ${PREFIX}/bin
