d2b8746bd8a16dc3969d637f5d4cef9c97a9d6fd
danielnachun <daniel.nachun@gmail.com>
2023-07-06 16:33:26 -0700

    add powershell

--- powershell-native/build.sh
+++ powershell-native/build.sh
@@ -6,4 +6,4 @@ sed -i "s/add_subdirectory(test)//" src/libpsl-native/CMakeLists.txt
 cmake -S src/libpsl-native -B build ${CMAKE_ARGS}
 cmake --build build
 mkdir -p ${PREFIX}/lib
-cp src/powershell-unix/libpsl-native.dylib ${PREFIX}/lib
+cp src/powershell-unix/libpsl-native${SHLIB_EXT} ${PREFIX}/lib
