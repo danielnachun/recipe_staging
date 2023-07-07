b77ad6e15839f348345734731b55fbc7dd78b650
danielnachun <daniel.nachun@gmail.com>
2023-07-06 20:43:06 -0700

    add libprelude

---
+++ libprelude/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export SETUPTOOLS_USE_DISTUTILS=stdlib
+export HAVE_CXX=yes
+export CFLAGS="-Wno-int-conversion"
+
+./configure --disable-silent \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} \
+    --disable-silent-rules \
+    --without-valgrind \
+    --without-lua \
+    --without-ruby \
+    --without-perl \
+    --without-swig \
+    --without-python2 
+make
+make install
