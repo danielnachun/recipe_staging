63371413bf13a23ac5fac4813ceb1aed998d1379
danielnachun <daniel.nachun@gmail.com>
2023-07-08 14:45:06 -0700

    add compose-language-service

---
+++ compose-language-service/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+npm pack --ignore-scripts
+
+npm install -ddd \
+    --global \
+    --build-from-source \
+    --prefix=${PREFIX}/libexec/${PKG_NAME} \
+    ${PKG_NAME}-${PKG_VERSION}.tgz
+
+mkdir -p ${PREFIX}/bin
+ln -sf ${PREFIX}/libexec/${PKG_NAME}/bin/docker-compose-langserver ${PREFIX}/bin
