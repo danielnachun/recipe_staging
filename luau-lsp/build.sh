54f56c208137f2557b7a064dd4863450f471db21
danielnachun <daniel.nachun@gmail.com>
2023-07-09 12:20:04 -0700

    add luau-lsp

--- luau-lsp/build.sh
+++ luau-lsp/build.sh
@@ -2,7 +2,11 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cmake -S . -B build ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release
+if [[ ${target_platform} =~ .*osx.* ]]; then
+    cmake -S . -B build ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15
+else
+    cmake -S . -B build ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wno-maybe-uninitialized"
+fi
 cmake --build build --target Luau.LanguageServer.CLI --config Release
 
 mkdir -p ${PREFIX}/bin
