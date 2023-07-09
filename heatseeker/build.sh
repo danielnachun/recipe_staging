ded7a31171b9ad9acb618607aa69a2a5e39dddb7
danielnachun <daniel.nachun@gmail.com>
2023-07-08 17:41:55 -0700

    add heatseeker

--- heatseeker/build.sh
+++ heatseeker/build.sh
@@ -10,7 +10,7 @@ cargo-bundle-licenses \
 cargo install --locked --root "${PREFIX}" --path .
 
 # strip debug symbols
-"${STRIP}" "${PREFIX}/bin/${PKG_NAME}"
+"${STRIP}" "${PREFIX}/bin/hs"
 
 # remove extra build file
 rm -f "${PREFIX}/.crates.toml"
