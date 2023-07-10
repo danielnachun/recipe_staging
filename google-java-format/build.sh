a06219b1aa4f6a5c8b754f45e3cfc2d6023ff6e4
danielnachun <daniel.nachun@gmail.com>
2023-07-10 12:14:08 -0700

    add google-java-format

--- google-java-format/build.sh
+++ google-java-format/build.sh
@@ -11,7 +11,7 @@ mkdir -p ${PREFIX}/bin
 cp com/google/googlejavaformat/google-java-format/${PKG_VERSION}/google-java-format-${PKG_VERSION}-all-deps.jar ${PREFIX}/libexec/${PKG_NAME}
 cat << EOF > ${PREFIX}/bin/google-java-format
 #!/bin/sh
-exec ${JAVA_HOME}/bin/java -jar ${PREFIX}/libexec/${PKG_NAME}/google-java-format-${PKG_VERSION}-all-deps.jar "\$@"
+JAVA_HOME=${JAVA_HOME} exec ${JAVA_HOME}/bin/java -jar ${PREFIX}/libexec/${PKG_NAME}/google-java-format-${PKG_VERSION}-all-deps.jar "\$@"
 EOF
 
 install -m 755 scripts/google-java-format-diff.py ${PREFIX}/bin/google-java-format-diff
