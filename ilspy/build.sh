dc412636f60a46679b5c97a15ca0fb2931808c98
danielnachun <daniel.nachun@gmail.com>
2023-07-11 16:02:58 -0700

    add ilspy

--- ilspy/build.sh
+++ ilspy/build.sh
@@ -6,7 +6,22 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
 rm global.json
-dotnet publish --no-self-contained --output bin -maxcpucount:1 ILSpy.XPlat.slnf --runtime osx-x64
+case ${target_platform} in 
+    osx-64 )
+        dotnet publish --no-self-contained --configuration Darwin ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime osx-x64 ;;
+        rm -rf bin/InstrumentationEngine/ubuntu
+    osx-arm64 )
+        dotnet publish --no-self-contained --configuration Darwin ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime osx-arm64 ;;
+        rm -rf bin/InstrumentationEngine/ubuntu
+    linux-64 )
+        dotnet publish --no-self-contained --configuration Linux ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime linux-x64 ;;
+        rm -rf bin/InstrumentationEngine/macos
+    linux-arm64 )
+        dotnet publish --no-self-contained --configuration Linux ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime linux-arm64 ;;
+        rm -rf bin/InstrumentationEngine/macos
+esac
+
+rm -rf bin/InstrumentationEngine/alpine
 sed -i "s/net6.0/net7.0/" bin/ilspycmd.runtimeconfig.json
 sed -i "s/6.0.0/7.0.0/" bin/ilspycmd.runtimeconfig.json
 cp -r bin/* ${PREFIX}/libexec/${PKG_NAME}
