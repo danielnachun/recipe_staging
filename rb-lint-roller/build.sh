ead53f3b4b1d5d063eaae0564f9f7a4e4e24d09d
danielnachun <daniel.nachun@gmail.com>
2023-07-11 22:30:01 -0700

    fix rb-lint-roller

--- rb-lint-roller/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-gem install -N -l -V --ignore-dependencies lint_roller-${PKG_VERSION}.gem
-gem unpack lint_roller-${PKG_VERSION}.gem
