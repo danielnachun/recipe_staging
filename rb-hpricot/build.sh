e71fe3fda0e6c7a14539c89980ee080f3717b7fb
danielnachun <daniel.nachun@gmail.com>
2023-07-12 21:51:31 -0700

    add other packages

--- rb-hpricot/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-gem install -N -l -V --ignore-dependencies hpricot-${PKG_VERSION}.gem -- --with-cflags="-Wno-incompatible-function-pointer-types" 
-gem unpack hpricot-${PKG_VERSION}.gem
