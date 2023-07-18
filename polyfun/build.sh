6992f15d98e264a841fa96814768de3e3062797b
danielnachun <daniel.nachun@gmail.com>
2023-07-18 14:29:14 -0700

    add polyfun

--- polyfun/build.sh
+++ polyfun/build.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-install -Dd "${PREFIX}/opt/polyfun"
-cp -r "${SRC_DIR}"/* "${PREFIX}/opt/polyfun"
+set -o xtrace -o nounset -o pipefail -o errexit
 
-install -Dd "${PREFIX}/bin"
-ln -sf "${PREFIX}/opt/polyfun/munge_polyfun_sumstats.py" "${PREFIX}/bin"
-ln -sf "${PREFIX}/opt/polyfun/polyfun.py" "${PREFIX}/bin"
-ln -sf "${PREFIX}/opt/polyfun/polyloc.py" "${PREFIX}/bin"
-ln -sf "${PREFIX}/opt/polyfun/extract_snpvar.py" "${PREFIX}/bin"
-ln -sf "${PREFIX}/opt/polyfun/finemapper.py" "${PREFIX}/bin"
-ln -sf "${PREFIX}/opt/polyfun/test_polyfun.py" "${PREFIX}/bin"
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+cp -r "${SRC_DIR}"/* "${PREFIX}/libexec/${PKG_NAME}"
+
+mkdir -p ${PREFIX}/bin
+ln -sf "${PREFIX}/libexec/${PKG_NAME}/munge_polyfun_sumstats.py" "${PREFIX}/bin"
+ln -sf "${PREFIX}/libexec/${PKG_NAME}/polyfun.py" "${PREFIX}/bin"
+ln -sf "${PREFIX}/libexec/${PKG_NAME}/polyloc.py" "${PREFIX}/bin"
+ln -sf "${PREFIX}/libexec/${PKG_NAME}/extract_snpvar.py" "${PREFIX}/bin"
+ln -sf "${PREFIX}/libexec/${PKG_NAME}/finemapper.py" "${PREFIX}/bin"
+ln -sf "${PREFIX}/libexec/${PKG_NAME}/test_polyfun.py" "${PREFIX}/bin"
