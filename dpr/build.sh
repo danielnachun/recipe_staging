062519582d4997c2f010df5213aed0a163f5b7b2
danielnachun <daniel.nachun@gmail.com>
2023-07-19 23:13:29 -0700

    fix duplicated symbols

--- dpr/build.sh
+++ dpr/build.sh
@@ -5,7 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 arc unarchive DPR.rar
 cd DPR
 make clean
-make CPP="${CXX} -I${PREFIX}/include -L${PREFIX}/lib -Wno-narrowing -Wl,--allow-multiple-definition" FORCE_DYNAMIC=1
+make CPP="${CXX} -I${PREFIX}/include -L${PREFIX}/lib -Wno-narrowing" FORCE_DYNAMIC=1
 
 mkdir -p ${PREFIX}/bin
 install -m 755 bin/DPR ${PREFIX}/bin
