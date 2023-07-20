fe510cccc93233e1a12133c7f3c32c7d6835a003
danielnachun <daniel.nachun@gmail.com>
2023-07-20 15:44:15 -0700

    add king

---
+++ king/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CXX_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+
+${CXX} -I${CXX_INCLUDE_PATH} -L${LIBRARY_PATH} -lm -lz -O2 -fopenmp -o king *.cpp
+mkdir -p $PREFIX/bin
+install -m 755 $PREFIX/bin/king
