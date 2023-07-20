5657e8e52ccbab30702cb9c700341f30c368223c
danielnachun <daniel.nachun@gmail.com>
2023-07-20 00:09:09 -0700

    fix cstdint

--- dpr/build.sh
+++ dpr/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 arc unarchive DPR.rar
 patch -p1 -i ${RECIPE_DIR}/0001-fix-duplicated-symbols.patch
+patch -p1 -i ${RECIPE_DIR}/0002-fix-cstdint.patch
 cd DPR
 make clean
 make CPP="${CXX} -I${PREFIX}/include -L${PREFIX}/lib -Wno-narrowing" FORCE_DYNAMIC=1
