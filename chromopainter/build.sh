51b23fb2a825107301be7d21e03cad7d47a466a6
danielnachun <daniel.nachun@gmail.com>
2023-07-20 14:31:34 -0700

    add chromopainter

---
+++ chromopainter/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+#export CXXFLAGS="-std=c++03"
+
+./configure --disable-debug \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} \
+    --libdir=${PREFIX}/lib
+
+make install
