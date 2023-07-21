dd97bbb83c201309904423b3a66d050af388d533
danielnachun <daniel.nachun@gmail.com>
2023-07-20 17:21:56 -0700

    add members

--- members/build.sh
+++ members/build.sh
@@ -6,6 +6,6 @@ make
 
 mkdir -p ${PREFIX}/bin 
 mkdir -p ${PREFIX}/share/man/man1
-install -m 755 members -t ${PREFIX}/bin 
-install -m 644 members.1 -t ${PREFIX}/share/man/man1
+install -m 755 members ${PREFIX}/bin 
+install -m 644 members.1 ${PREFIX}/share/man/man1
 
