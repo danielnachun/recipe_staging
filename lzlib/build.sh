6a160b7470e48d91475f80f7de94232250128b3c
danielnachun <daniel.nachun@gmail.com>
2023-07-21 00:19:43 -0700

    start fixing lzlib

--- lzlib/build.sh
+++ lzlib/build.sh
@@ -2,6 +2,8 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+sed -i "s/-Wl,--soname=/-install_name /g" Makefile.in
+
 ./configure --disable-debug \
     --disable-dependency-tracking \
     --enable-shared \
