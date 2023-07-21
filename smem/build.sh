0588058880ba85d394fa3843d08e3068ca6956db
danielnachun <daniel.nachun@gmail.com>
2023-07-20 17:45:46 -0700

    add smem

--- smem/build.sh
+++ smem/build.sh
@@ -6,6 +6,6 @@ make smemcap
 
 mkdir -p ${PREFIX}/bin 
 mkdir -p ${PREFIX}/share/man/man8
-install -m 755 smem -t ${PREFIX}/bin 
-install -m 755 smemcap -t ${PREFIX}/bin 
-install -m 644 smem.8 -t ${PREFIX}/share/man/man8
+install -m 755 smem  ${PREFIX}/bin 
+install -m 755 smemcap ${PREFIX}/bin 
+install -m 644 smem.8 ${PREFIX}/share/man/man8
