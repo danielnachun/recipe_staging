a9df3cee3773f1df81d4744d7a93fdd410765a8d
danielnachun <daniel.nachun@gmail.com>
2023-07-20 23:25:03 -0700

    add sanity

--- sanity/build.sh
+++ sanity/build.sh
@@ -2,9 +2,12 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cd src
-make CC=${CXX}
-cd ..
+rm -rf bin/*
+
+pushd src
+    make Sanity CC="${CXX} -I${PREFIX}/include"
+    make Sanity_distance CC="${CXX} -I${PREFIX}/include"
+popd ..
 
 mkdir -p ${PREFIX}/bin
 install -m 755 bin/Sanity ${PREFIX}/bin
