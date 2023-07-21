d4d809efe53673b2a29192616f927a2eadd3c796
danielnachun <daniel.nachun@gmail.com>
2023-07-21 00:23:35 -0700

    add lzlib

--- lzlib/build.sh
+++ lzlib/build.sh
@@ -2,8 +2,6 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-sed -i "s/-Wl,--soname=/-install_name /g" Makefile.in
-
 ./configure --disable-debug \
     --disable-dependency-tracking \
     --enable-shared \
