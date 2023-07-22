a7d1f9e8b3b432b5ade0492c5c50060874843ab3
danielnachun <daniel.nachun@gmail.com>
2023-07-21 21:26:33 -0700

    add otters

--- otters/build.sh
+++ otters/build.sh
@@ -1,12 +1,15 @@
-#!/bin/bash 
+#!/usr/bin/env bash 
 
-install -Dd ${PREFIX}/bin ${PREFIX}/libexec/otters
-cp -r PRSmodels ${PREFIX}/libexec/otters
-rm -rf ${PREFIX}/libexec/otters/PRSmodels/__pycache__
-cp -r *.py ${PREFIX}/libexec/otters
-chmod +x ${PREFIX}/libexec/otters/imputing.py
-chmod +x ${PREFIX}/libexec/otters/testing.py
-chmod +x ${PREFIX}/libexec/otters/training.py
-ln -sf ${PREFIX}/libexec/otters/imputing.py ${PREFIX}/bin/imputing
-ln -sf ${PREFIX}/libexec/otters/testing.py ${PREFIX}/bin/testing
-ln -sf ${PREFIX}/libexec/otters/training.py ${PREFIX}/bin/training
+set -o xtrace -o nounset -o pipefail -o errexit
+
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+cp -r PRSmodels ${PREFIX}/libexec/${PKG_NAME}
+rm -rf ${PREFIX}/libexec/${PKG_NAME}/PRSmodels/__pycache__
+cp -r *.py ${PREFIX}/libexec/${PKG_NAME}
+chmod +x ${PREFIX}/libexec/${PKG_NAME}/imputing.py
+chmod +x ${PREFIX}/libexec/${PKG_NAME}/testing.py
+chmod +x ${PREFIX}/libexec/${PKG_NAME}/training.py
+ln -sf ${PREFIX}/libexec/${PKG_NAME}/imputing.py ${PREFIX}/bin/imputing
+ln -sf ${PREFIX}/libexec/${PKG_NAME}/testing.py ${PREFIX}/bin/testing
+ln -sf ${PREFIX}/libexec/${PKG_NAME}/training.py ${PREFIX}/bin/training
