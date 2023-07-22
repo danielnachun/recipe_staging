e236c7d32c37dde919b89024ec71cab713cd73e4
danielnachun <daniel.nachun@gmail.com>
2023-07-22 14:56:38 -0700

    add gcsfuse

---
+++ gcsfuse/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+sed -i "s/os.Mkdir(/os.MkdirAll(/g" tools/build_gcsfuse/main.go
+go build ./tools/build_gcsfuse
+./build_gcsfuse ${SRC_DIR} ${PREFIX} ${PKG_VERSION}
