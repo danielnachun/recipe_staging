888c8cd7d4685e5069a895c86fcb1da9bacb1e48
danielnachun <daniel.nachun@gmail.com>
2023-07-22 14:45:50 -0700

    add fork-cleaner

--- fork-cleaner/build.sh
+++ fork-cleaner/build.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CGO_ENABLED=0
 make
-install -Dm 755 ${PKG_NAME} ${PREFIX}/bin/${PKG_NAME}
+mkdir -p ${PREFIX}/bin
+install -m 755 ${PKG_NAME} ${PREFIX}/bin/${PKG_NAME}
