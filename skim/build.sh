c65fca4b610c9d47aa437c2cdf1c9690de12d718
danielnachun <daniel.nachun@gmail.com>
2023-07-23 13:19:21 -0700

    add skim

--- skim/build.sh
+++ skim/build.sh
@@ -17,14 +17,14 @@ mkdir -p ${PREFIX}/etc/bash_completion.d
 mkdir -p ${PREFIX}/share/fish/vendor_completions.d
 mkdir -p ${PREFIX}/share/zsh/site-functions
 mkdir -p ${PREFIX}/share/man/man1
-install -Dm 644 shell/key-bindings.bash ${PREFIX}/etc/bash_completion.d/key-bindings.bash
-install -Dm 644 shell/completion.bash ${PREFIX}/etc/bash_completion.d/completion.bash
-install -Dm 644 shell/key-bindings.fish ${PREFIX}/share/fish/vendor_completions.d/skim.fish
-install -Dm 644 shell/key-bindings.zsh ${PREFIX}/share/zsh/site-functions/key-bindings.zsh
-install -Dm 644 shell/completion.zsh ${PREFIX}/share/zsh/site-functions/completions.zsh
-install -Dm 644 man/man1/sk.1 ${PREFIX}/share/man/man1/sk.1
-install -Dm 644 man/man1/sk-tmux.1 ${PREFIX}/share/man/man1/sk-tmux.1
-install -Dm 755 bin/sk-tmux ${PREFIX}/bin/sk-tmux
+install -m 644 shell/key-bindings.bash ${PREFIX}/etc/bash_completion.d/key-bindings.bash
+install -m 644 shell/completion.bash ${PREFIX}/etc/bash_completion.d/completion.bash
+install -m 644 shell/key-bindings.fish ${PREFIX}/share/fish/vendor_completions.d/skim.fish
+install -m 644 shell/key-bindings.zsh ${PREFIX}/share/zsh/site-functions/key-bindings.zsh
+install -m 644 shell/completion.zsh ${PREFIX}/share/zsh/site-functions/completions.zsh
+install -m 644 man/man1/sk.1 ${PREFIX}/share/man/man1/sk.1
+install -m 644 man/man1/sk-tmux.1 ${PREFIX}/share/man/man1/sk-tmux.1
+install -m 755 bin/sk-tmux ${PREFIX}/bin/sk-tmux
 
 # strip debug symbols
 "$STRIP" "$PREFIX/bin/sk"
