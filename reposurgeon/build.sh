2508d163a7f48da94f1ef99b148dd54bdd4851bb
danielnachun <daniel.nachun@gmail.com>
2023-07-23 12:38:55 -0700

    add reposurgeon

--- reposurgeon/build.sh
+++ reposurgeon/build.sh
@@ -8,5 +8,5 @@ export XML_CATALOG_FILES=${PREFIX}/etc/xml/catalog
 make
 make install prefix=${PREFIX}
 
-mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/share/emacs/site-lisp
 install -m 644 reposurgeon-mode.el ${PREFIX}/share/emacs/site-lisp/reposurgeon-mode.el
