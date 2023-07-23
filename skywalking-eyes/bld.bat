c19f7da151949764fc5c1143ec0afe66e614a4fd
danielnachun <daniel.nachun@gmail.com>
2023-07-23 13:20:34 -0700

    add skywalking-eyes

--- skywalking-eyes/bld.bat
+++ skywalking-eyes/bld.bat
@@ -1,6 +1,6 @@
 set "CGO_ENABLED=0"
 set "LDFLAGS=-s -w -X github.com/apache/skywalking-eyes/commands.version=%PKG_VERSION%"
-go build -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="%LDFLAGS%" .\cmd\license-eye || goto :error
+go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="%LDFLAGS%" .\cmd\license-eye || goto :error
 goto :EOF
 
 :error
