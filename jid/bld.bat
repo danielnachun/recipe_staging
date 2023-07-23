ba9f8e7421c95a9fbd0cd9fdfe98acf13dd1282e
danielnachun <daniel.nachun@gmail.com>
2023-07-22 18:07:03 -0700

    add jid

---
+++ jid/bld.bat
@@ -0,0 +1,9 @@
+set "CGO_ENABLED=0"
+set "LDFLAGS=-s -w"
+cd cmd\jid
+go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="%LDFLAGS%" || goto :error
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
