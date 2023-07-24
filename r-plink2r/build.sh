457d1f100cde6834bb122ad6ea975dab314dd552
danielnachun <daniel.nachun@gmail.com>
2023-07-23 17:25:34 -0700

    add r-plink2r

---
+++ r-plink2r/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export DISABLE_AUTOBREW=1
+
+# shellcheck disable=SC2086
+cd plink2R
+sed -i '/~~ Optionally other standard keywords, one per line, from file KEYWORDS in ~~/d' man/plink2R-package.Rd
+sed -i '/~~ the R documentation directory ~~/d' man/plink2R-package.Rd
+${R} CMD INSTALL --build . ${R_ARGS}
