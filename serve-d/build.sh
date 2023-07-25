6a5aef357104e6d9da21e84d7bf6ed2e020ab864
danielnachun <daniel.nachun@gmail.com>
2023-07-24 23:19:37 -0700

    add serve-d

--- serve-d/build.sh
+++ serve-d/build.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash 
+
+set -o xtrace -o nounset -o pipefail -o errexit
 
 dub upgrade
 dub build --build=release --compiler=ldc2
 
-install -Dd ${PREFIX}/bin
-install -Dm 755 serve-d ${PREFIX}/bin
+mkdir -p ${PREFIX}/bin
+install -m 755 serve-d ${PREFIX}/bin
