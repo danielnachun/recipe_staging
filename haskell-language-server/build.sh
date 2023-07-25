e6239ae910ee4be43b9907f7125cbbfdcf82810e
danielnachun <daniel.nachun@gmail.com>
2023-07-25 01:55:53 -0700

    add haskell-language-server

---
+++ haskell-language-server/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+cabal v2-update
+cabal --store-dir=${SRC_DIR}/.cabal v2-install \
+    --extra-lib-dirs=${PREFIX}/lib \
+    --flags=-dynamic \
+	--jobs=${CPU_COUNT} \
+	--max-backjumps=100000 \
+	--install-method=copy \
+	--installdir=${PREFIX}/bin
