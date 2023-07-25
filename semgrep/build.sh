f5dc0d8660df547b3521ac84d8423e4241fca98c
danielnachun <daniel.nachun@gmail.com>
2023-07-25 02:03:36 -0700

    add semgrep

---
+++ semgrep/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export OPAMROOT=${SRC_DIR}/opamroot
+export OPAMYES=1
+export SETUPTOOLS_USE_DISTUTILS=stdlib
+
+opam init --no-setup --disable-sandboxing
+opam update -y
+opam install -vvv -j ${CPU_COUNT} -y --deps-only --no-depexts ./libs/ocaml-tree-sitter-core
+opam install -vvv -j ${CPU_COUNT} -y --deps-only --no-depexts ./
+
+pushd ./libs/ocaml-tree-sitter-core
+	./configure
+popd
+
+opam install -vvv -j ${CPU_COUNT} --deps-only --no-depexts -y .
+opam exec -- make core
+opam exec -- make core-install
+
+mkdir -p ${PREFIX}/bin
+install -m 755 _build/install/default/bin/semgrep-core ${PREFIX}/bin/semgrep-core
+install -m 755 _build/install/default/bin/spacegrep ${PREFIX}/bin/spacegrep
+
+export SEMGREP_SKIP_BIN=1
+cd cli
+${PYTHON} -m pip install . -vv
