88d8d85cb86c390a9c831c002a7bda770f1aa7ee
danielnachun <daniel.nachun@gmail.com>
2023-07-25 00:46:59 -0700

    add comby

---
+++ comby/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export OPAMROOT=${SRC_DIR}/.opam
+export OPAMYES=1
+export OPAMDOWNLOADJOBS=${CPU_COUNT}
+export OPAMJOBS=${CPU_COUNT}
+
+opam init --no-setup --disable-sandboxing
+opam exec -- opam install . --deps-only -y --no-depexts
+
+export LIBRARY_PATH="${OPAMROOT}/default/lib/hack_parallel"
+opam exec -- make release
+
+install -Dm 755 _build/default/src/main.exe ${PREFIX}/bin/comby
