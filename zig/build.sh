f8c22d1154864bf30f4ad939cdb82dce9471f3aa
danielnachun <daniel.nachun@gmail.com>
2023-07-26 22:59:35 -0700

    add zig

---
+++ zig/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+if [[ ${target_platform} =~ .*linux.* ]]; then
+    mkdir -p build
+    cd build
+
+    cmake .. ${CMAKE_ARGS} \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_PREFIX_PATH=${PREFIX} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_C_COMPILER=${CC} \
+      -DCMAKE_CXX_COMPILER=${CXX} \
+      -DZIG_STATIC_LLVM=OFF
+
+    cmake --build .
+    cmake --install . -v
+else
+    mkdir -p ${PREFIX}/bin
+    mkdir -p ${PREFIX}/lib/zig
+    install -m 755 zig ${PREFIX}/bin
+    cp -R lib/* ${PREFIX}/lib/zig
+fi
