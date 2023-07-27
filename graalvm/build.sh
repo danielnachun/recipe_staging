3a6612ba26e5e02c1b54eed6b81bfcae495db7fd
danielnachun <daniel.nachun@gmail.com>
2023-07-27 11:20:44 -0700

    add graalvm

--- graalvm/build.sh
+++ graalvm/build.sh
@@ -2,7 +2,15 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-./configure --disable-silent \
-    --disable-dependency-tracking \
-    --prefix=${PREFIX} 
-make install
+export CFLAGS="$CFLAGS --sysroot $CONDA_BUILD_SYSROOT"
+export CXXFLAGS="$CXXFLAGS --sysroot $CONDA_BUILD_SYSROOT"
+export LDFLAGS="$LDFLAGS --sysroot $CONDA_BUILD_SYSROOT"
+export CPATH="$BUILD_PREFIX/include"
+export LIBRARY_PATH="$BUILD_PREFIX/lib"
+
+git init $SRC_DIR
+git -C $SRC_DIR config --local user.name "none"
+git -C $SRC_DIR config --local user.email "none@example.org"
+git -C $SRC_DIR commit --allow-empty -m "dummy commit"
+
+mx --env ce build 
