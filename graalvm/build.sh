a77f49c0fc8ca3392fb86e098278e3516657daec
danielnachun <daniel.nachun@gmail.com>
2023-07-29 16:19:06 -0700

    add graalvm

--- graalvm/build.sh
+++ graalvm/build.sh
@@ -8,9 +8,10 @@ set -o xtrace -o nounset -o pipefail -o errexit
 #export CPATH="$BUILD_PREFIX/include"
 #export LIBRARY_PATH="$BUILD_PREFIX/lib"
 
-git init $SRC_DIR
-git -C $SRC_DIR config --local user.name "none"
-git -C $SRC_DIR config --local user.email "none@example.org"
-git -C $SRC_DIR commit --allow-empty -m "dummy commit"
+git init $SRC_DIR/graal
+git -C $SRC_DIR/graal config --local user.name "none"
+git -C $SRC_DIR/graal config --local user.email "none@example.org"
+git -C $SRC_DIR/graal commit --allow-empty -m "dummy commit"
 
-mx --env ce build 
+export MX_PRIMARY_SUITE_PATH=graal/vm
+mx --env ce-darwin build 
