e993ab5ba9df4f01607806894be6e967f7771ad4
danielnachun <daniel.nachun@gmail.com>
2023-07-31 23:34:54 -0700

    add clojure-lsp

--- clojure-lsp/build.sh
+++ clojure-lsp/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 mkdir -p ${PREFIX}/bin
 
 export GRAALVM_HOME=${BUILD_PREFIX}/lib/jvm
+sed -i "s?\"--native-image-info\"?\"--native-image-info\" \"--native-compiler-options=-L${PREFIX}/lib\"?" cli/build.clj
 bb native-cli
 
 install -m 755 clojure-lsp ${PREFIX}/bin
