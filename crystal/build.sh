f83d0e39f2e3fdf45b19280e2d7acba732dc9e8c
danielnachun <daniel.nachun@gmail.com>
2023-08-03 21:45:14 -0700

    add crystal

---
+++ crystal/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export PATH="${PATH}:${SRC_DIR}/boot/bin"
+export LLVM_CONFIG=${BUILD_PREFIX}/bin/llvm_config
+export CRYSTAL_LIBRARY_PATH="${PREFIX}/lib:${CONDA_BUILD_SYSROOT}/usr/lib"
+
+mkdir -p .build
+make install release=true FLAGS="--no-debug -D use_libiconv" interpreter=true PREFIX=${PREFIX}
+install -m 644 src/llvm/ext/llvm_ext.o ${PREFIX}/share/crystal/src/llvm/ext
