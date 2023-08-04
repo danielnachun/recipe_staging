47b5bbde93e9dbfaff8ad65f224874d0374678ab
danielnachun <daniel.nachun@gmail.com>
2023-08-03 21:45:53 -0700

    add crystalline

---
+++ crystalline/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export LLVM_CONFIG=${BUILD_PREFIX}/bin/llvm-config
+export CRYSTAL_LIBRARY_PATH="${PREFIX}/lib:${CONDA_BUILD_SYSROOT}/usr/lib"
+
+shards install
+crystal build ./src/crystalline.cr \
+    --release \
+    --no-debug \
+    -Dpreview_mt \
+    -Duse_libiconv \
+    --progress \
+    --stats \
+    --time \
+    -o crystalline
+
+mkdir -p ${PREFIX}/bin
+install -m 755 crystalline ${PREFIX}/bin
