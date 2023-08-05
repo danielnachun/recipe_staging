2fcf148e466ec236c38cd75ad416e4c07ea7a789
danielnachun <daniel.nachun@gmail.com>
2023-08-05 10:52:06 -0700

    add shards

--- shards/build.sh
+++ shards/build.sh
@@ -3,6 +3,6 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 export CRYSTAL_LIBRARY_PATH="${PREFIX}/lib:${CONDA_BUILD_SYSROOT}/usr/lib"
-export DYLD_LIBRARY_PATH="${PREFIX}/lib"
+export CRYSTAL_LIBRARY_RPATH="${PREFIX}/lib"
 
 make install release=true FLAGS="--no-debug" CRYSTAL=${BUILD_PREFIX}/bin/crystal SHARDS=false PREFIX=${PREFIX}
