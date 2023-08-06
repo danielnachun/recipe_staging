8c305701b60a007755552991cab812b9a4f4d616
danielnachun <daniel.nachun@gmail.com>
2023-08-05 17:47:32 -0700

    add csharpier

--- csharpier/build.sh
+++ csharpier/build.sh
@@ -6,17 +6,7 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
 rm -rf global.json
-case ${target_platform} in 
-    osx-64 )
-        dotnet publish --no-self-contained Src/CSharpier.Cli/CSharpier.Cli.csproj -maxcpucount:1 --output bin --runtime osx-x64 --framework net7.0 ;;
-    osx-arm64 )
-        dotnet publish --no-self-contained Src/CSharpier.Cli/CSharpier.Cli.csproj -maxcpucount:1 --output bin --runtime osx-arm64 --framework net7.0 ;;
-    linux-64 )
-        dotnet publish --no-self-contained Src/CSharpier.Cli/CSharpier.Cli.csproj -maxcpucount:1 --output bin --runtime linux-x64 --framework net7.0 ;;
-    linux-arm64 )
-        dotnet publish --no-self-contained Src/CSharpier.Cli/CSharpier.Cli.csproj -maxcpucount:1 --output bin --runtime linux-arm64 --framework net7.0 ;;
-esac
-
+dotnet publish --no-self-contained Src/CSharpier.Cli/CSharpier.Cli.csproj -maxcpucount:1 --output bin --runtime ${target_platform//-64/-x64} --framework net7.0
 cp -r bin/* ${PREFIX}/libexec/${PKG_NAME}
 
 tee ${PREFIX}/bin/dotnet-csharpier << EOF
