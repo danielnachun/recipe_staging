6fe59a78cd811120b0fe4544f18fbc9999866272
danielnachun <daniel.nachun@gmail.com>
2023-08-17 11:47:22 -0700

    add uutils-findutils

--- uutils-findutils/build.sh
+++ uutils-findutils/build.sh
@@ -6,10 +6,10 @@ cargo-bundle-licenses \
     --format yaml \
     --output THIRDPARTY.yml
 
+export LIBCLANG_PATH=${BUILD_PREFIX}/lib
+export RUSTONIG_DYNAMIC_LIBONIG=1
 # build statically linked binary with Rust
 cargo install --locked --root ${PREFIX}/libexec --path .
 
-export LIBCLANG_PATH=${BUILD_PREFIX}/lib
-export RUSTONIG_DYNAMIC_LIBONIG=1
 mkdir -p ${PREFIX}/bin
 ls ${PREFIX}/libexec/bin | xargs -I % bash -c "ln -sf ${PREFIX}/libexec/bin/% ${PREFIX}/bin/u%"
