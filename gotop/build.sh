e1a010ccb0e361de1c36dbf2a32fe0ea44e242d1
danielnachun <daniel.nachun@gmail.com>
2023-08-17 21:47:11 -0700

    add gotop

--- gotop/build.sh
+++ gotop/build.sh
@@ -2,6 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-#export CGO_ENABLED=0
-export LDFLAGS="-s -w -X main.Version=${PKG_VERSION} -X main.BuildDate=$(date +%Y%m%dT%H%M%S)"
+export LDFLAGS="-s -w"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}" ./cmd/gotop
