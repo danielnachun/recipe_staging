f0fe31a008b23c4334098622710fab751e7dad6e
danielnachun <daniel.nachun@gmail.com>
2023-08-19 01:13:04 -0700

    fix rpaths

--- crystal/build.sh
+++ crystal/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export PATH="${PATH}:${SRC_DIR}/boot/bin"
 export LLVM_CONFIG=${BUILD_PREFIX}/bin/llvm_config
 export CRYSTAL_LIBRARY_PATH="${PREFIX}/lib:${CONDA_BUILD_SYSROOT}/usr/lib"
+sed -i "s/&& !flag?(:darwin)//" src/crystal/main.cr
 
 mkdir -p .build
 make install release=true FLAGS="--no-debug -D use_libiconv" interpreter=true PREFIX=${PREFIX}
--- crystalline/build.sh
+++ crystalline/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export LLVM_CONFIG=${BUILD_PREFIX}/bin/llvm-config
 export CRYSTAL_LIBRARY_PATH="${PREFIX}/lib:${CONDA_BUILD_SYSROOT}/usr/lib"
+export CRYSTAL_LIBRARY_RPATH="${PREFIX}/lib"
 
 shards install
 crystal build ./src/crystalline.cr \
