c67ff346ab4ab0960f9003a0ef582a4433056a5a
danielnachun <daniel.nachun@gmail.com>
2023-08-19 13:46:04 -0700

    fix test

--- lua-language-server/build.sh
+++ lua-language-server/build.sh
@@ -2,8 +2,6 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-export CXXFLAGS=-std=c++11
-
 pushd 3rd/luamake
     compile/build.sh 
 popd
@@ -11,8 +9,9 @@ popd
 
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}/bin 
-mkdir -p ${PREFIX}/libexec/log 
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}/log 
 mkdir -p ${PREFIX}/bin
+
 install -m 755 bin/lua-language-server ${PREFIX}/libexec/${PKG_NAME}/bin
 install -m 755 bin/main.lua ${PREFIX}/libexec/${PKG_NAME}/bin
 install -m 644 main.lua ${PREFIX}/libexec/${PKG_NAME}
@@ -20,4 +19,9 @@ install -m 644 debugger.lua ${PREFIX}/libexec/${PKG_NAME}
 cp -r locale ${PREFIX}/libexec/${PKG_NAME}
 cp -r meta ${PREFIX}/libexec/${PKG_NAME}
 cp -r script ${PREFIX}/libexec/${PKG_NAME}
-ln -sf ${PREFIX}/libexec/${PKG_NAME}/bin/lua-language-server ${PREFIX}/bin
+cp changelog.md ${PREFIX}/libexec/${PKG_NAME}
+
+tee ${PREFIX}/bin/${PKG_NAME} << EOF
+#!/bin/sh
+exec ${PREFIX}/libexec/${PKG_NAME}/bin/${PKG_NAME} "\$@"
+EOF
