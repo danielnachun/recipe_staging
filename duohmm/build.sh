e1506485308626d1be43779e11a1a3294643a271
danielnachun <daniel.nachun@gmail.com>
2023-08-28 18:19:51 -0700

    add duohmm

---
+++ duohmm/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export IFLAGS="${CXXFLAGS}"
+make CXX=${CXX} VERSION=${PKG_VERSION} LFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -lz -lboost_iostreams -lboost_program_options"
+
+mkdir -p ${PREFIX}/bin
+install -m 755 bin/duohmm ${PREFIX}/bin
