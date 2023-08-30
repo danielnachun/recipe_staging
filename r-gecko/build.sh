e8b40280de0ba33035ed39e5eea9609e1c37d737
danielnachun <daniel.nachun@gmail.com>
2023-08-30 00:51:47 -0700

    add r-gecko

--- r-gecko/build.sh
+++ r-gecko/build.sh
@@ -4,6 +4,6 @@ export DISABLE_AUTOBREW=1
 
 rm -rf src/*.o
 rm -rf src/*.dll
-sed -i 's/$(SHLIB_OPENMP_CXXFLAGS)/$(SHLIB_OPENMP_CXXFLAGS) -Wno-c++11-narrowing/' Makevars
+sed -i 's/$(SHLIB_OPENMP_CXXFLAGS)/$(SHLIB_OPENMP_CXXFLAGS) -Wno-c++11-narrowing/' src/Makevars
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
