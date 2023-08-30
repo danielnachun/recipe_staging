b4e05e4a9dbe68a5baf95b8d7a40740693139fe1
danielnachun <daniel.nachun@gmail.com>
2023-08-29 22:13:01 -0700

    add r-gecko

--- r-gecko/build.sh
+++ r-gecko/build.sh
@@ -5,4 +5,4 @@ export DISABLE_AUTOBREW=1
 rm -rf src/*.o
 rm -rf src/*.dll
 # shellcheck disable=SC2086
-${R} CMD INSTALL --build . ${R_ARGS}
+${R} CMD INSTALL --configure-args="CXXFLAGS=-Wno-c++11-narrowing" --build . ${R_ARGS}
