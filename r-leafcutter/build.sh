de39f16a7c3b12bc851e8765f9ee13efa16df55f
danielnachun <daniel.nachun@gmail.com>
2023-10-13 16:09:53 -0700

    update r-leafcutter

--- r-leafcutter/build.sh
+++ r-leafcutter/build.sh
@@ -5,3 +5,19 @@ sed -i 's/-DBOOST_DISABLE_ASSERTS/-DBOOST_DISABLE_ASSERTS -D_REENTRANT/' leafcut
 
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build leafcutter ${R_ARGS}
+
+pushd clustering
+    chmod -R +x *.py
+    chmod -R +x *.R
+    cp -r *.py ${PREFIX}/bin
+    cp -r *.R ${PREFIX}/bin
+popd
+
+pushd scripts
+    chmod -R +x *.py
+    chmod -R +x *.R
+    chmod -R +x *.pl
+    cp -r *.py ${PREFIX}/bin
+    cp -r *.R ${PREFIX}/bin
+    cp -r *.pl ${PREFIX}/bin
+popd
