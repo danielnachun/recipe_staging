e638a5bfbb50c3ee4790bafa81a6cbbfea930ede
Daniel Nachun <daniel.nachun@gmail.com>
2023-10-14 01:19:59 -0700

    ronn (#770)
    * fix names
    
    * update ronn

--- ronn/build.sh
+++ ronn/build.sh
@@ -2,12 +2,18 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-gem install -N -l -V --ignore-dependencies ronn-${PKG_VERSION}.gem --bindir=${PREFIX}/bin
-gem unpack ronn-${PKG_VERSION}.gem
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}/bin
+gem install -N -l -V --ignore-dependencies --bindir "${PREFIX}/libexec/${PKG_NAME}/bin" ${PKG_NAME}-${PKG_VERSION}.gem 
+gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
 
 mkdir -p ${PREFIX}/share/man/man1
 mkdir -p ${PREFIX}/share/man/man7
 ln -sf ${PREFIX}/share/rubygems/gems/ronn-${PKG_VERSION}/man/ronn.1 ${PREFIX}/share/man/man1
 ln -sf ${PREFIX}/share/rubygems/gems/ronn-${PKG_VERSION}/man/ronn-format.7 ${PREFIX}/share/man/man7
 
-sed -i "s?exec \"\$bindir/ruby\"?GEM_HOME=${PREFIX}/share/rubygems exec \"\$bindir/ruby\"?" ${PREFIX}/bin/ronn
+mkdir -p ${PREFIX}/bin
+sed -i "s?\$bindir?${PREFIX}/bin?g" ${PREFIX}/libexec/${PKG_NAME}/bin/${PKG_NAME}
+cat << EOF > ${PREFIX}/bin/${PKG_NAME}
+#!/bin/sh
+GEM_HOME="${PREFIX}/share/rubygems" exec "${PREFIX}/libexec/${PKG_NAME}/bin/${PKG_NAME}" "\$@"
+EOF
