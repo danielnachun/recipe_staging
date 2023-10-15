67a0d9119c05f7a83c8ec260cd5edc048d054858
danielnachun <daniel.nachun@gmail.com>
2023-10-15 15:36:33 -0700

    add fsharplint

--- fsharplint/build.sh
+++ fsharplint/build.sh
@@ -9,9 +9,18 @@ rm -rf global.json
 rm -rf paket.lock
 sed -i 's/net5.0/net7.0/g' src/FSharpLint.Console/FSharpLint.Console.fsproj
 sed -i 's/net5.0/net7.0/g' src/FSharpLint.Core/FSharpLint.Core.fsproj
-#dotnet tool restore
-dotnet fake build
-dotnet publish --no-self-contained src/FSharpLint.Console/FSharpLint.Console.fsproj --output ${PREFIX}/libexec/${PKG_NAME}
+sed -i 's/net5.0/net7.0/g' tests/FSharpLint.Benchmarks/FSharpLint.Benchmarks.fsproj
+sed -i 's/net5.0/net7.0/g' tests/FSharpLint.FunctionalTest/FSharpLint.FunctionalTest.fsproj
+sed -i 's/net5.0/net7.0/g' tests/FSharpLint.Core.Tests/FSharpLint.Core.Tests.fsproj
+sed -i 's/net5.0/net7.0/g' tests/FSharpLint.Console.Tests/FSharpLint.Console.Tests.fsproj
+sed -i 's/net5.0/net7.0/g' paket.dependencies
+sed -i 's/~> 0.53.0//' paket.dependencies
+mv .config/dotnet-tools.json .config/dotnet-tools.json.bak
+jq '.tools.paket.version = "7.2.1"' .config/dotnet-tools.json.bak > .config/dotnet-tools.json
+
+dotnet tool restore
+dotnet paket install
+dotnet publish --no-self-contained src/FSharpLint.Console/FSharpLint.Console.fsproj --output ${PREFIX}/libexec/${PKG_NAME} --framework net7.0
 
 tee ${PREFIX}/bin/dotnet-fsharplint << EOF
 #!/bin/sh
