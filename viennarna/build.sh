6050d1213452965b993a08762c53a99c27e38b1d
danielnachun <daniel.nachun@gmail.com>
2023-10-22 09:58:38 -0700

    add viennarna

---
+++ viennarna/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## Choose extra configure options depending on the operating system
+## (mac or linux)
+##
+if [ `uname` == Darwin ] ; then
+    extra_config_options="LDFLAGS=-Wl,-headerpad_max_install_names"
+fi
+
+export CXXFLAGS="-Wno-register"
+
+## Configure and make
+./configure --prefix=$PREFIX \
+            --with-kinwalker \
+            --with-cluster \
+            --disable-lto \
+            --without-doc \
+            --without-tutorial \
+            --without-tutorial-pdf \
+            --without-cla \
+            --without-rnaxplorer \
+            ${extra_config_options} \
+            && \
+make -j${CPU_COUNT}
+
+## Install
+make install
