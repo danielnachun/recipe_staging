f540e377698beeb2d6a15cb79daf88cc8b6ea43a
danielnachun <daniel.nachun@gmail.com>
2023-11-07 19:13:25 -0800

    fix phaser

--- phaser/build.sh
+++ phaser/build.sh
@@ -12,17 +12,24 @@ cp -r phaser_pop ${PREFIX}/libexec/${PKG_NAME}
 
 sed -i 's/95%/95%%/' ${PREFIX}/libexec/${PKG_NAME}/phaser_pop/phaser_cis_var.py
 
-chmod +x ${PREFIX}/libexec/${PKG_NAME}/phaser/phaser.py 
-chmod +x ${PREFIX}/libexec/${PKG_NAME}/phaser_annotate/phaser_annotate.py 
-chmod +x ${PREFIX}/libexec/${PKG_NAME}/phaser_gene_ae/phaser_gene_ae.py 
-chmod +x ${PREFIX}/libexec/${PKG_NAME}/phaser_pop/phaser_cis_var.py 
-chmod +x ${PREFIX}/libexec/${PKG_NAME}/phaser_pop/phaser_expr_matrix.py 
+sed -i 's?/home/bin/python2?/usr/bin/env python2?' ${PREFIX}/libexec/${PKG_NAME}/phaser/phaser.py
+sed -i '1i #!/usr/bin/env python2' ${PREFIX}/libexec/${PKG_NAME}/phaser_annotate/phaser_annotate.py
+sed -i '1i #!/usr/bin/env python2' ${PREFIX}/libexec/${PKG_NAME}/phaser_gene_ae/phaser_gene_ae.py
+sed -i '1i #!/usr/bin/env python2' ${PREFIX}/libexec/${PKG_NAME}/phaser_pop/phaser_cis_var.py
+sed -i '1i #!/usr/bin/env python2' ${PREFIX}/libexec/${PKG_NAME}/phaser_pop/phaser_expr_matrix.py
+
+chmod +x ${PREFIX}/libexec/${PKG_NAME}/phaser/phaser.py
+chmod +x ${PREFIX}/libexec/${PKG_NAME}/phaser_annotate/phaser_annotate.py
+chmod +x ${PREFIX}/libexec/${PKG_NAME}/phaser_gene_ae/phaser_gene_ae.py
+chmod +x ${PREFIX}/libexec/${PKG_NAME}/phaser_pop/phaser_cis_var.py
+chmod +x ${PREFIX}/libexec/${PKG_NAME}/phaser_pop/phaser_expr_matrix.py
 
 ln -sf ${PREFIX}/libexec/${PKG_NAME}/phaser/phaser.py ${PREFIX}/bin
 ln -sf ${PREFIX}/libexec/${PKG_NAME}/phaser_annotate/phaser_annotate.py ${PREFIX}/bin
 ln -sf ${PREFIX}/libexec/${PKG_NAME}/phaser_gene_ae/phaser_gene_ae.py ${PREFIX}/bin
 ln -sf ${PREFIX}/libexec/${PKG_NAME}/phaser_pop/phaser_cis_var.py ${PREFIX}/bin
 ln -sf ${PREFIX}/libexec/${PKG_NAME}/phaser_pop/phaser_expr_matrix.py ${PREFIX}/bin
+
 
 pushd phaser
     ${PYTHON} -m pip install . -vv
