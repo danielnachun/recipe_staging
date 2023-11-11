41931ac0425735ae2a29ec13e3985882f5e85a60
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-11 13:50:44 -0800

    sd 1.0.0 (#828)
    * sd 1.0.0
    
    * sd 1.0.0

--- sd/build.sh
+++ sd/build.sh
@@ -15,12 +15,11 @@ cargo install --locked --root ${PREFIX} --path .
 # remove extra build file
 rm -f "${PREFIX}/.crates.toml"
 
-fingerprint_dir=$(ls target/**/release/build | grep "sd\-" | head -n 1)
 mkdir -p ${PREFIX}/etc/bash_completion.d
 mkdir -p ${PREFIX}/share/fish/vendor_completions.d
 mkdir -p ${PREFIX}/share/zsh/site-functions
 mkdir -p ${PREFIX}/share/man/man1
-install -m 644 $(find . -name sd.bash) ${PREFIX}/etc/bash_completion.d/sd.bash
-install -m 644 $(find . -name sd.fish) ${PREFIX}/share/fish/vendor_completions.d/sd.fish
-install -m 644 $(find . -name _sd) ${PREFIX}/share/zsh/site-functions/_sd
-install -m 644 $(find . -name sd.1) ${PREFIX}/share/man/man1/sd.1
+install -m 644 gen/completions/sd.bash ${PREFIX}/etc/bash_completion.d/sd
+install -m 644 gen/completions/sd.fish ${PREFIX}/share/fish/vendor_completions.d/sd.fish
+install -m 644 gen/completions/_sd ${PREFIX}/share/zsh/site-functions/_sd
+install -m 644 gen/sd.1 ${PREFIX}/share/man/man1/sd.1
