e7c53d7030ae82ddc3a068a4a37bfb5eb84cd105
danielnachun <daniel.nachun@gmail.com>
2023-11-18 17:11:01 -0800

    update boot-clj

--- boot-clj/build.sh
+++ boot-clj/build.sh
@@ -7,7 +7,7 @@ mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 cp boot.jar ${PREFIX}/libexec/${PKG_NAME}
 
 tee ${PREFIX}/bin/boot << EOF
-#!/bin/sh
+#!/usr/bin/env bash
 declare -a "options=(\$BOOT_JVM_OPTIONS)"
 JAVA_HOME=${JAVA_HOME} exec "${JAVA_HOME}/bin/java" "\${options[@]}" -Dboot.app.path="${PREFIX}/bin/boot" -jar "${PREFIX}/libexec/${PKG_NAME}/boot.jar" "\$@"
 EOF
