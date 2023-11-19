c063a8a1e97176478306b0df99443850e5fc4d8b
danielnachun <daniel.nachun@gmail.com>
2023-11-18 16:14:25 -0800

    update abcl

--- abcl/build.sh
+++ abcl/build.sh
@@ -7,22 +7,10 @@ ant
 
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/abcl
-mkdir -p ${PREFIX}/etc/conda/activate.d
-mkdir -p ${PREFIX}/etc/conda/deactivate.d
 install -m 644 dist/abcl.jar ${PREFIX}/libexec/abcl
 install -m 644 dist/abcl-contrib.jar ${PREFIX}/libexec/abcl
 
 cat << EOF > ${PREFIX}/bin/abcl
 #!/bin/sh
-exec rlwrap java -cp ${PREFIX}/libexec/abcl/abcl.jar:"\$CLASSPATH" org.armedbear.lisp.Main "\$@"
-EOF
-
-cat << EOF > ${PREFIX}/etc/conda/activate.d/activate_abcl.sh
-#!/bin/sh
-export JAVA_HOME=${PREFIX}
-EOF
-
-cat << EOF > ${PREFIX}/etc/conda/deactivate.d/deactivate_abcl.sh
-#!/bin/sh
-unset JAVA_HOME
+exec rlwrap ${JAVA_HOME}/bin/java -cp ${PREFIX}/libexec/abcl/abcl.jar:"\$CLASSPATH" org.armedbear.lisp.Main "\$@"
 EOF
