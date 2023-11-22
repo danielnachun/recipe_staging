0f133ddc0545f78054676a94e23f88acac2d8470
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-22 10:03:28 -0800

    git-cliff: add back completions (#858)

--- git-cliff/build.sh
+++ git-cliff/build.sh
@@ -9,6 +9,13 @@ cargo-bundle-licenses \
 # build statically linked binary with Rust
 cargo install --locked --root ${PREFIX} --path ${PKG_NAME}
 
+export OUT_DIR=$SRC_DIR
+${PKG_NAME}-completions
+mkdir -p ${PREFIX}/share/fish/vendor_completions.d
+mkdir -p ${PREFIX}/share/zsh/site-functions
+install -m 644 ${PKG_NAME}.fish ${PREFIX}/share/fish/vendor_completions.d/${PKG_NAME}.fish
+install -m 644 _${PKG_NAME} ${PREFIX}/share/zsh/site-functions/_${PKG_NAME}
+
 # strip debug symbols
 "$STRIP" "$PREFIX/bin/${PKG_NAME}"
 
