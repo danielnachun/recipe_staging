6eae0f14a398703982448b24e99d16584f91338a
danielnachun <daniel.nachun@gmail.com>
2023-11-25 00:11:39 -0800

    add joker

--- joker/build.sh
+++ joker/build.sh
@@ -5,5 +5,6 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w -X main.version=v${PKG_VERSION}"
 
+go generate ./...
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
-go-licenses save . --save_path=license-files
+#go-licenses save . --save_path=license-files
