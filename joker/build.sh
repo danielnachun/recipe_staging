add2a32127eed51a88756935435864e6584c4fdd
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-25 10:41:20 -0800

    add joker (#874)
    * add joker
    
    * add joker

---
+++ joker/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CGO_ENABLED=0
+export LDFLAGS="-s -w -X main.version=v${PKG_VERSION}"
+
+go generate ./...
+go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
+#go-licenses save . --save_path=license-files
