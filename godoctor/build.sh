47293a7ee87140fda9bf967649bfdfa119cc0b7f
danielnachun <daniel.nachun@gmail.com>
2023-11-25 22:50:31 -0800

    add godoctor

---
+++ godoctor/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CGO_ENABLED=0
+export LDFLAGS="-s -w -X main.version=v${PKG_VERSION}"
+
+go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
+go-licenses save . --save_path=license-files
