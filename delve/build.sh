cfe1576bcf926ea68fd57261d26c218f2e0c7b9e
danielnachun <daniel.nachun@gmail.com>
2023-11-26 17:39:06 -0800

    update delve

--- delve/build.sh
+++ delve/build.sh
@@ -5,3 +5,4 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/dlv -ldflags="${LDFLAGS}" ./cmd/dlv
+go-licenses save ./cmd/dlv --save_path=license-files
