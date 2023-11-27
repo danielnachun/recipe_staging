c65bbb32583cca9838b696ca6221616ee563bf9d
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-26 20:02:28 -0800

    update glow-md (#990)

--- glow-md/build.sh
+++ glow-md/build.sh
@@ -5,3 +5,4 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w -X main.Version=${PKG_VERSION}"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/glow -ldflags="${LDFLAGS}"
+go-licenses save . --save_path=license-files --ignore github.com/mattn/go-localereader
