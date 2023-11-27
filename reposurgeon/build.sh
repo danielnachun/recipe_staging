21d204ffa3c679a47e68b1afb520bc23f40829c6
danielnachun <daniel.nachun@gmail.com>
2023-11-26 20:34:00 -0800

    update reposurgeon

--- reposurgeon/build.sh
+++ reposurgeon/build.sh
@@ -7,6 +7,10 @@ export XML_CATALOG_FILES=${PREFIX}/etc/xml/catalog
 
 make
 make install prefix=${PREFIX}
+go-licenses save ./cutter --save_path=license-files_repocutter --ignore github.com/termie/go-shutil
+go-licenses save ./mapper --save_path=license-files_repomapper --ignore github.com/termie/go-shutil
+go-licenses save ./surgeon --save_path=license-files_reposurgeon --ignore github.com/termie/go-shutil
+go-licenses save ./tool --save_path=license-files_repotool --ignore github.com/termie/go-shutil
 
 mkdir -p ${PREFIX}/share/emacs/site-lisp
 install -m 644 reposurgeon-mode.el ${PREFIX}/share/emacs/site-lisp/reposurgeon-mode.el
