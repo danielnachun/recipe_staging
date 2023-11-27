8a4712ddb09f70f6a9170e5a411374758677ab23
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-26 19:37:32 -0800

    update age (#959)

--- age/build.sh
+++ age/build.sh
@@ -7,6 +7,7 @@ export LDFLAGS="-s -w -X main.Version=v${PKG_VERSION}"
 mkdir -p ${PREFIX}/share/man/man1
 
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin -ldflags="${LDFLAGS}" "filippo.io/age/cmd/..."
+go-licenses save . --save_path=license-files
 
 install -m 644 doc/age.1 ${PREFIX}/share/man/man1/age.1
 install -m 644 doc/age-keygen.1 ${PREFIX}/share/man/man1/age-keygen.1
