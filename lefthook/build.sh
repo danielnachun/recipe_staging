fb675b29d0775b8d64522f07caf5ef19348c8515
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-26 19:41:38 -0800

    update lefthook (#1011)

--- lefthook/build.sh
+++ lefthook/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CGO_ENABLED=0
 go build -trimpath -buildmode=pie -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
+go-licenses save . --save_path=license-files
 
 mkdir -p ${PREFIX}/etc/bash_completion.d 
 mkdir -p ${PREFIX}/fish/vendor_completions.d 
