e4e6b3d3e30b1abbc44657bc215d7f256c825092
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-26 21:40:50 -0800

    update akita-cli (#1019)

--- akita-cli/build.sh
+++ akita-cli/build.sh
@@ -6,3 +6,4 @@ export CGO_ENABLED=1
 export GO_LDFLAGS="-s -w"
 export LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${GO_LDFLAGS}"
+go-licenses save . --save_path=license-files --ignore github.com/akitasoftware/plugin-flickr
