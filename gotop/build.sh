349731c8f47f1252f5320f241377455251ccd8d8
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-26 22:41:05 -0800

    update gotop (#1000)

--- gotop/build.sh
+++ gotop/build.sh
@@ -4,3 +4,11 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export LDFLAGS="-s -w"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}" ./cmd/gotop
+go-licenses save ./cmd/gotop --save_path=license-files \
+    --ignore github.com/xxxserxxx/gotop/v4 \
+    --ignore github.com/xxxserxxx/gotop/v4/colorschemes \
+    --ignore github.com/xxxserxxx/gotop/v4/devices \
+    --ignore github.com/xxxserxxx/gotop/v4/termui \
+    --ignore github.com/xxxserxxx/gotop/v4/utils \
+    --ignore github.com/xxxserxxx/gotop/v4/widgets \
+    --ignore github.com/xxxserxxx/gotop/v4/termui/drawille-go
