df15132760dc01b610888e983f107f35847f98db
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-26 20:02:49 -0800

    update gomuks (#999)

--- gomuks/build.sh
+++ gomuks/build.sh
@@ -5,3 +5,4 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
+go-licenses save . --save_path=license-files --ignore maunium.net/go/gomuks
