53097edcf12563cfc8eb9aab4e10c781e60a9881
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-26 21:18:57 -0800

    update periscope (#1026)

--- periscope/build.sh
+++ periscope/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w -X main.version=${PKG_VERSION}"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/psc -ldflags="${LDFLAGS}" ./cmd/psc
+#go-licenses save ./cmd/psc --save_path=license-files
 mkdir -p ${PREFIX}/etc/bash_completion.d 
 mkdir -p ${PREFIX}/share/fish/vendor_completions.d 
 mkdir -p ${PREFIX}/share/zsh/site-functions
