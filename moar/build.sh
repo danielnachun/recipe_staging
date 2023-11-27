3bac4940ab6cbbe2097689a7839b61636b73fe58
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-26 19:41:49 -0800

    update moar (#1017)

--- moar/build.sh
+++ moar/build.sh
@@ -5,5 +5,6 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w -X main.versionString=${PKG_VERSION}"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
+go-licenses save . --save_path=license-files
 mkdir -p ${PREFIX}/share/man/man1
 install -m 644 ${PKG_NAME}.1 ${PREFIX}/share/man/man1/${PKG_NAME}.1
