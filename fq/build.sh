10a6640ddc669190bcc55346eb0626eab84e92de
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-26 19:39:40 -0800

    update fq (#981)

--- fq/build.sh
+++ fq/build.sh
@@ -3,5 +3,5 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 export CGO_ENABLED=0
-LDFLAGS="-s -w"
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
+go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
+go-licenses save . --save_path=license-files
