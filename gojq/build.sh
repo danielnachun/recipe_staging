dafd9d69fbff843198f13f8d6a3900edcac338f0
danielnachun <daniel.nachun@gmail.com>
2023-11-26 18:53:48 -0800

    update gojq

--- gojq/build.sh
+++ gojq/build.sh
@@ -5,5 +5,6 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}" ./cmd/${PKG_NAME}
+go-licenses save . --save_path=license-files
 mkdir -p ${PREFIX}/share/zsh/site-functions
 install -m 644 _${PKG_NAME} ${PREFIX}/share/zsh/site-functions/_${PKG_NAME}
