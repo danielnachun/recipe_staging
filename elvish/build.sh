3456eb5ecf43f2f62383eaee3a6b82211fd06805
danielnachun <daniel.nachun@gmail.com>
2023-11-26 17:46:32 -0800

    update elvish

--- elvish/build.sh
+++ elvish/build.sh
@@ -5,3 +5,4 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w -X src.elv.sh/pkg/buildinfo.VersionSuffix="
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}" ./cmd/elvish
+go-licenses save ./cmd/elvish --save_path=license-files
