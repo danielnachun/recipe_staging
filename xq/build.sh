ef18b9d2428e2cd18634f9b5a91a5b5ecd7715a2
danielnachun <daniel.nachun@gmail.com>
2023-11-26 20:47:24 -0800

    update xq

--- xq/build.sh
+++ xq/build.sh
@@ -5,5 +5,6 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w -X main.version=${PKG_VERSION}"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
+go-licenses save . --save_path=license-files
 mkdir -p ${PREFIX}/share/man/man1
 install -m 644 docs/xq.man ${PREFIX}/share/man/man1/xq.1
