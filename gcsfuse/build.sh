1de0b06529ed1b58cdc1fa39a10a761c59682f8e
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-26 23:05:48 -0800

    update gcsfuse (#984)

--- gcsfuse/build.sh
+++ gcsfuse/build.sh
@@ -5,3 +5,4 @@ set -o xtrace -o nounset -o pipefail -o errexit
 sed -i "s/os.Mkdir(/os.MkdirAll(/g" tools/build_gcsfuse/main.go
 go build ./tools/build_gcsfuse
 ./build_gcsfuse ${SRC_DIR} ${PREFIX} ${PKG_VERSION}
+go-licenses save . --save_path=license-files
