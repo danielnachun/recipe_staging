ac300734a73c7649640e29f5ccb975608f0b0d48
danielnachun <daniel.nachun@gmail.com>
2023-11-26 18:55:14 -0800

    update golangci-lint

--- golangci-lint/build.sh
+++ golangci-lint/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w -X main.version=${PKG_VERSION}"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}" ./cmd/${PKG_NAME}
+#go-licenses save ./cmd/${PKG_NAME} --save_path=license-files
 mkdir -p ${PREFIX}/etc/bash_completion.d 
 mkdir -p ${PREFIX}/share/fish/vendor_completions.d 
 mkdir -p ${PREFIX}/share/zsh/site-functions
