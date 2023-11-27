0c375e98b658e52db187f06352a0c5c1513b416c
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-26 21:40:57 -0800

    update orgalorg (#1023)

--- orgalorg/build.sh
+++ orgalorg/build.sh
@@ -5,3 +5,9 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w -X main.version=${PKG_VERSION}"
 go build -mod=mod -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
+go-licenses save . --save_path=license-files \
+	--ignore github.com/kovetskiy/lorg \
+	--ignore github.com/reconquest/nopio-go \
+	--ignore github.com/reconquest/prefixwriter-go \
+	--ignore github.com/reconquest/runcmd \
+	--ignore github.com/zazab/zhash
