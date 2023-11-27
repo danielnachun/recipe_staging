0e56d4f3d119c7cd2107ae40bb4c8c4cc7a8874b
danielnachun <daniel.nachun@gmail.com>
2023-11-26 18:44:19 -0800

    update git-town

--- git-town/build.sh
+++ git-town/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w -X github.com/git-town/git-town/v7/src/cmd.version=v${PKG_VERSION}"
 go build -trimpath -buildmode=pie -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
+go-licenses save . --save_path=license-files
 
 mkdir -p ${PREFIX}/etc/bash_completion.d
 mkdir -p ${PREFIX}/fish/vendor_completions.d
