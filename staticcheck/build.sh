220cd9941dbc865da7d1ec3b597965b515c1f211
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-27 00:08:44 -0800

    update staticcheck (#1050)

--- staticcheck/build.sh
+++ staticcheck/build.sh
@@ -4,4 +4,5 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CGO_ENABLED=0
 export LDFLAGS="-s -w"
-go build -trimpath -buildmode=pie -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}" ./cmd/staticcheck
+go build -trimpath -buildmode=pie -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}" ./cmd/${PKG_NAME}
+go-licenses save ./cmd/${PKG_NAME} --save_path=license-files
