ca1da5436c5ee2e0356237c570232d742890cd7c
danielnachun <daniel.nachun@gmail.com>
2023-11-26 17:16:29 -0800

    update chezmoi

--- chezmoi/build.sh
+++ chezmoi/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w -X main.version=${PKG_VERSION}"
 go build -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
+go-licenses save . --save_path=license-files --ignore github.com/mattn/go-localereader
 mkdir -p ${PREFIX}/etc/bash_completion.d
 mkdir -p ${PREFIX}/share/fish/vendor_completions.d
 mkdir -p ${PREFIX}/share/zsh/site-functions
