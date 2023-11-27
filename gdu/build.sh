9ab10381e36fc2454d5e498dcd81de65798be1b7
danielnachun <daniel.nachun@gmail.com>
2023-11-26 18:41:47 -0800

    update gdu

--- gdu/build.sh
+++ gdu/build.sh
@@ -5,3 +5,4 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}" ./cmd/gdu
+go-licenses save ./cmd/gdu --save_path=license-files
