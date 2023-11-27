3f6f46bad3c0e827f98d46df3f94d6456e6cc3f8
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-26 20:02:12 -0800

    update akamai (#962)

--- akamai/build.sh
+++ akamai/build.sh
@@ -5,3 +5,4 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}" cli/main.go
+go-licenses save ./cli --save_path=license-files
