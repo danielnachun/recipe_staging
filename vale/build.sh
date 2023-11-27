35fd3cccaa7879b07d26f88afa8064985d38a1c9
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-26 21:41:29 -0800

    update vale (#1042)

--- vale/build.sh
+++ vale/build.sh
@@ -5,3 +5,4 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CGO_ENABLED=0
 export LDFLAGS="-s -w -X main.version=${PKG_VERSION}"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}" ./cmd/vale
+go-licenses save ./cmd/vale --save_path=license-files --ignore github.com/xi2/xz
