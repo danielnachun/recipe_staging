65a4b7612d13e883accac3da27f7ae25215cd57c
danielnachun <daniel.nachun@gmail.com>
2023-11-26 19:15:17 -0800

    update micro

--- micro/build.sh
+++ micro/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CGO_ENABLED=0
 make build-tags
+go-licenses save ./cmd/micro --save_path=license-files
 
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/share/man/man1
