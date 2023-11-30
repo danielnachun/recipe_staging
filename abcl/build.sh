3b834b385aac567b716466663b9d74d6ddf45ffc
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-30 10:24:29 -0800

    abcl: use tee (#1068)

--- abcl/build.sh
+++ abcl/build.sh
@@ -10,7 +10,7 @@ mkdir -p ${PREFIX}/libexec/abcl
 install -m 644 dist/abcl.jar ${PREFIX}/libexec/abcl
 install -m 644 dist/abcl-contrib.jar ${PREFIX}/libexec/abcl
 
-cat << EOF > ${PREFIX}/bin/abcl
+tee ${PREFIX}/bin/abcl << EOF
 #!/bin/sh
 exec rlwrap ${JAVA_HOME}/bin/java -cp ${PREFIX}/libexec/abcl/abcl.jar:"\$CLASSPATH" org.armedbear.lisp.Main "\$@"
 EOF
