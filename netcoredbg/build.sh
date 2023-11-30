8e5aab30117cad41e47af2b65aa19409edb3d6bc
Daniel Nachun <daniel.nachun@gmail.com>
2023-11-30 10:24:37 -0800

    netcoredbg: use tee (#1070)

--- netcoredbg/build.sh
+++ netcoredbg/build.sh
@@ -12,7 +12,7 @@ cmake --build build
 cmake --install build
 
 mkdir -p ${PREFIX}/bin
-cat << EOF > ${PREFIX}/bin/${PKG_NAME}
+tee ${PREFIX}/bin/${PKG_NAME} << EOF
 #!/bin/sh
 DOTNET_ROOT=${DOTNET_ROOT} exec ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME} "\$@"
 EOF
