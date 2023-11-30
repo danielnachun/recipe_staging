023cd0286cfa83efee3c8ccb7512680a957dceec
danielnachun <daniel.nachun@gmail.com>
2023-11-29 23:20:32 -0800

    reek: use tee

--- reek/build.sh
+++ reek/build.sh
@@ -8,13 +8,13 @@ gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
 
 mkdir -p ${PREFIX}/bin
 sed -i "s?\$bindir?${PREFIX}/bin?g" ${PREFIX}/libexec/${PKG_NAME}/bin/${PKG_NAME}
-cat << EOF > ${PREFIX}/bin/${PKG_NAME}
+tee ${PREFIX}/bin/${PKG_NAME} << EOF
 #!/bin/sh
 GEM_HOME="${PREFIX}/share/rubygems" exec "${PREFIX}/libexec/${PKG_NAME}/bin/${PKG_NAME}" "\$@"
 EOF
 
 sed -i "s?\$bindir?${PREFIX}/bin?g" ${PREFIX}/libexec/${PKG_NAME}/bin/code_climate_reek
-cat << EOF > ${PREFIX}/bin/code_climate_reek
+tee ${PREFIX}/bin/code_climate_reek << EOF
 #!/bin/sh
 GEM_HOME="${PREFIX}/share/rubygems" exec "${PREFIX}/libexec/${PKG_NAME}/bin/code_climate_reek" "\$@"
 EOF
