df028dc62fa780c616b8d1faf648c5a66247d60f
danielnachun <daniel.nachun@gmail.com>
2023-12-24 11:42:47 -0600

    dapars2: use better URL and test

--- dapars2/build.sh
+++ dapars2/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-install -Dm 755 "${SRC_DIR}/code/DaPars_Extract_Anno.py" "${PREFIX}/bin/DaPars_Extract_Anno.py"
-install -Dm 755 "${SRC_DIR}/code/Dapars2_Multi_Sample.py" "${PREFIX}/bin/Dapars2_Multi_Sample.py"
-install -Dm 755 "${SRC_DIR}/code/gtf2bed12.py" "${PREFIX}/bin/gtf2bed12.py"
+set -o xtrace -o nounset -o pipefail -o errexit
+
+mkdir -p ${PREFIX}/bin
+install -m 755 "${SRC_DIR}/code/DaPars_Extract_Anno.py" "${PREFIX}/bin/DaPars_Extract_Anno.py"
+install -m 755 "${SRC_DIR}/code/Dapars2_Multi_Sample.py" "${PREFIX}/bin/Dapars2_Multi_Sample.py"
+install -m 755 "${SRC_DIR}/code/gtf2bed12.py" "${PREFIX}/bin/gtf2bed12.py"
