eaf40c7a614a11629fe5e3e29a0f59fb863e83ff
Daniel Nachun <daniel.nachun@gmail.com>
2023-12-26 11:32:30 -0600

    ptwas: use better test and URL (#1261)

--- ptwas/build.sh
+++ ptwas/build.sh
@@ -2,8 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-sed -i "s?/usr/bin/?${PREFIX}/bin/?g" PTWAS_est/PTWAS_est
-sed -i "s?/usr/bin/?${PREFIX}/bin/?g" PTWAS_scan/make_GAMBIT_DB.R
+sed -i "s?/usr/bin/perl?/usr/bin/env perl?g" PTWAS_est/PTWAS_est
+sed -i "s?/usr/bin/Rscript?/usr/bin/env Rscript?g" PTWAS_scan/make_GAMBIT_DB.R
+sed -i "s/\r//g" PTWAS_scan/make_GAMBIT_DB.R
 
 mkdir -p ${PREFIX}/bin
 install -m 755 PTWAS_est/PTWAS_est ${PREFIX}/bin
