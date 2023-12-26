20fdbbc2790cc0be9aba5430578a35da5e59810e
Daniel Nachun <daniel.nachun@gmail.com>
2023-12-26 01:10:17 -0600

    gambit: use better URL (#1167)
    * gambit: use better URL
    
    * gambit: use better URL

--- gambit/build.sh
+++ gambit/build.sh
@@ -2,7 +2,8 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-make CXX="${CXX} -I${PREFIX}/include -L${PREFIX}/lib" CC="${CC} -Wno-overflow"
+sed -i "s/-flto//g" Makefile
+make CXX="${CXX} -I${PREFIX}/include" LDFLAGS="-L${PREFIX}/lib -lm -lz" CC="${CC} -Wno-overflow"
 
 mkdir -p ${PREFIX}/bin
 install -m 755 bin/GAMBIT ${PREFIX}/bin
