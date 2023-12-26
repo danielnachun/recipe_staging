a05047a2075f91efea036aa8e6ff56eebc37994e
Daniel Nachun <daniel.nachun@gmail.com>
2023-12-26 11:32:34 -0600

    paintor: use a better test (#1260)

--- paintor/build.sh
+++ paintor/build.sh
@@ -2,13 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cd nlopt-2.4.2
-./configure --prefix="$SRC_DIR"
-make
-make install
-
-cd ..
-make 
+sed -i 's?-I/$(curr)/eigen/Eigen??g' Makefile
+sed -i 's/causal_index\[i\]-1/static_cast<int>(causal_index[i]-1)/g' Functions_model.cpp
+make CC="${CXX} -I${PREFIX}/include/eigen3/Eigen -I${PREFIX}/include -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
 	
 mkdir -p ${PREFIX}/bin
 install -m 755 PAINTOR ${PREFIX}/bin
