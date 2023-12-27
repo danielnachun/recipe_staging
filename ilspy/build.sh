6408b086bf5b2389acd7cc442bb591510d93aed6
danielnachun <daniel.nachun@gmail.com>
2023-12-27 00:43:29 -0600

    ilspy 8.2

--- ilspy/build.sh
+++ ilspy/build.sh
@@ -6,25 +6,27 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
 rm global.json
-case ${target_platform} in 
-    osx-64 )
-        dotnet publish --no-self-contained ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime osx-x64
-        rm -rf bin/InstrumentationEngine/ubuntu ;;
-    osx-arm64 )
-        dotnet publish --no-self-contained ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime osx-arm64
-        rm -rf bin/InstrumentationEngine/ubuntu ;;
-    linux-64 )
-        dotnet publish --no-self-contained ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime linux-x64
-        rm -rf bin/InstrumentationEngine/macos ;;
-    linux-arm64 )
-        dotnet publish --no-self-contained ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime linux-arm64
-        rm -rf bin/InstrumentationEngine/macos ;;
-esac
+dotnet publish --no-self-contained ILSpy.XPlat.slnf -maxcpucount:1 --output ${PREFIX}/libexec/${PKG_NAME}
+#case ${target_platform} in 
+    #osx-64 )
+        #dotnet publish --no-self-contained ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime osx-x64
+        #rm -rf bin/InstrumentationEngine/ubuntu ;;
+    #osx-arm64 )
+        #dotnet publish --no-self-contained ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime osx-arm64
+        #rm -rf bin/InstrumentationEngine/ubuntu ;;
+    #linux-64 )
+        #dotnet publish --no-self-contained ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime linux-x64
+        #rm -rf bin/InstrumentationEngine/macos ;;
+    #linux-arm64 )
+        #dotnet publish --no-self-contained ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime linux-arm64
+        #rm -rf bin/InstrumentationEngine/macos ;;
+#esac
 
-rm -rf bin/InstrumentationEngine/alpine
-sed -i "s/net6.0/net7.0/" bin/ilspycmd.runtimeconfig.json
-sed -i "s/6.0.0/7.0.0/" bin/ilspycmd.runtimeconfig.json
-cp -r bin/* ${PREFIX}/libexec/${PKG_NAME}
+rm -rf ${PREFIX}/libexec/${PKG_NAME}/InstrumentationEngine/ubuntu
+rm -rf ${PREFIX}/libexec/${PKG_NAME}/InstrumentationEngine/macos
+rm -rf ${PREFIX}/libexec/${PKG_NAME}/InstrumentationEngine/alpine
+#sed -i "s/net6.0/net7.0/" ${PREFIX}/libexec/${PKG_NAME}/ilspycmd.runtimeconfig.json
+#sed -i "s/6.0.0/7.0.0/" ${PREFIX}/libexec/${PKG_NAME}/ilspycmd.runtimeconfig.json
 
 tee ${PREFIX}/bin/ilasm << EOF
 #!/bin/sh
