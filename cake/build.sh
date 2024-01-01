f6aebf946e4f1c26f26961ef32fe4b53bb107df3
danielnachun <daniel.nachun@gmail.com>
2024-01-01 12:55:25 -0600

    cake 4.0.0

--- cake/build.sh
+++ cake/build.sh
@@ -6,7 +6,8 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
 rm -rf global.json
-dotnet publish --no-self-contained src/Cake/Cake.csproj --output ${PREFIX}/libexec/${PKG_NAME} --framework net7.0
+framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
+dotnet publish --no-self-contained src/Cake/Cake.csproj --output ${PREFIX}/libexec/${PKG_NAME} --framework net${framework_version}
 
 tee ${PREFIX}/bin/cake << EOF
 #!/bin/sh
