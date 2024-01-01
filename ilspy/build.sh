c3ddcd809f43138fe59253df07cb3efd7b771bf5
Daniel Nachun <daniel.nachun@gmail.com>
2023-12-31 23:08:30 -0600

    ilspy 8.2 (#1269)
    * ilspy 8.2
    
    * ilspy 8.2

--- ilspy/build.sh
+++ ilspy/build.sh
@@ -6,35 +6,15 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
 rm global.json
-case ${target_platform} in 
-    osx-64 )
-        dotnet publish --no-self-contained ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime osx-x64
-        rm -rf bin/InstrumentationEngine/ubuntu ;;
-    osx-arm64 )
-        dotnet publish --no-self-contained ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime osx-arm64
-        rm -rf bin/InstrumentationEngine/ubuntu ;;
-    linux-64 )
-        dotnet publish --no-self-contained ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime linux-x64
-        rm -rf bin/InstrumentationEngine/macos ;;
-    linux-arm64 )
-        dotnet publish --no-self-contained ILSpy.XPlat.slnf -maxcpucount:1 --output bin --runtime linux-arm64
-        rm -rf bin/InstrumentationEngine/macos ;;
-esac
+dotnet publish --no-self-contained ICSharpCode.ILSpyCmd/ICSharpCode.ILSpyCmd.csproj --output ${PREFIX}/libexec/${PKG_NAME}
+dotnet publish --no-self-contained ICSharpCode.Decompiler/ICSharpCode.Decompiler.csproj --output ${PREFIX}/libexec/${PKG_NAME}
+dotnet publish --no-self-contained ICSharpCode.ILSpyX/ICSharpCode.ILSpyX.csproj --output ${PREFIX}/libexec/${PKG_NAME}
 
-rm -rf bin/InstrumentationEngine/alpine
-sed -i "s/net6.0/net7.0/" bin/ilspycmd.runtimeconfig.json
-sed -i "s/6.0.0/7.0.0/" bin/ilspycmd.runtimeconfig.json
-cp -r bin/* ${PREFIX}/libexec/${PKG_NAME}
-
-tee ${PREFIX}/bin/ilasm << EOF
-#!/bin/sh
-DOTNET_ROOT=${DOTNET_ROOT} exec ${PREFIX}/libexec/${PKG_NAME}/ilasm "\$@"
-EOF
-
-tee ${PREFIX}/bin/ildasm << EOF
-#!/bin/sh
-DOTNET_ROOT=${DOTNET_ROOT} exec ${PREFIX}/libexec/${PKG_NAME}/ildasm "\$@"
-EOF
+rm -rf ${PREFIX}/libexec/${PKG_NAME}/InstrumentationEngine/ubuntu
+rm -rf ${PREFIX}/libexec/${PKG_NAME}/InstrumentationEngine/macos
+rm -rf ${PREFIX}/libexec/${PKG_NAME}/InstrumentationEngine/alpine
+sed -i "s/net6.0/net8.0/" ${PREFIX}/libexec/${PKG_NAME}/ilspycmd.runtimeconfig.json
+sed -i "s/6.0.0/8.0.0/" ${PREFIX}/libexec/${PKG_NAME}/ilspycmd.runtimeconfig.json
 
 tee ${PREFIX}/bin/ilspycmd << EOF
 #!/bin/sh
