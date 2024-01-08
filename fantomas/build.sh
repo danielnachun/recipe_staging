07781e1526d975ab0691918013789b33debe4bf9
danielnachun <daniel.nachun@gmail.com>
2024-01-08 00:13:59 -0800

    fantomas: make noarch

--- fantomas/build.sh
+++ fantomas/build.sh
@@ -8,7 +8,8 @@ mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 rm -rf global.json
 dotnet publish --no-self-contained src/Fantomas/Fantomas.fsproj --output ${PREFIX}/libexec/${PKG_NAME}
 
+rm -rf ${PREFIX}/libexec/${PKG_NAME}/fantomas
 tee ${PREFIX}/bin/fantomas << EOF
 #!/bin/sh
-DOTNET_ROOT=${DOTNET_ROOT} exec ${PREFIX}/libexec/${PKG_NAME}/fantomas "\$@"
+DOTNET_ROOT=${DOTNET_ROOT} exec ${DOTNET_ROOT}/dotnet run ${PREFIX}/libexec/${PKG_NAME}/fantomas.dll "\$@"
 EOF
