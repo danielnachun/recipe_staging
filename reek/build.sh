695750db1a1548d937635cbd500868cc0f71dff8
danielnachun <daniel.nachun@gmail.com>
2024-01-08 23:32:57 -0800

    reek: make noarch

--- reek/build.sh
+++ reek/build.sh
@@ -2,19 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-mkdir -p ${PREFIX}/libexec/${PKG_NAME}/bin
-gem install -N -l -V --ignore-dependencies --bindir "${PREFIX}/libexec/${PKG_NAME}/bin" ${PKG_NAME}-${PKG_VERSION}.gem
+gem install -N -l -V --ignore-dependencies --bindir "${PREFIX}/bin" ${PKG_NAME}-${PKG_VERSION}.gem
 gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
-
-mkdir -p ${PREFIX}/bin
-sed -i "s?\$bindir?${PREFIX}/bin?g" ${PREFIX}/libexec/${PKG_NAME}/bin/${PKG_NAME}
-tee ${PREFIX}/bin/${PKG_NAME} << EOF
-#!/bin/sh
-GEM_HOME="${PREFIX}/share/rubygems" exec "${PREFIX}/libexec/${PKG_NAME}/bin/${PKG_NAME}" "\$@"
-EOF
-
-sed -i "s?\$bindir?${PREFIX}/bin?g" ${PREFIX}/libexec/${PKG_NAME}/bin/code_climate_reek
-tee ${PREFIX}/bin/code_climate_reek << EOF
-#!/bin/sh
-GEM_HOME="${PREFIX}/share/rubygems" exec "${PREFIX}/libexec/${PKG_NAME}/bin/code_climate_reek" "\$@"
-EOF
