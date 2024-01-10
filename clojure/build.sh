5ecb6b09c8a808bb10ee9cba7ddea2253b619303
Daniel Nachun <daniel.nachun@gmail.com>
2024-01-09 17:58:50 -0800

    clojure: make noarch (#1283)

--- clojure/build.sh
+++ clojure/build.sh
@@ -3,21 +3,9 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 mkdir -p ${PREFIX}/bin
-mkdir -p ${PREFIX}/libexec/${PKG_NAME}/bin
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
 sed -i "s?\${HOMEBREW_RUBY_PATH}?${BUILD_PREFIX}/bin/ruby?g" install.sh
 sed -i "s?libexec?libexec/${PKG_NAME}?g" install.sh
 sed -i "s?libexec?libexec/${PKG_NAME}?g" clojure
 ./install.sh ${PREFIX}
-mv ${PREFIX}/bin/clj ${PREFIX}/libexec/${PKG_NAME}/bin/clj
-mv ${PREFIX}/bin/clojure ${PREFIX}/libexec/${PKG_NAME}/bin/clojure
-
-tee ${PREFIX}/bin/clj << EOF
-#/bin/sh
-JAVA_HOME=${PREFIX}/lib/jvm exec ${PREFIX}/libexec/${PKG_NAME}/bin/clj "\$@"
-EOF
-
-tee ${PREFIX}/bin/clojure << EOF
-#/bin/sh
-JAVA_HOME=${PREFIX}/lib/jvm exec ${PREFIX}/libexec/${PKG_NAME}/bin/clojure "\$@"
-EOF
