e057ffa6a3fcfc5a92d60709a7ea76e4b0da45de
Daniel Nachun <daniel.nachun@gmail.com>
2024-01-09 21:13:05 -0800

    marksman: make noarch (#1337)

--- marksman/build.sh
+++ marksman/build.sh
@@ -12,5 +12,5 @@ mkdir -p ${PREFIX}/bin
 rm -rf ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME}
 tee ${PREFIX}/bin/${PKG_NAME} << EOF
 #!/bin/sh
-DOTNET_ROOT=${DOTNET_ROOT} exec ${DOTNET_ROOT}/dotnet run ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME}.dll "\$@"
+DOTNET_ROOT=${DOTNET_ROOT} exec ${DOTNET_ROOT}/dotnet exec ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME}.dll "\$@"
 EOF
