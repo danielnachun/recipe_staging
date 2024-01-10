5fe73afd73b47d28e88d3d5dbc635ad08b567e9e
Daniel Nachun <daniel.nachun@gmail.com>
2024-01-09 17:20:49 -0800

    aspera-cli: remove workaround (#1280)

--- aspera-cli/build.sh
+++ aspera-cli/build.sh
@@ -4,18 +4,4 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}/bin
 gem build ${PKG_NAME}.gemspec
-gem install -N -l -V --ignore-dependencies --bindir "${PREFIX}/libexec/${PKG_NAME}/bin" ${PKG_NAME}-${PKG_VERSION}.gem
-
-mkdir -p ${PREFIX}/bin
-sed -i "s?\$bindir?${PREFIX}/bin?g" ${PREFIX}/libexec/${PKG_NAME}/bin/ascli
-sed -i "s?\$bindir?${PREFIX}/bin?g" ${PREFIX}/libexec/${PKG_NAME}/bin/asession
-
-tee ${PREFIX}/bin/ascli << EOF
-#!/bin/sh
-GEM_HOME="${PREFIX}/share/rubygems" exec "${PREFIX}/libexec/${PKG_NAME}/bin/ascli" "\$@"
-EOF
-
-tee ${PREFIX}/bin/asession << EOF
-#!/bin/sh
-GEM_HOME="${PREFIX}/share/rubygems" exec "${PREFIX}/libexec/${PKG_NAME}/bin/asession" "\$@"
-EOF
+gem install -N -l -V --ignore-dependencies --bindir "${PREFIX}/bin" ${PKG_NAME}-${PKG_VERSION}.gem
