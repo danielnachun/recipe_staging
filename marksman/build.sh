e50c344217fbd1ae3b084f98e091e6fe13c1327b
Daniel Nachun <daniel.nachun@gmail.com>
2024-01-09 19:18:22 -0800

    marksman: make noarch (#1305)

--- marksman/build.sh
+++ marksman/build.sh
@@ -2,8 +2,6 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-export LD_LIBRARY_PATH="${PREFIX}/lib:${CONDA_BUILD_SYSROOT}/usr/lib"
-
 rm global.json
 dotnet tool restore
 
@@ -11,7 +9,8 @@ mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 dotnet publish --no-self-contained Marksman/Marksman.fsproj --output ${PREFIX}/libexec/${PKG_NAME}
 
 mkdir -p ${PREFIX}/bin
+rm -rf ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME}
 tee ${PREFIX}/bin/${PKG_NAME} << EOF
 #!/bin/sh
-DOTNET_ROOT=${DOTNET_ROOT} exec ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME} "\$@"
+DOTNET_ROOT=${DOTNET_ROOT} exec ${DOTNET_ROOT}/dotnet run ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME}.dll "\$@"
 EOF
