396a2dbf9a76c54666cfdd6a90ac6ee0f5d3fde0
Daniel Nachun <daniel.nachun@gmail.com>
2024-01-09 17:43:02 -0800

    csharpier: make noarch (#1286)

--- csharpier/build.sh
+++ csharpier/build.sh
@@ -9,7 +9,8 @@ rm -rf global.json
 framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
 dotnet publish --no-self-contained Src/CSharpier.Cli/CSharpier.Cli.csproj --output ${PREFIX}/libexec/${PKG_NAME} --framework net${framework_version}
 
+rm ${PREFIX}/libexec/${PKG_NAME}/dotnet-csharpier
 tee ${PREFIX}/bin/dotnet-csharpier << EOF
 #!/bin/sh
-DOTNET_ROOT=${DOTNET_ROOT} exec ${PREFIX}/libexec/${PKG_NAME}/dotnet-csharpier "\$@"
+DOTNET_ROOT=${DOTNET_ROOT} exec ${DOTNET_ROOT}/dotnet run ${PREFIX}/libexec/${PKG_NAME}/dotnet-csharpier.dll "\$@"
 EOF
