1dc9bf77e308d93a98af4feed97a227f435c13b7
danielnachun <daniel.nachun@gmail.com>
2024-01-18 17:35:22 -0800

    add dune

--- dune/build.sh
+++ dune/build.sh
@@ -6,8 +6,9 @@ make release
 make PREFIX=${PREFIX} install
 mkdir -p ${PREFIX}/share/emacs/site-lisp/${PKG_NAME}
 mv ${PREFIX}/man/man1/* ${PREFIX}/share/man/man1
+mv ${PREFIX}/man/man3/* ${PREFIX}/share/man/man3
+mv ${PREFIX}/man/man5/* ${PREFIX}/share/man/man5
 rm -rf ${PREFIX}/man/man1
-mv ${PREFIX}/man/man3 ${PREFIX}/share/man
-mv ${PREFIX}/man/man5 ${PREFIX}/share/man
-mv ${PREFIX}/man/man7 ${PREFIX}/share/man
+rm -rf ${PREFIX}/man/man3
+rm -rf ${PREFIX}/man/man5
 mv ${PREFIX}/share/emacs/site-lisp/*.el ${PREFIX}/share/emacs/site-lisp/${PKG_NAME}
