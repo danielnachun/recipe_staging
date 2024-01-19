a0c087bc1fe448342bcafbe098e4a087a15e03c7
Daniel Nachun <daniel.nachun@gmail.com>
2024-01-19 01:49:03 -0800

    add fend (#1384)
    * add fend
    
    * use newer syroot
    
    * add fend
    
    * add fend

---
+++ fend/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+cargo-bundle-licenses \
+    --format yaml \
+    --output THIRDPARTY.yml
+
+# build statically linked binary with Rust
+cargo install --locked --root ${PREFIX} --path cli
+./documentation/build.sh
+mkdir -p ${PREFIX}/share/man/man1
+install -m 644 documentation/fend.1 ${PREFIX}/share/man/man1/fend.1
+
+# strip debug symbols
+"$STRIP" "$PREFIX/bin/${PKG_NAME}"
+
+# remove extra build file
+rm -f "${PREFIX}/.crates.toml"
