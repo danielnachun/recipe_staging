a075ef9c86261a9f53ea67f05f59556c12be1c83
danielnachun <daniel.nachun@gmail.com>
2024-01-19 21:10:48 -0800

    add hadolint

--- hadolint/build.sh
+++ hadolint/build.sh
@@ -3,9 +3,8 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 cabal v2-update
-cabal v2-install -v3 \
+cabal --store-dir=${SRC_DIR}/.cabal v2-install \
     --extra-lib-dirs=${PREFIX}/lib \
-    --constraint "ShellCheck==0.9.0" \
     --allow-newer=base,Cabal,deepseq \
 	--jobs=${CPU_COUNT} \
 	--max-backjumps=100000 \
