f53762539e34066c2cf3e321f88762458304158a
danielnachun <daniel.nachun@gmail.com>
2024-01-27 11:46:03 -0800

    add xmlto

--- xmlto/build.sh
+++ xmlto/build.sh
@@ -3,6 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 export XML_CATALOG_FILES=${PREFIX}/etc/catalog
+export CFLAGS="${CFLAGS} -Wno-implicit-int"
 ./configure --disable-silent \
     --disable-dependency-tracking \
     --prefix=${PREFIX} \
