990c4d17e832e07ea193726c3967549f8f4fbc79
Daniel Nachun <daniel.nachun@gmail.com>
2024-02-02 20:54:09 -0800

    add lrzip (#1382)
    * add lrzip
    
    * add lrzip
    
    * add lrzip

---
+++ lrzip/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CFLAGS="-O2 -march=native -fomit-frame-pointer -I$PREFIX/include $CFLAGS"
+export CXXFLAGS="-O2 -march=native -fomit-frame-pointer -I$PREFIX/include $CXXFLAGS"
+
+if [[ ${target_platform} =~ .*osx.* ]]; then
+    sed -i 's?SUBDIRS = C ASM/x86?SUBDIRS = C?' lzma/Makefile.am
+    sed -i 's/-f elf64/-f macho64/' configure.ac
+fi
+autoreconf --force --install --verbose
+./configure --prefix=$PREFIX 
+make V=1 SHELL=bash
+make install
+
