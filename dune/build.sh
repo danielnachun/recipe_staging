728ace26d0c64eb107b2972695b12f855ff900cc
Daniel Nachun <daniel.nachun@gmail.com>
2024-02-08 10:36:54 -0800

    add dune (#1375)
    * add dune
    
    * add dune
    
    * add dune
    
    * add dune
    
    * add dune

---
+++ dune/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+make release
+make PREFIX=${PREFIX} install
+mkdir -p ${PREFIX}/share/emacs/site-lisp/${PKG_NAME}
+mkdir -p ${PREFIX}/share/man/man1
+mkdir -p ${PREFIX}/share/man/man3
+mkdir -p ${PREFIX}/share/man/man5
+mv ${PREFIX}/man/man1/* ${PREFIX}/share/man/man1
+mv ${PREFIX}/man/man3/* ${PREFIX}/share/man/man3
+mv ${PREFIX}/man/man5/* ${PREFIX}/share/man/man5
+rm -rf ${PREFIX}/man/man1
+rm -rf ${PREFIX}/man/man3
+rm -rf ${PREFIX}/man/man5
+mv ${PREFIX}/share/emacs/site-lisp/*.el ${PREFIX}/share/emacs/site-lisp/${PKG_NAME}
