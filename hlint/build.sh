73098a9bb95be9fb40360a3d7f2ecb2c5a8226f4
danielnachun <daniel.nachun@gmail.com>
2024-02-11 13:33:25 -0800

    add hlint

---
+++ hlint/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+cabal v2-update
+cabal --store-dir=${SRC_DIR}/.cabal v2-install \
+    --extra-lib-dirs=${PREFIX}/lib \
+	--jobs=${CPU_COUNT} \
+	--max-backjumps=100000 \
+	--install-method=copy \
+	--installdir=${PREFIX}/bin
