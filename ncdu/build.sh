db8b18cf301d7e7172d8cfa589fe8ef6e887ac92
danielnachun <daniel.nachun@gmail.com>
2024-02-11 11:10:07 -0800

    ncdu 2.3

--- ncdu/build.sh
+++ ncdu/build.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-zig build --prefix ${PREFIX} -Drelease-fast=true -Dcpu=core2 -Dtarget=native-native-gnu.2.17
+zig build --prefix ${PREFIX} -Dpie=true -Doptimize=ReleaseFast -Dcpu=core2
 mkdir -p ${PREFIX}/share/man/man1
 install -m 755 ncdu.1 ${PREFIX}/share/man/man1/ncdu.1
