4e07f386b1d1831b35637a62f0e7d19a80537fa9
danielnachun <daniel.nachun@gmail.com>
2024-02-12 15:56:57 -0800

    add alex

--- alex/build.sh
+++ alex/build.sh
@@ -4,20 +4,23 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export PATH=${SRC_DIR}/binary/bin:${PATH}
 
-cp src/Parser.y src/Parser.y.boot
-cp src/Scan.x src/Scan.x.boot
-
+cabal v2-update
 cabal v2-install \
 	alex \
+    happy \
 	--jobs=${CPU_COUNT} \
 	--max-backjumps=100000 \
 	--install-method=copy \
 	--installdir=${SRC_DIR}/binary/bin
 
+cd alex
+
 happy -agc src/Parser.y -o src/Parser.hs
 alex -g src/Scan.x -o src/Scan.hs
 
-cabal v2-update
+mv src/Parser.y src/Parser.y.boot
+mv src/Scan.x src/Scan.x.boot
+
 cabal --store-dir=${SRC_DIR}/.cabal v2-install \
     --extra-lib-dirs=${PREFIX}/lib \
 	--jobs=${CPU_COUNT} \
