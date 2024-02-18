baa1350750705c23e6d88510ca02a2b7700914ae
danielnachun <daniel.nachun@gmail.com>
2024-02-18 14:45:39 -0800

    ronn: make noarch

--- ronn/build.sh
+++ ronn/build.sh
@@ -2,18 +2,10 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-mkdir -p ${PREFIX}/libexec/${PKG_NAME}/bin
-gem install -N -l -V --ignore-dependencies --bindir "${PREFIX}/libexec/${PKG_NAME}/bin" ${PKG_NAME}-${PKG_VERSION}.gem 
+gem install -N -l -V --ignore-dependencies --bindir "${PREFIX}/bin" ${PKG_NAME}-${PKG_VERSION}.gem 
 gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
 
 mkdir -p ${PREFIX}/share/man/man1
 mkdir -p ${PREFIX}/share/man/man7
 ln -sf ${PREFIX}/share/rubygems/gems/ronn-${PKG_VERSION}/man/ronn.1 ${PREFIX}/share/man/man1
 ln -sf ${PREFIX}/share/rubygems/gems/ronn-${PKG_VERSION}/man/ronn-format.7 ${PREFIX}/share/man/man7
-
-mkdir -p ${PREFIX}/bin
-sed -i "s?\$bindir?${PREFIX}/bin?g" ${PREFIX}/libexec/${PKG_NAME}/bin/${PKG_NAME}
-tee ${PREFIX}/bin/${PKG_NAME} << EOF
-#!/bin/sh
-GEM_HOME="${PREFIX}/share/rubygems" exec "${PREFIX}/libexec/${PKG_NAME}/bin/${PKG_NAME}" "\$@"
-EOF
