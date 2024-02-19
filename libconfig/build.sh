27a0e0836ae9f5513fbdeda70356960cc15c2fea
danielnachun <daniel.nachun@gmail.com>
2024-02-19 01:57:13 -0800

    add libconfig

--- libconfig/build.sh
+++ libconfig/build.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-autoreconf -i
+set -o xtrace -o nounset -o pipefail -o errexit
+
+autoupdate
+autoreconf --force --install --verbose
 ./configure --prefix=$PREFIX
 make
 make check
