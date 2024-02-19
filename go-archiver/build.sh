7dd692ee5145ef6df1eb124c3a35bdd8d9d31ef2
danielnachun <daniel.nachun@gmail.com>
2024-02-19 15:57:38 -0800

    add go-archiver

--- go-archiver/build.sh
+++ go-archiver/build.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-export GOPATH="$SRC_DIR"
+#!/usr/bin/env bash
 
-cd src/github.com/mholt/archiver
-cd cmd/arc
-go get -v .
-go build
+set -o xtrace -o nounset -o pipefail -o errexit
 
-mkdir -p $PREFIX/bin
-mv arc $PREFIX/bin
+export CGO_ENABLED=0
+export LDFLAGS="-s -w"
+go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
+go-licenses save . --save_path=license-files --ignore github.com/xi2/xz
