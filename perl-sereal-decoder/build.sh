db25d98ce1631fda13dc19bc45369b4bda95e7a3
Daniel Nachun <daniel.nachun@gmail.com>
2024-02-20 15:36:05 -0800

    perl-sereal-decoder: remove unneeded workaround (#1517)
    * perl-sereal-decoder: remove unneeded workaround
    
    * perl-sereal-decoder: remove unneeded workaround

--- perl-sereal-decoder/build.sh
+++ perl-sereal-decoder/build.sh
@@ -4,7 +4,6 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export PERL_MM_USE_DEFAULT=1
 perl Makefile.PL INSTALLDIRS=site
-sed -i "s? ,${PREFIX}/lib??" Makefile
 make
 make test
 make install
