5f207184477b267c66d1b65de35a318d5b443ed3
danielnachun <daniel.nachun@gmail.com>
2024-02-20 17:41:35 -0800

    rb-fsevent: fix Apple Silicon

--- rb-fsevent/build.sh
+++ rb-fsevent/build.sh
@@ -6,7 +6,11 @@ gem install -N -l -V --ignore-dependencies ${PKG_NAME}-${PKG_VERSION}.gem
 gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
 if [[ ${target_platform} =~ .*osx.* ]]; then
     cd ${PREFIX}/share/rubygems/gems/${PKG_NAME}-${PKG_VERSION}/ext
-    MACOSX_DEPLOYMENT_TARGET=12.0 rake replace_exe
+    if [[ ${target_platform} == "osx-64" ]]; then
+        MACOSX_DEPLOYMENT_TARGET=12.0 rake replace_exe
+    else
+        rake replace_exe
+    fi
 else
     rm -rf ${PREFIX}/share/rubygems/gems/${PKG_NAME}-${PKG_VERSION}/ext
 fi
