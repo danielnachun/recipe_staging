f0a9fd443a9e39e63b0328caf5c0f4364f432ae4
danielnachun <daniel.nachun@gmail.com>
2024-02-22 14:51:35 -0800

    caviarbf: fix Apple Silicon build

--- caviarbf/build.sh
+++ caviarbf/build.sh
@@ -8,6 +8,7 @@ mkdir -p ${PREFIX}/bin
 install -m 755 caviarbf ${PREFIX}/bin
 install -m 755 model_search ${PREFIX}/bin
 
+export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
 cd caviarbf-r-package/caviarbf
 rm -rf src/*.o
 R CMD INSTALL .
