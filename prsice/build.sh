c7b741763e3f81ffc52b0eaa35c1fd80aabc1a61
danielnachun <daniel.nachun@gmail.com>
2024-02-22 12:34:16 -0800

    add prsice

---
+++ prsice/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+cmake -S . -B build -DEIGEN_INCLUDE_DIR=${PREFIX}/include/eigen3 -DCMAKE_VERBOSE_MAKEFILE=ON ${CMAKE_ARGS}
+cmake --build build -- -j ${CPU_COUNT}
+cmake --install build
+
+mkdir -p ${PREFIX}/bin
+install -m 755 ${SRC_DIR}/bin/PRSice ${PREFIX}/bin/PRSice
