967c6c8778909302c0fb993a71a5f3cf3420752f
danielnachun <daniel.nachun@gmail.com>
2024-02-23 23:10:40 -0800

    add r-nabor

--- r-nabor/build.sh
+++ r-nabor/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -o errexit -o pipefail
 export DISABLE_AUTOBREW=1
-export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
+sed -ie 's/PKG_CPPFLAGS =/PKG_CPPFLAGS = -DHAVE_WORKING_LOG1P/' src/Makevars
 ${R} CMD INSTALL --build .
