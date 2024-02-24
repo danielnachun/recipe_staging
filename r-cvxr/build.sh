5a18f1923e6c09b475c6ff3e7267f910e7c774bd
danielnachun <daniel.nachun@gmail.com>
2024-02-23 21:46:12 -0800

    add r-cvxr

--- r-cvxr/build.sh
+++ r-cvxr/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
-sed -ie 's/PKG_CPPFLAGS =/PKG_CPPFLAGS = -DHAVE_WORKING_LOG1P/' src/Makevars
+sed -ie 's/PKG_CPPFLAGS=/PKG_CPPFLAGS=-DHAVE_WORKING_LOG1P /' src/Makevars
 ${R} CMD INSTALL --build . ${R_ARGS}
