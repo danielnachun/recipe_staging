b23a25629654f1f3dc85d64aad7328bb3ff806e4
danielnachun <daniel.nachun@gmail.com>
2024-02-24 12:56:59 -0800

    add bioconductor-beachmat

--- bioconductor-beachmat/build.sh
+++ bioconductor-beachmat/build.sh
@@ -3,10 +3,10 @@ mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
 echo -e "CC=$CC
-PKG_CPPFLAGS=-DHAVE_WORKING_LOG1P
 FC=$FC
 CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
 CXX14=$CXX" > ~/.R/Makevars
+sed -ie 's/PKG_CPPFLAGS=/PKG_CPPFLAGS=-DHAVE_WORKING_LOG1P /' src/Makevars
 $R CMD INSTALL --build .
