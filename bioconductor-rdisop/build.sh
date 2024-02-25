5a498594a06adeaadd33d5c41b4adc66fdf2e357
danielnachun <daniel.nachun@gmail.com>
2024-02-24 16:30:13 -0800

    add bioconductor-rdisop

--- bioconductor-rdisop/build.sh
+++ bioconductor-rdisop/build.sh
@@ -8,4 +8,6 @@ CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
 CXX14=$CXX" > ~/.R/Makevars
+sed -i 's/unary_function/__unary_function/' src/imslib/src/ims/utils/compose_f_gx_t.h
+sed -i 's/binary_function/__binary_function/' src/imslib/src/ims/utils/compose_f_gx_hy_t.h
 $R CMD INSTALL --build .
