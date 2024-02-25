5248472deafffed730e9480fa5cc8f5e632bfe94
danielnachun <daniel.nachun@gmail.com>
2024-02-25 14:03:14 -0800

    finestructure: fix Apple Silicon

--- finestructure/build.sh
+++ finestructure/build.sh
@@ -4,6 +4,11 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CXXFLAGS="-std=c++03"
 
+# Don't use SSE on ARM
+if [[ ${target_platform} == "osx-arm64" ]];
+    sed -i 's/-mfpmath=sse -msse -msse2//' Makefile.am
+fi
+
 ./configure --disable-debug \
     --disable-dependency-tracking \
     --prefix=${PREFIX} \
