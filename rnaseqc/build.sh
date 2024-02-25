ed027b2e2706d70beb21233eb02dc1c4ae739205
danielnachun <daniel.nachun@gmail.com>
2024-02-25 14:56:30 -0800

    add rnaseqc

--- rnaseqc/build.sh
+++ rnaseqc/build.sh
@@ -5,15 +5,15 @@ set -o xtrace -o nounset -o pipefail -o errexit
 cd rnaseqc
 sed -i 's/CC/CXX/g' Makefile
 sed -i 's?./configure &&?./autogen.sh && ./configure &&?' Makefile
-sed -i "s?-lz?-L${PREFIX}/lib -lz?g" SeqLib/bwa/Makefile
-sed -i "s?-lz?-L${PREFIX}/lib -lz?g" SeqLib/fermi-lite/Makefile
+sed -i "s?-lz?-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -lz?g" SeqLib/bwa/Makefile
+sed -i "s?-lz?-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -lz?g" SeqLib/fermi-lite/Makefile
 make CXX="${CXX} -std=c++14" \
      CC="${CC} -fcommon" \
      STDLIB="-I${PREFIX}/include" \
      INCLUDES="-I${PREFIX}/include" \
-     LDFLAGS="-L${PREFIX}/lib" \
+     LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib" \
      CPPFLAGS="-I${PREFIX}/include" \
-     LIBRARY_PATHS="-L${PREFIX}/lib" || (cat SeqLib/config.log; exit 1)
+     LIBRARY_PATHS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib" || (cat SeqLib/config.log; exit 1)
 
 mkdir -p ${PREFIX}/bin
 install -m 755 rnaseqc ${PREFIX}/bin
