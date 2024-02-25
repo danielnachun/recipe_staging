458e91a19af829c373aee92da3cb4b069f94f837
danielnachun <daniel.nachun@gmail.com>
2024-02-24 16:51:08 -0800

    add bioconductor-rdisop

--- bioconductor-rdisop/build.sh
+++ bioconductor-rdisop/build.sh
@@ -8,7 +8,7 @@ CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
 CXX14=$CXX" > ~/.R/Makevars
-if [[ ${target_platform} == .*osx.* ]]; then
+if [[ ${target_platform} =~ .*osx.* ]]; then
     sed -i 's/unary_function/__unary_function/' src/imslib/src/ims/utils/compose_f_gx_t.h
     sed -i 's/binary_function/__binary_function/' src/imslib/src/ims/utils/compose_f_gx_hy_t.h
 fi
