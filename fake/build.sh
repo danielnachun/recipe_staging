9c4fe8fbccb74a8b3208fe41cfee0c3165cc2501
danielnachun <daniel.nachun@gmail.com>
2024-02-26 00:17:36 -0800

    fake: make noarch

--- fake/build.sh
+++ fake/build.sh
@@ -6,6 +6,7 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
 rm -rf global.json
+rm -rf src/app/fake-cli/bin/Debug
 dotnet tool restore
 dotnet paket restore
 dotnet publish --no-self-contained src/app/fake-cli/fake-cli.fsproj --output ${PREFIX}/libexec/${PKG_NAME}
@@ -15,3 +16,4 @@ tee ${PREFIX}/bin/fake-cli << EOF
 #!/bin/sh
 DOTNET_ROOT=${DOTNET_ROOT} exec ${DOTNET_ROOT}/dotnet exec ${PREFIX}/libexec/${PKG_NAME}/fake-cli.dll "\$@"
 EOF
+exit 1
