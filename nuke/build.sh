79488c37d2f50bf34bf06ae3f6da05d892f9e858
danielnachun <daniel.nachun@gmail.com>
2024-02-25 23:39:05 -0800

    nuke: make noarch

--- nuke/build.sh
+++ nuke/build.sh
@@ -8,7 +8,8 @@ mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 rm -rf global.json
 dotnet publish --no-self-contained "source/Nuke.GlobalTool/Nuke.GlobalTool.csproj" --output ${PREFIX}/libexec/${PKG_NAME}
 
+rm -rf ${PREFIX}/libexec/${PKG_NAME}/Nuke.GlobalTool
 tee ${PREFIX}/bin/nuke << EOF
 #!/bin/sh
-DOTNET_ROOT=${DOTNET_ROOT} exec ${PREFIX}/libexec/${PKG_NAME}/Nuke.GlobalTool "\$@"
+DOTNET_ROOT=${DOTNET_ROOT} exec ${PREFIX}/libexec/${PKG_NAME}/Nuke.GlobalTool.dll "\$@"
 EOF
