7de8af152873b1abb21df2cfc4a8124d1e241629
danielnachun <daniel.nachun@gmail.com>
2024-02-25 23:55:29 -0800

    cake: make noarch

--- cake/build.sh
+++ cake/build.sh
@@ -7,7 +7,7 @@ mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
 rm -rf global.json
 framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
-sed -i 's/6.7.0/6.8.0/g' src/Cake.NuGet/Cake.NuGet.csproj
+sed -i 's/6.7.0/6.8.1/g' src/Cake.NuGet/Cake.NuGet.csproj
 dotnet publish --no-self-contained src/Cake/Cake.csproj --output ${PREFIX}/libexec/${PKG_NAME} --framework net${framework_version}
 
 rm ${PREFIX}/libexec/${PKG_NAME}/Cake
