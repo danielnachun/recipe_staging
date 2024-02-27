607f2755ad0c941d4a1867fc5c981a819609f3df
danielnachun <daniel.nachun@gmail.com>
2024-02-26 18:59:58 -0800

    allow newer Guile

--- autogen/build.sh
+++ autogen/build.sh
@@ -12,10 +12,9 @@ fi
 sed -i 's/${CFLAGS} -D$d/${CFLAGS} ${LDFLAGS} ${LIBS} -D$d/' autoopts/test/defs.in
 sed -i 's/${CFLAGS} ${INC} -o ${testname}/${CFLAGS} ${LDFLAGS} ${LIBS} ${INC} -o ${testname}/' autoopts/test/library.test
 sed -i 's/CC="${CC} ${CFLAGS} ${INC}"/CC="${CC} ${CFLAGS} ${INC} ${LDFLAGS}"/' autoopts/test/getopt.test
-#sed -i 's/#elif GUILE_VERSION < 203000'
-sed -i 's/"2.2 2.0 1.8"/"3.0 2.2 2.0 1.8"/' configure
 
 autoreconf --force --install --verbose
+sed -i 's/"2.2 2.0 1.8"/"3.0 2.2 2.0 1.8"/' configure
 ./configure ac_cv_func_utimensat=no \
     --disable-debug \
     --disable-dependency-tracking \
