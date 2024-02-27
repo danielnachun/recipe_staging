cb657a879102ba99f06312acdc29f33a81068898
danielnachun <daniel.nachun@gmail.com>
2024-02-27 09:58:14 -0800

    dpr: include source code

--- dpr/build.sh
+++ dpr/build.sh
@@ -4,7 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 # Remake bin directory
 mkdir -p bin
-make CPP="${CXX} -I${PREFIX}/include -L${PREFIX}/lib -Wno-narrowing" FORCE_DYNAMIC=1
+make CPP="${CXX} -I${PREFIX}/include -I${PREFIX}/include/eigen3 -L${PREFIX}/lib -Wno-narrowing" FORCE_DYNAMIC=1
 
 mkdir -p ${PREFIX}/bin
 install -m 755 bin/DPR ${PREFIX}/bin
