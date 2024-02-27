797cd13711424c39e94481b0f9ed2d1c275ef8b5
Daniel Nachun <daniel.nachun@gmail.com>
2024-02-27 12:34:52 -0800

    sdpr: fix Apple Silicon build (#1761)
    * sdpr: fix Apple Silicon build
    
    * sdpr: fix Apple Silicon build

--- sdpr/build.sh
+++ sdpr/build.sh
@@ -10,7 +10,15 @@ if [[ ${target_platform} =~ .*osx.* ]]; then
     sed -i "s/--no-as-needed,//" Makefile
 fi
 
-make CC=${CXX}
+sed -i 's?x86intrin.h?simde/x86/avx512.h?' mcmc.cpp
+sed -i 's?xmmintrin.h?simde/x86/sse.h?' sse_mathfun.h
+sed -i 's?emmintrin.h?simde/x86/sse2.h?' sse_mathfun.h
+
+if [[ ${target_platform} == "osx-arm64" ]]; then
+    sed -i 's/-lmkl_rt/-lopenblas/' Makefile
+fi
+
+make CC="${CXX} -DSIMDE_ENABLE_NATIVE_ALIASES"
 
 mkdir -p ${PREFIX}/inb
 install -m 755 SDPR ${PREFIX}/bin
