128968c55306c021f1c6b3ebb3d6ad2fdaba2ff8
danielnachun <daniel.nachun@gmail.com>
2024-02-27 12:10:51 -0800

    sdpr: fix Apple Silicon build

--- sdpr/build.sh
+++ sdpr/build.sh
@@ -10,6 +10,14 @@ if [[ ${target_platform} =~ .*osx.* ]]; then
     sed -i "s/--no-as-needed,//" Makefile
 fi
 
+sed -i 's?x86intrin.h?simde/x86/avx512.h?' mcmc.cpp
+sed -i 's?xmmintrin.h?simde/x86/sse.h?' sse_mathfun.h
+sed -i 's?emmintrin.h?simde/x86/sse2.h?' sse_mathfun.h
+
+if [[ ${target_platform} == "osx-arm64" ]]; then
+    sed -i 's/-lmkl_rt/-lopenblas/' Makefile
+fi
+
 make CC=${CXX}
 
 mkdir -p ${PREFIX}/inb
