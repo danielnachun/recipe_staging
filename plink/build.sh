a99163247dccbbaaedc13ec1560d071c4b64fa8f
danielnachun <daniel.nachun@gmail.com>
2024-02-27 16:21:29 -0800

    add plink

--- plink/build.sh
+++ plink/build.sh
@@ -1,3 +1,9 @@
-# Install as plink
-mkdir -p $PREFIX/bin
-cp plink $PREFIX/bin/plink
+#!/usr/bin/env bash
+
+BLASFLAGS="-L${PREFIX}/lib"
+if [[ ${target_platform} == "osx-arm64"]]; then
+    BLASFLAGS="${BLASFLAGS} -lopenblas"
+else
+    BLASFLAGS="${BLASFLAGS} -lmkl_core -lmkl_intel_thread -lmkl_intel_lp64 -liomp5"
+fi
+make CXX=${CXX} CC=${CC} MACFLAGS="" ZLIB=${PREFIX}/lib/libz${SHLIB_EXT}
