c74e00a6f37d41f6832fe020bd4d140c36662fcd
danielnachun <daniel.nachun@gmail.com>
2024-02-27 23:30:22 -0800

    add plink

--- plink/build.sh
+++ plink/build.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-BLASFLAGS="-L${PREFIX}/lib"
-if [[ ${target_platform} == "osx-arm64"]]; then
-    BLASFLAGS="${BLASFLAGS} -lopenblas"
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export BLASFLAGS="-L${PREFIX}/lib"
+if [[ ${target_platform} == "osx-arm64" ]]; then
+    export BLASFLAGS="${BLASFLAGS} -lopenblas"
 else
-    BLASFLAGS="${BLASFLAGS} -lmkl_core -lmkl_intel_thread -lmkl_intel_lp64 -liomp5"
+    # Don't strip dylibs on macOS Intel
+    if [[ ${target_platform} == "osx-64" ]]; then
+        export LDFLAGS="$(echo "${LDFLAGS}" | sed -e 's/-Wl,-dead_strip_dylibs//')"
+    fi
+    export BLASFLAGS="${BLASFLAGS} -lmkl_core -lmkl_intel_thread -lmkl_intel_lp64 -liomp5"
 fi
-make CXX=${CXX} CC=${CC} MACFLAGS="" ZLIB=${PREFIX}/lib/libz${SHLIB_EXT}
+make CXX="${CXX} -DDYNAMIC_ZLIB -Wno-c++11-narrowing" CC="${CC} -DDYNAMIC_ZLIB" BLASFLAGS="${BLASFLAGS}" MACFLAGS="" ZLIB=${PREFIX}/lib/libz${SHLIB_EXT}
+
+mkdir -p ${PREFIX}/bin
+install -m 755 plink ${PREFIX}/bin
