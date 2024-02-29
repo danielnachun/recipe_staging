36f6dc5dfd147be9ea7a3ea23ee794f1cf2b109d
danielnachun <daniel.nachun@gmail.com>
2024-02-29 01:30:46 -0800

    prsice: fix Apple Silicon build

--- prsice/build.sh
+++ prsice/build.sh
@@ -3,6 +3,9 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 export CXXFLAGS="${CXXFLAGS} -DDYNAMIC_ZLIB"
+if [[ ${target_platform} == "osx-arm64" ]]; then
+    export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include/simde"
+fi
 cmake -S . -B build -DEIGEN_INCLUDE_DIR=${PREFIX}/include/eigen3 -DCMAKE_VERBOSE_MAKEFILE=ON ${CMAKE_ARGS}
 cmake --build build -- -j ${CPU_COUNT}
 cmake --install build
