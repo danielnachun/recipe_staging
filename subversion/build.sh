e3ebf31afc304b541ef3f09fb9fd37f074434d0f
danielnachun <daniel.nachun@gmail.com>
2024-03-01 00:27:07 -0800

    add subversion

--- subversion/build.sh
+++ subversion/build.sh
@@ -2,6 +2,7 @@
 set -ex
 
 export CFLAGS="${CFLAGS} -U__USE_XOPEN2K -std=c99"
+export LDFLAGS=$(echo ${LDFLAGS} | sed 's/-Wl,-dead_strip_dylibs//g')
 autoreconf --force --verbose --install
 
 ./configure \
@@ -11,12 +12,11 @@ autoreconf --force --verbose --install
   --with-sqlite="${PREFIX}" \
   --disable-static
 
-#make -j ${CPU_COUNT}
-make -j 1
+make -j ${CPU_COUNT}
 #make -j ${CPU_COUNT} check CLEANUP=true TESTS=subversion/tests/cmdline/basic_tests.py
 make install
 
-make --trace swig-pl-lib || cat Makefile
+make swig-pl-lib
 make install-swig-pl-lib
 pushd subversion/bindings/swig/perl/native
 ${PERL} Makefile.PL INSTALLDIRS=vendor NO_PERLLOCAL=1 NO_PACKLIST=1
