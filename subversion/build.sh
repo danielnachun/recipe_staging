d81f2919b87d369da0bfb4d696d80bf47e9a1a60
danielnachun <daniel.nachun@gmail.com>
2024-03-01 00:46:35 -0800

    add subversion

--- subversion/build.sh
+++ subversion/build.sh
@@ -16,9 +16,11 @@ make -j ${CPU_COUNT}
 #make -j ${CPU_COUNT} check CLEANUP=true TESTS=subversion/tests/cmdline/basic_tests.py
 make install
 
-make swig-pl-lib
-make install-swig-pl-lib
-pushd subversion/bindings/swig/perl/native
-${PERL} Makefile.PL INSTALLDIRS=vendor NO_PERLLOCAL=1 NO_PACKLIST=1
-make install
-popd
+if [[ ${target_platform} != "osx-arm64" ]]; then
+    make swig-pl-lib
+    make install-swig-pl-lib
+    pushd subversion/bindings/swig/perl/native
+    ${PERL} Makefile.PL INSTALLDIRS=vendor NO_PERLLOCAL=1 NO_PACKLIST=1
+    make install
+    popd
+fi
