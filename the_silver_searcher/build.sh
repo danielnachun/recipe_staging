2578923d5afce77e902f3c0a3a95c370cfba1814
Daniel Nachun <daniel.nachun@gmail.com>
2024-03-02 11:35:32 -0800

    add the_silver_searcher (#1489)
    * add the_silver_searcher
    
    * add cram
    
    * add the_silver_searcher
    
    * add the_silver_searcher

---
+++ the_silver_searcher/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+./autogen.sh
+# turn clang-format test off: otherwise Mac build gives
+#   fatal: not a git repository (or any of the parent directories): .git
+# and then hangs forever for some reason
+export CFLAGS="${CFLAGS} -fcommon"
+CLANG_FORMAT=no ./configure --prefix="${PREFIX}"
+make -j${CPU_COUNT}
+make test
+make -j${CPU_COUNT} install
