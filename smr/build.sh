f44f965d327d0aa73378f307f6bbf1fe3cb0a328
danielnachun <daniel.nachun@gmail.com>
2024-03-02 13:30:31 -0800

    smr: fix build

--- smr/build.sh
+++ smr/build.sh
@@ -4,7 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 cmake -S . -B build \
     -Dzlib_path=${PREFIX}/include \
-    -Deigen_path=${PREFIX}/include \
+    -Deigen_path=${PREFIX}/include/eigen3 \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
     -DCMAKE_BUILD_TYPE=Release \
