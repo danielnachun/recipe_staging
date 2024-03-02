723b98c4d7c424a5b38ddb3f5052f119546428d4
danielnachun <daniel.nachun@gmail.com>
2024-03-02 11:59:00 -0800

    add rb-fsevent

--- rb-fsevent/build.sh
+++ rb-fsevent/build.sh
@@ -7,8 +7,10 @@ gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
 if [[ ${target_platform} =~ .*osx.* ]]; then
     cd ${PREFIX}/share/rubygems/gems/${PKG_NAME}-${PKG_VERSION}/ext
     unset MACOSX_DEPLOYMENT_TARGET
+    if [[ ${target_platform} == "osx-arm64" ]]; then
+        export SDKROOT=/Applications/Xcode_15.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
+    fi
     rake replace_exe
-    #MACOSX_DEPLOYMENT_TARGET=$(xcrun --show-sdk-version) rake replace_exe
 else
     rm -rf ${PREFIX}/share/rubygems/gems/${PKG_NAME}-${PKG_VERSION}/ext
 fi
