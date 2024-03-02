c7bf3a1b1f6080b23855be3915585ca659126dde
danielnachun <daniel.nachun@gmail.com>
2024-03-02 15:53:44 -0800

    add rb-fsevent

--- rb-fsevent/build.sh
+++ rb-fsevent/build.sh
@@ -6,7 +6,7 @@ gem install -N -l -V --ignore-dependencies ${PKG_NAME}-${PKG_VERSION}.gem
 gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
 if [[ ${target_platform} =~ .*osx.* ]]; then
     cd ${PREFIX}/share/rubygems/gems/${PKG_NAME}-${PKG_VERSION}/ext
-    sed -i "s?\$SDK_INFO\['Path'\]?${CONDA_BUILD_SYSROOT}?" rakefile.rb
+    sed -i "s?\$SDK_INFO\['Path'\]?\"${CONDA_BUILD_SYSROOT}\"?" rakefile.rb
     sed -i '/$CFLAGS = "#{$CFLAGS} -mmacosx-version-min=#{$MACOSX_DEPLOYMENT_TARGET}"/d' rakefile.rb
     rake replace_exe
 else
