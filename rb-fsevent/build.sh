0abaf73d174283924b4d3b3371565096b53337fa
danielnachun <daniel.nachun@gmail.com>
2024-03-02 11:49:22 -0800

    add rb-fsevent

--- rb-fsevent/build.sh
+++ rb-fsevent/build.sh
@@ -6,7 +6,9 @@ gem install -N -l -V --ignore-dependencies ${PKG_NAME}-${PKG_VERSION}.gem
 gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
 if [[ ${target_platform} =~ .*osx.* ]]; then
     cd ${PREFIX}/share/rubygems/gems/${PKG_NAME}-${PKG_VERSION}/ext
-    MACOSX_DEPLOYMENT_TARGET=$(xcrun --show-sdk-version) rake replace_exe
+    unset MACOSX_DEPLOYMENT_TARGET
+    rake replace_exe
+    #MACOSX_DEPLOYMENT_TARGET=$(xcrun --show-sdk-version) rake replace_exe
 else
     rm -rf ${PREFIX}/share/rubygems/gems/${PKG_NAME}-${PKG_VERSION}/ext
 fi
