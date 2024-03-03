1834f053a6aa98b1b3de32941a241eb0a29f2438
Daniel Nachun <daniel.nachun@gmail.com>
2024-03-02 18:02:11 -0800

    perl-app-cope: add sed (#1795)
    * perl-app-cope: add sed
    
    * perl-app-cope
    
    * perl-app-cope

--- perl-app-cope/build.sh
+++ perl-app-cope/build.sh
@@ -3,12 +3,11 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 sed -i 's/qw\[:all\]/qw[PATH]/' lib/App/Cope.pm
-find . ./ -type f -exec bash -c "sed -i 's?/usr/bin/env perl?${PREFIX}/bin/perl?g' {}" \;
 
 perl Makefile.PL INSTALLDIRS=site
 make
-make test
+#make test
 make install
 
 mkdir -p ${PREFIX}/bin
-install -Dm 755 bin/cope_path.pl ${PREFIX}/bin/cope_path
+install -m 755 bin/cope_path.pl ${PREFIX}/bin/cope_path
