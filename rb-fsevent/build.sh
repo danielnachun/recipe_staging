ac0983cf13561100560bedba345aeced665af6ca
Daniel Nachun <daniel.nachun@gmail.com>
2024-03-02 16:08:54 -0800

    rb-fsevent: fix Apple Silicon (#1522)
    * rb-fsevent: fix Apple Silicon
    
    * add rb-fsevent
    
    * add rb-fsevent
    
    * add rb-fsevent
    
    * add rb-fsevent
    
    * add rb-fsevent
    
    * add rb-fsevent

--- rb-fsevent/build.sh
+++ rb-fsevent/build.sh
@@ -6,7 +6,9 @@ gem install -N -l -V --ignore-dependencies ${PKG_NAME}-${PKG_VERSION}.gem
 gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
 if [[ ${target_platform} =~ .*osx.* ]]; then
     cd ${PREFIX}/share/rubygems/gems/${PKG_NAME}-${PKG_VERSION}/ext
-    MACOSX_DEPLOYMENT_TARGET=12.0 rake replace_exe
+    sed -i "s?\$SDK_INFO\['Path'\]?\"${CONDA_BUILD_SYSROOT}\"?" rakefile.rb
+    sed -i '/$CFLAGS = "#{$CFLAGS} -mmacosx-version-min=#{$MACOSX_DEPLOYMENT_TARGET}"/d' rakefile.rb
+    rake replace_exe
 else
     rm -rf ${PREFIX}/share/rubygems/gems/${PKG_NAME}-${PKG_VERSION}/ext
 fi
