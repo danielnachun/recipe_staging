efea7d2c384a5c5a1fee0a410e4fa95ae094ea26
danielnachun <daniel.nachun@gmail.com>
2024-03-04 15:08:39 -0800

    luaformatter

--- luaformatter/build.sh
+++ luaformatter/build.sh
@@ -2,6 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+tar xzf luaformatter.tar.gz
+cd luaformatter
+
 cmake -S . -B build \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
