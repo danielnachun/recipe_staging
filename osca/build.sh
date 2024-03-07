3f9ac314e0f43a93656c5f652db65a45ed21eabb
danielnachun <daniel.nachun@gmail.com>
2024-03-07 10:23:44 -0800

    osca: support Apple Silicon

--- osca/build.sh
+++ osca/build.sh
@@ -4,15 +4,21 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 sed -i 's/PRIVATE  ${mkl_include_dir} PRIVATE ${rmath_lib_dir}/PRIVATE  ${mkl_include_dir} PRIVATE ${rmath_include_dir}/g' CMakeLists.txt
 
-if [[ ${target_platform} =~ .*osx.* ]]; then
+if [[ ${target_platform} == "osx-64" ]]; then
     sed -i 's/gnu_thread/intel_thread/g' CMakeLists.txt
+fi
+
+if [[ ${target_platform} != "osx-arm64" ]]; then
+    blas_arg = "-Dmkl_path=${PREFIX}"
+else
+    blas_arg = "-Dblas_path=${PREFIX}"
 fi
 
 cmake -S . -B build \
     -Dzlib_path=${PREFIX} \
     -Dgsl_path=${PREFIX} \
     -Deigen_path=${PREFIX}/include \
-    -Dmkl_path=${PREFIX} \
+    ${blas_arg} \
     -Drmath_lib_path=${PREFIX}/lib \
     -Drmath_include_path=${PREFIX}/lib/R/include \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
