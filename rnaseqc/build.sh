8c8672f3c6734452ab37a23a347abf1a8fd59e14
danielnachun <daniel.nachun@gmail.com>
2024-03-06 17:47:05 -0800

    rnaseqc: fix build

--- rnaseqc/build.sh
+++ rnaseqc/build.sh
@@ -2,18 +2,18 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cd rnaseqc
 sed -i 's/CC/CXX/g' Makefile
-sed -i 's?./configure &&?./autogen.sh && ./configure &&?' Makefile
-sed -i "s?-lz?-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -lz?g" SeqLib/bwa/Makefile
-sed -i "s?-lz?-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -lz?g" SeqLib/fermi-lite/Makefile
+#sed -i 's?./configure &&?./autogen.sh && ./configure &&?' Makefile
+#sed -i "s?-lz?-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -lz?g" SeqLib/bwa/Makefile
+#sed -i "s?-lz?-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -lz?g" SeqLib/fermi-lite/Makefile
+sed -i 's?$(foreach file,$(OBJECTS),$(SRCDIR)/$(file)) SeqLib/lib/libseqlib.a SeqLib/lib/libhts.a?$(foreach file,$(OBJECTS),$(SRCDIR)/$(file))?' Makefile
+sed -i 's?ar -rcs rnaseqc.a $^?$(CXX) -O3 $(LIBRARY_PATHS) -o librnaseqc$(SHLIB_EXT) $^ $(LIBS)?' Makefile
+sed -i 's/realloc(b->data, len)/(unsigned char*)realloc(b->data, len)/' htslib/cram/cram_io.h
+sed -i 's/-lpthread/-lpthread -lseqlib -lhts -lbwa/' Makefile
+
 make CXX="${CXX} -std=c++14" \
-     CC="${CC} -fcommon" \
-     STDLIB="-I${PREFIX}/include" \
-     INCLUDES="-I${PREFIX}/include" \
-     LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib" \
-     CPPFLAGS="-I${PREFIX}/include" \
-     LIBRARY_PATHS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib" || (cat SeqLib/config.log; exit 1)
+     INCLUDE_DIRS="-I${PREFIX}/include" \
+     LIBRARY_PATHS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
 
 mkdir -p ${PREFIX}/bin
 install -m 755 rnaseqc ${PREFIX}/bin
