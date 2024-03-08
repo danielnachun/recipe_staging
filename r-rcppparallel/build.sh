c08a7f20dedc49ffc49eefabe11723962e45098a
Daniel Nachun <daniel.nachun@gmail.com>
2024-03-07 21:28:11 -0800

    r-rcppparallel: fix Apple Silicon build (#1806)
    * r-rcppparallel: fix Apple Silicon build
    
    * r-rcppparallel: fix build on Apple Silicon
    
    * add r-rcppparallel

--- r-rcppparallel/build.sh
+++ r-rcppparallel/build.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash 
 
+export TBB_INC=${PREFIX}/include
+export TBB_LIB=${PREFIX}/lib
+export TBB_USE_SYMLINKS=FALSE
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
