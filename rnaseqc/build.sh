830c46b4aabc7225c1196fab854d5b4fa8cf7055
danielnachun <daniel.nachun@gmail.com>
2024-03-07 19:10:09 -0800

    rnaseqc: fix build

--- rnaseqc/build.sh
+++ rnaseqc/build.sh
@@ -9,7 +9,7 @@ sed -i 's/CC/CXX/g' Makefile
 sed -i 's?$(foreach file,$(OBJECTS),$(SRCDIR)/$(file)) SeqLib/lib/libseqlib.a SeqLib/lib/libhts.a?$(foreach file,$(OBJECTS),$(SRCDIR)/$(file))?' Makefile
 sed -i 's?ar -rcs rnaseqc.a $^?$(CXX) -O3 $(LIBRARY_PATHS) -o librnaseqc$(SHLIB_EXT) $^ $(LIBS)?' Makefile
 sed -i 's/realloc(b->data, len)/(unsigned char*)realloc(b->data, len)/' htslib/cram/cram_io.h
-sed -i 's/-lpthread/-lpthread -lseqlib -lhts -lbwa/' Makefile
+sed -i 's/-lpthread/-lpthread -lseqlib -lhts -lbwa -lfml/' Makefile
 
 make CXX="${CXX} -std=c++14" \
      INCLUDE_DIRS="-I${PREFIX}/include" \
