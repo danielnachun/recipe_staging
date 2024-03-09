435dfa5b66e1dc8f1438f307589624fad74531e9
danielnachun <daniel.nachun@gmail.com>
2024-03-08 19:00:20 -0800

    add r-rstan

--- r-rstan/build.sh
+++ r-rstan/build.sh
@@ -4,7 +4,7 @@
 sed -i.bak 's|STANHEADERS_SRC =.*|STANHEADERS_SRC = $$PREFIX/lib/R/library/StanHeaders/include/src|' src/Makevars
 
 export DISABLE_AUTOBREW=1
-sed -i "s?-DBOOST_DISABLE_ASSERTS?-DBOOST_DISABLE_ASSERTS -DHAVE_WORKING_LOG1P?" src/Makevars
+sed -i 's|-DBOOST_DISABLE_ASSERTS|-DBOOST_DISABLE_ASSERTS -DHAVE_WORKING_LOG1P|' src/Makevars
 
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
