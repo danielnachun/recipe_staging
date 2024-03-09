a8a53f68da8bf12506c24de77163eab0bd7ce841
danielnachun <daniel.nachun@gmail.com>
2024-03-08 18:47:19 -0800

    add r-rstan

--- r-rstan/build.sh
+++ r-rstan/build.sh
@@ -4,6 +4,7 @@
 sed -i.bak 's|STANHEADERS_SRC =.*|STANHEADERS_SRC = $$PREFIX/lib/R/library/StanHeaders/include/src|' src/Makevars
 
 export DISABLE_AUTOBREW=1
+export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
 
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
