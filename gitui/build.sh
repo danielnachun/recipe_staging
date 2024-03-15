d8167591661d9ce9175040d7fb5eed28b2e2431c
danielnachun <daniel.nachun@gmail.com>
2024-03-14 23:06:10 -0700

    add gitui

--- gitui/build.sh
+++ gitui/build.sh
@@ -1,16 +1,6 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-cargo-bundle-licenses \
-    --format yaml \
-    --output THIRDPARTY.yml
-
-# build statically linked binary with Rust
-cargo install --locked --root ${PREFIX} --path .
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/${PKG_NAME}"
-
-# remove extra build file
-rm -f "${PREFIX}/.crates.toml"
+set -ex
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+cargo build --release --locked
+cargo install --path . --root $PREFIX --locked
+rm $PREFIX/.crates2.json
+rm $PREFIX/.crates.toml
