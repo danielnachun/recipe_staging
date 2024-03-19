66111411c236750ba3e4385440a84688f6ce2731
danielnachun <daniel.nachun@gmail.com>
2024-03-19 11:19:26 -0700

    add r-rfast

--- r-rfast/build.sh
+++ r-rfast/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export DISABLE_AUTOBREW=1
+sed -ie 's/PKG_CPPFLAGS =/PKG_CPPFLAGS = -DHAVE_WORKING_LOG1P/' src/Makevars
 
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
