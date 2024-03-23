44bd2da6ded86e67e201b629f24e65292233ce4b
danielnachun <daniel.nachun@gmail.com>
2024-03-23 14:13:21 -0700

    add kallisto

--- kallisto/build.sh
+++ kallisto/build.sh
@@ -5,6 +5,7 @@ autoreconf --force --install --verbose
 ./configure
 cd ../.. || exit 1
 mkdir -p "${PREFIX}/bin"
+sed -i 's/ENABLE_AVX2 MATCHES "OFF"/CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64" AND ENABLE_AVX2 MATCHES "OFF"' ext/bifrost/CMakeLists.txt
 mkdir -p build
 cd build || exit 1
 cmake -DCMAKE_INSTALL_PREFIX:PATH="$PREFIX" .. -DUSE_HDF5=ON
