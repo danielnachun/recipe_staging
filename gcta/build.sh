fa91621786575315fef98cc58a1988dc9daa4db5
danielnachun <daniel.nachun@gmail.com>
2024-03-24 23:38:47 -0700

    add gcta

--- gcta/build.sh
+++ gcta/build.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-mkdir -p $PREFIX/bin
-cp gcta-1.94.1 $PREFIX/bin/gcta-1.94.1
-ln -s $PREFIX/bin/gcta-1.94.1 $PREFIX/bin/gcta
-ln -s $PREFIX/bin/gcta-1.94.1 $PREFIX/bin/gcta64
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export EIGEN3_INCLUDE_DIR="${PREFIX}/include/eigen3"
+export SPECTRA_LIB="${PREFIX}"
+export BOOST_LIB="${PREFIX}"
+export MKLROOT="${PREFIX}"
+
+cmake -S . -B build ${CMAKE_ARGS}
+cmake --build build
+cmake --install build
+
