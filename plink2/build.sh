86b98b5da11d6d47ce474016036da7a0705604b6
danielnachun <daniel.nachun@gmail.com>
2024-03-25 02:00:57 -0700

    add plink2

--- plink2/build.sh
+++ plink2/build.sh
@@ -8,13 +8,15 @@ sed -i 's|-L/usr/local/lib||' build_dynamic/Makefile
 export EXTRA_FLAGS=""
 if [[ ${target_platform} =~ .*linux.* ]]; then
     export EXTRA_FLAGS="${EXTRA_FLAGS} DYNAMIC_MKL=1"
-    export CFLAGS="${CFLAGS} USE_MKL=1"
-    export CXXFLAGS="${CXXFLAGS} USE_MKL=1"
+    export CFLAGS="${CFLAGS} -DUSE_MKL=1"
+    export CXXFLAGS="${CXXFLAGS} -DUSE_MKL=1"
 fi
 if [[ ${target_platform} == "osx-64" ]]; then
     export EXTRA_FLAGS="${EXTRA_FLAGS} NO_SSE42=1"
+    export CFLAGS="${CFLAGS} -DIGNORE_BUNDLED_SIMDE=1"
+    export CXXFLAGS="${CXXFLAGS} -DIGNORE_BUNDLED_SIMDE=1"
 fi
 cd build_dynamic
-make CXX="${CXX}" CC=${CC} CXXFLAGS="${CXXFLAGS} -I. -std=c++11" CFLAGS="${CFLAGS} -I. -O2 -std=gnu99" LINKFLAGS="${LDFLAGS} -lm -pthread -lzstd -lz" STATIC_ZSTD=""
+make CXX="${CXX}" CC=${CC} CXXFLAGS="${CXXFLAGS} -std=c++11" CFLAGS="${CFLAGS} -I. -O2 -std=gnu99" LINKFLAGS="${LDFLAGS} -lm -pthread -lzstd -lz" STATIC_ZSTD=""
 mkdir -p ${PREFIX}/bin
 install -m 755 plink2 ${PREFIX}/bin
