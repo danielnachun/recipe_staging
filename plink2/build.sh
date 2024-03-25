f5bf5476b24c6eb1725b1f532c51f1165692a696
danielnachun <daniel.nachun@gmail.com>
2024-03-25 00:06:54 -0700

    add plink2

--- plink2/build.sh
+++ plink2/build.sh
@@ -1,3 +1,8 @@
-# Install as plink2
-mkdir -p $PREFIX/bin
-cp plink2 $PREFIX/bin/plink2
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+sed -i 's|-I/opt/homebrew/include -I/usr/local/include||g' Makefile
+sed -i 's|-L/opt/homebrew/lib -L/usr/local/lib||g' Makefile
+
+make CXX=${CXX} CC=${CC} CXXFLAGS="${CXXFLAGS}" CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}"
