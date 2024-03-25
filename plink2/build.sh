9b13842c37301e1d1bf17d5b1ef66a61d2cbac86
danielnachun <daniel.nachun@gmail.com>
2024-03-25 10:28:47 -0700

    add plink2

--- plink2/build.sh
+++ plink2/build.sh
@@ -8,13 +8,13 @@ sed -i 's|-L/usr/local/lib||' build_dynamic/Makefile
 export EXTRA_FLAGS=""
 if [[ ${target_platform} =~ .*linux.* ]]; then
     export EXTRA_FLAGS="${EXTRA_FLAGS} DYNAMIC_MKL=1"
-    export CFLAGS="${CFLAGS} -DUSE_MKL=1"
-    export CXXFLAGS="${CXXFLAGS} -DUSE_MKL=1"
+    export CFLAGS="${CFLAGS} -DUSE_MKL -DLAPACK_ILP64"
+    export CXXFLAGS="${CXXFLAGS} -DUSE_MKL -DLAPACK_ILP64"
 fi
 if [[ ${target_platform} == "osx-arm64" ]]; then
     export EXTRA_FLAGS="${EXTRA_FLAGS} NO_SSE42=1"
-    export CFLAGS="${CFLAGS} -DIGNORE_BUNDLED_SIMDE=1"
-    export CXXFLAGS="${CXXFLAGS} -DIGNORE_BUNDLED_SIMDE=1"
+    export CFLAGS="${CFLAGS} -DIGNORE_BUNDLED_SIMDE"
+    export CXXFLAGS="${CXXFLAGS} -DIGNORE_BUNDLED_SIMDE"
 fi
 cd build_dynamic
 make CXX="${CXX}" CC=${CC} CXXFLAGS="${CXXFLAGS} -std=c++11" CFLAGS="${CFLAGS} -I. -O2 -std=gnu99" LINKFLAGS="${LDFLAGS} -lm -pthread -lzstd -lz" STATIC_ZSTD=""
