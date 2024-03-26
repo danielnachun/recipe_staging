135582e5a1d241b5c915bc328224d1e1ec6705db
danielnachun <daniel.nachun@gmail.com>
2024-03-26 13:11:19 -0700

    add gen3-client

---
+++ gen3-client/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CGO_ENABLED=0
+export LDFLAGS="-s -w"
+go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
+go-licenses save . --save_path=license-files
