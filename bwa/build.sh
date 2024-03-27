0f147bdb5d7b87cf28a4bf8b1f80feb283f37d05
danielnachun <daniel.nachun@gmail.com>
2024-03-27 12:03:43 -0700

    add bwa

--- bwa/build.sh
+++ bwa/build.sh
@@ -3,6 +3,11 @@
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
+if [[ ${target_platform} == "osx-arm64" ]]; then
+    sed -i 's?emmintrin.h?simde/x86/sse2.h?' ksw.c
+    export CFLAGS="${CFLAGS} -DSIMDE_ENABLE_NATIVE_ALIASES"
+fi
+
 make CFLAGS="${CFLAGS} -fcommon"
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/share/man/man1
