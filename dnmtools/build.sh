0dd57610c8a5228e8d17596e4b405fd6d144ee3d
danielnachun <daniel.nachun@gmail.com>
2024-03-27 17:47:15 -0700

    add dnmtools

--- dnmtools/build.sh
+++ dnmtools/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -rf src/abismal/src/popcnt.hpp
+ln -sf ${PREFIX}/include/popcnt.hpp src/abismal/src
+
 ./configure --prefix=$PREFIX
 make CXXFLAGS="-O3 -DNDEBUG -D_LIBCPP_DISABLE_AVAILABILITY"
 make install
