eaf7fb75642795b8af4626c6f4bc356872d8c358
danielnachun <daniel.nachun@gmail.com>
2024-03-27 18:07:57 -0700

    add dnmtools

--- dnmtools/build.sh
+++ dnmtools/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf src/abismal/src/popcnt.hpp
-cp ${PREFIX}/include/popcnt.hpp src/abismal/src
+cp ${PREFIX}/include/libpopcnt.h src/abismal/src/popcnt.hpp
 
 ./configure --prefix=$PREFIX
 make CXXFLAGS="-O3 -DNDEBUG -D_LIBCPP_DISABLE_AVAILABILITY"
