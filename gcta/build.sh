cc4e5f88c4c18b87fa91e796ce105823f570acde
danielnachun <daniel.nachun@gmail.com>
2024-03-29 22:06:59 -0700

    add gcta

--- gcta/build.sh
+++ gcta/build.sh
@@ -9,10 +9,12 @@ export MKLROOT="${PREFIX}"
 export LDFLAGS="${LDFLAGS} -lgsl"
 export LDFLAGS=$(echo $LDFLAGS | sed 's/-Wl,-dead_strip_dylibs//g')
 
-sed -i 's/CMAKE_EXE_LINKER_FLAGS "/CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}/g' CMakeLists.txt
+sed -i 's/CMAKE_EXE_LINKER_FLAGS "/CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /g' CMakeLists.txt
 sed -i 's/zstd_s/zstd/' CMakeLists.txt
 
 cmake -S . -B build ${CMAKE_ARGS}
 cmake --build build
 cmake --install build
 
+mkdir -p ${PREFIX}/bin
+install -m 755 build/gcta64 ${PREFIX}/bin/gcta
