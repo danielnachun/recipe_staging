03267a68c95adcfc18ebdc47f694167f3d7fae1c
danielnachun <daniel.nachun@gmail.com>
2024-03-29 22:45:37 -0700

    add gcta

--- gcta/build.sh
+++ gcta/build.sh
@@ -11,6 +11,10 @@ export LDFLAGS=$(echo $LDFLAGS | sed 's/-Wl,-dead_strip_dylibs//g')
 
 sed -i 's/CMAKE_EXE_LINKER_FLAGS "/CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /g' CMakeLists.txt
 sed -i 's/zstd_s/zstd/' CMakeLists.txt
+sed -i '?link_directories(/usr/local/lib)?d' CMakeLists.txt
+if [[ ${target_platform} == "osx-arm64" ]]; then
+    sed -i 's/"aarch64"/"arm64"/' CMakelists.txt
+fi
 
 cmake -S . -B build ${CMAKE_ARGS}
 cmake --build build
