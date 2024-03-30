42a601d067acbf71d150a73653a6e7f69b046ced
danielnachun <daniel.nachun@gmail.com>
2024-03-29 23:12:43 -0700

    add gcta

--- gcta/build.sh
+++ gcta/build.sh
@@ -5,7 +5,6 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export EIGEN3_INCLUDE_DIR="${PREFIX}/include/eigen3"
 export SPECTRA_LIB="${PREFIX}"
 export BOOST_LIB="${PREFIX}"
-export MKLROOT="${PREFIX}"
 export LDFLAGS="${LDFLAGS} -lgsl"
 export LDFLAGS=$(echo $LDFLAGS | sed 's/-Wl,-dead_strip_dylibs//g')
 
@@ -14,6 +13,9 @@ sed -i 's/zstd_s/zstd/' CMakeLists.txt
 sed -i '/link_directories(\/usr\/local\/lib)/d' CMakeLists.txt
 if [[ ${target_platform} == "osx-arm64" ]]; then
     sed -i 's/"aarch64"/"arm64"/' CMakelists.txt
+    export OPENBLAS="${PREFIX}"
+else
+    export MKLROOT="${PREFIX}"
 fi
 
 cmake -S . -B build ${CMAKE_ARGS}
