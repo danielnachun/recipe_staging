5b9142d5a5725d0ec73a8af666d84d64e4b2d2eb
danielnachun <daniel.nachun@gmail.com>
2024-04-10 17:55:38 -0700

    add plinkseq

--- plinkseq/build.sh
+++ plinkseq/build.sh
@@ -2,17 +2,32 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+# Devendor sqlite
+rm sources/plinkseq/sources/lib/sqlite3.c
+rm sources/plinkseq/sources/include/plinkseq/sqlite3.h
+
+# Devendor protobuf
 rm -rf sources/ext
-sed -i 's/double_t/Double_t/g; s/int_t/Int_t/g; s/bool_t/Bool_t/g' sources/plinkseq/sources/lib/matrix.proto
-protobuf --proto_path=sources/plinkseq/sources/lib --cpp_out=sources/plinkseq/sources/lib
+rm sources/plinkseq/sources/include/plinkseq/matrix.pb.h
+rm sources/plinkseq/sources/include/plinkseq/variant.pb.h
+rm sources/plinkseq/sources/lib/matrix.pb.cpp
+rm sources/plinkseq/sources/lib/variant.pb.cpp
+
+#sed -i 's/double_t/Double_t/g; s/int_t/Int_t/g; s/bool_t/Bool_t/g' sources/plinkseq/sources/lib/matrix.proto
+protoc --proto_path=sources/plinkseq/sources/lib --cpp_out=sources/plinkseq/sources/lib sources/plinkseq/sources/lib/matrix.proto
+protoc --proto_path=sources/plinkseq/sources/lib --cpp_out=sources/plinkseq/sources/lib sources/plinkseq/sources/lib/variant.proto
 mv sources/plinkseq/sources/lib/matrix.pb.cc sources/plinkseq/sources/lib/matrix.pb.cpp
 mv sources/plinkseq/sources/lib/variant.pb.cc sources/plinkseq/sources/lib/variant.pb.cpp
+cp sources/plinkseq/sources/lib/matrix.pb.h sources/plinkseq/sources/include/plinkseq
+cp sources/plinkseq/sources/lib/variant.pb.h sources/plinkseq/sources/include/plinkseq
+ln -sf ${PREFIX}/include/sqlite3.h sources/plinkseq/sources/include/plinkseq
+
 #sed -i 's/double_t/Double_t/g; s/int_t/Int_t/g; s/bool_t/Bool_t/g' sources/plinkseq/sources/include/plinkseq/matrix.pb.h
 #sed -i 's/double_t/Double_t/g; s/int_t/Int_t/g; s/bool_t/Bool_t/g' sources/plinkseq/sources/lib/matrix.pb.cpp
-sed -i 's/double_t/Double_t/g' sources/plinkseq/sources/lib/matdb.cpp
-sed -i 's/( i_hi < 0 )/( *i_hi < 0 )/' sources/plinkseq/sources/lib/r8lib.cpp
-sed -i 's/$(PROTOBUF_LIB_FULL_PATH)//g' Makefile
-sed -i 's/-lz/-lprotobuf -labsl_log_internal_message -labsl_log_internal_check_op -lz /g' Makefile
+#sed -i 's/double_t/Double_t/g' sources/plinkseq/sources/lib/matdb.cpp
+#sed -i 's/( i_hi < 0 )/( *i_hi < 0 )/' sources/plinkseq/sources/lib/r8lib.cpp
+#sed -i 's/$(PROTOBUF_LIB_FULL_PATH)//g' Makefile
+#sed -i 's/-lz/-lprotobuf -labsl_log_internal_message -labsl_log_internal_check_op -lsqlite3 -lz /g' Makefile
 make CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -Wno-register" STATIC_FLAG="${LDFLAGS}"
 
 mkdir -p ${PREFIX}/bin
