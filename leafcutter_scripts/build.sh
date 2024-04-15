67ba9c9edd9400665c091b90f236abef785d21bf
Daniel Nachun <daniel.nachun@gmail.com>
2024-04-15 01:16:24 -0700

    update leafcutter_scripts (#1991)

--- leafcutter_scripts/build.sh
+++ leafcutter_scripts/build.sh
@@ -4,16 +4,12 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 pushd clustering
     chmod -R +x *.py
-    chmod -R +x *.R
     cp -r *.py ${PREFIX}/bin
-    cp -r *.R ${PREFIX}/bin
 popd
 
 pushd scripts
     chmod -R +x *.py
-    chmod -R +x *.R
     chmod -R +x *.pl
     cp -r *.py ${PREFIX}/bin
-    cp -r *.R ${PREFIX}/bin
     cp -r *.pl ${PREFIX}/bin
 popd
