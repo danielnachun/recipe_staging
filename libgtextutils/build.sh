9a0c42397aa529df644be57f091cc216217ca766
Daniel Nachun <daniel.nachun@gmail.com>
2024-04-17 11:10:06 -0700

    add libgtextutils (#2009)
    * add libgtextutils
    
    * add libgtexutils

---
+++ libgtextutils/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#if [ "$(uname)" == "Darwin" ]; then
+    ## building the library requires an implementation of basic_stringbuf
+    ## from the c++ standard library
+    ## 10.9 seems to be the minimum possible deployment target
+    #MACOSX_DEPLOYMENT_TARGET=10.9
+#fi
+
+autoreconf --force --verbose --install
+./configure --prefix=$PREFIX
+make
+make install
