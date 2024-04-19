f821673ac39061b2bf18a48be1ca6d683c8c10f6
danielnachun <daniel.nachun@gmail.com>
2024-04-19 15:48:51 -0700

    polyfun: use env scripts

--- polyfun/build.sh
+++ polyfun/build.sh
@@ -8,10 +8,18 @@ rm -rf example_data
 rm -rf *.parquet
 cp -r "${SRC_DIR}"/* "${PREFIX}/libexec/${PKG_NAME}"
 
+env_script() {
+bin_name=$(basename $1)
+tee ${PREFIX}/bin/${bin_name} << EOF
+#!/bin/sh
+exec ${PREFIX}/libexec/${PKG_NAME}/${bin_name} "\$@"
+EOF
+}
+
 mkdir -p ${PREFIX}/bin
-ln -sf "${PREFIX}/libexec/${PKG_NAME}/munge_polyfun_sumstats.py" "${PREFIX}/bin"
-ln -sf "${PREFIX}/libexec/${PKG_NAME}/polyfun.py" "${PREFIX}/bin"
-ln -sf "${PREFIX}/libexec/${PKG_NAME}/polyloc.py" "${PREFIX}/bin"
-ln -sf "${PREFIX}/libexec/${PKG_NAME}/extract_snpvar.py" "${PREFIX}/bin"
-ln -sf "${PREFIX}/libexec/${PKG_NAME}/finemapper.py" "${PREFIX}/bin"
-ln -sf "${PREFIX}/libexec/${PKG_NAME}/test_polyfun.py" "${PREFIX}/bin"
+env_script munge_polyfun_sumstats.py
+env_script polyfun.py
+env_script polyloc.py
+env_script extract_snpvar.py
+env_script finemapper.py
+env_script test_polyfun.py
