ab7fb6cbcd501f58c488570d32e0fbda1c72c1dc
danielnachun <daniel.nachun@gmail.com>
2024-04-21 22:25:34 -0700

    polyfun: use env scripts

--- polyfun/build.sh
+++ polyfun/build.sh
@@ -12,14 +12,21 @@ env_script() {
 bin_name=$(basename $1)
 tee ${PREFIX}/bin/${bin_name} << EOF
 #!/bin/sh
-exec ${PREFIX}/libexec/${PKG_NAME}/${bin_name} "\$@"
+exec python3 ${PREFIX}/libexec/${PKG_NAME}/${bin_name} "\$@"
 EOF
 }
 
 mkdir -p ${PREFIX}/bin
+env_script aggregate_finemapper_results.py
+env_script compute_ldscores.py
+env_script compute_ldscores_ld.py
+env_script create_finemapper_jobs.py
+env_script extract_annotations.py
+env_script extract_snpvar.py
+env_script finemapper.py
+env_script ldsc.py
 env_script munge_polyfun_sumstats.py
 env_script polyfun.py
 env_script polyloc.py
-env_script extract_snpvar.py
-env_script finemapper.py
+env_script polypred.py
 env_script test_polyfun.py
