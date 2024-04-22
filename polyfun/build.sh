7dd9a67b9c5f5a645f0396811219b723081a3fb2
Daniel Nachun <daniel.nachun@gmail.com>
2024-04-21 23:47:41 -0700

    polyfun: use env scripts (#2017)
    * polyfun: use env scripts
    
    * polyfun: use env scripts
    
    * polyfun: use env scripts
    
    * polyfun: use env scripts

--- polyfun/build.sh
+++ polyfun/build.sh
@@ -9,6 +9,7 @@ rm -rf *.parquet
 cp -r "${SRC_DIR}"/* "${PREFIX}/libexec/${PKG_NAME}"
 
 env_script() {
+chmod +x ${1}
 bin_name=$(basename $1)
 tee ${PREFIX}/bin/${bin_name} << EOF
 #!/bin/sh
@@ -17,16 +18,16 @@ EOF
 }
 
 mkdir -p ${PREFIX}/bin
-env_script aggregate_finemapper_results.py
-env_script compute_ldscores.py
-env_script compute_ldscores_ld.py
-env_script create_finemapper_jobs.py
-env_script extract_annotations.py
-env_script extract_snpvar.py
-env_script finemapper.py
-env_script ldsc.py
-env_script munge_polyfun_sumstats.py
-env_script polyfun.py
-env_script polyloc.py
-env_script polypred.py
-env_script test_polyfun.py
+env_script ${PREFIX}/libexec/${PKG_NAME}/aggregate_finemapper_results.py
+env_script ${PREFIX}/libexec/${PKG_NAME}/compute_ldscores.py
+env_script ${PREFIX}/libexec/${PKG_NAME}/compute_ldscores_from_ld.py
+env_script ${PREFIX}/libexec/${PKG_NAME}/create_finemapper_jobs.py
+env_script ${PREFIX}/libexec/${PKG_NAME}/extract_annotations.py
+env_script ${PREFIX}/libexec/${PKG_NAME}/extract_snpvar.py
+env_script ${PREFIX}/libexec/${PKG_NAME}/finemapper.py
+env_script ${PREFIX}/libexec/${PKG_NAME}/ldsc.py
+env_script ${PREFIX}/libexec/${PKG_NAME}/munge_polyfun_sumstats.py
+env_script ${PREFIX}/libexec/${PKG_NAME}/polyfun.py
+env_script ${PREFIX}/libexec/${PKG_NAME}/polyloc.py
+env_script ${PREFIX}/libexec/${PKG_NAME}/polypred.py
+env_script ${PREFIX}/libexec/${PKG_NAME}/test_polyfun.py
