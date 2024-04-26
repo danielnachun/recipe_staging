5d61450b4a54ffe764abb9687fe14c2f7d9f3c75
Daniel Nachun <daniel.nachun@gmail.com>
2024-04-26 09:51:53 -0700

    update jdtls (#2093)
    * update jdtls
    
    * update jdtls
    
    * update jdtls
    
    * update jdtls
    
    * update jdtls
    
    * update jdtls
    
    * update jdtls

--- jdtls/build.sh
+++ jdtls/build.sh
@@ -3,7 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 touch ${BUILD_PREFIX}/lib/jvm/release
 
-./mvnw install -DskipTests=true -Dmaven.local.repo=$SRC_DIR
+./mvnw install -T 1 -DskipTests=true -Dmaven.local.repo=$SRC_DIR
 
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 mkdir -p ${PREFIX}/bin
