080c0e240078a09b35c178f56950a9d22d66d8c8
danielnachun <daniel.nachun@gmail.com>
2024-05-12 22:29:10 -0700

    pmd 7.1.0

--- pmd/build.sh
+++ pmd/build.sh
@@ -6,8 +6,8 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 mkdir -p ${PREFIX}/bin
-unzip pmd-dist/target/pmd-dist-7.1.0-SNAPSHOT-bin.zip
-cp -r pmd-bin-7.1.0-SNAPSHOT/* ${PREFIX}/libexec/${PKG_NAME}
+unzip pmd-dist/target/pmd-dist-7.2.0-SNAPSHOT-bin.zip
+cp -r pmd-bin-7.2.0-SNAPSHOT/* ${PREFIX}/libexec/${PKG_NAME}
 
 tee ${PREFIX}/bin/pmd << EOF
 JAVA_HOME=${JAVA_HOME} exec ${PREFIX}/libexec/${PKG_NAME}/bin/pmd "\$@"
