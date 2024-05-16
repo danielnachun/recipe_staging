ef3abd6545ef15f5f223b6b5b5b7f672dad50ac6
danielnachun <daniel.nachun@gmail.com>
2024-05-15 18:17:29 -0700

    add scqers

--- scqers/build.sh
+++ scqers/build.sh
@@ -2,6 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+mkdir -p ${PREFIX}/bin
 install -m 755 scripts/MPRA_Snakemake_Pipeline/workflow/scripts/clean_up_UMI_counts_v3_20220126.R ${PREFIX}/bin/clean_up_umi_counts
 install -m 755 scripts/MPRA_Snakemake_Pipeline/workflow/scripts/get_barcode_v2_fixed_pos_w_seq_check_20220201.py ${PREFIX}/bin/get_barcode_fixed_pos
 
