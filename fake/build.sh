e5fc84c7ad748a182bdebd25c35302d387d158d8
Daniel Nachun <daniel.nachun@gmail.com>
2024-05-19 18:24:06 -0400

    fake: update licenses (#2268)

--- fake/build.sh
+++ fake/build.sh
@@ -16,3 +16,5 @@ tee ${PREFIX}/bin/fake-cli << EOF
 #!/bin/sh
 DOTNET_ROOT=${DOTNET_ROOT} exec ${DOTNET_ROOT}/dotnet exec ${PREFIX}/libexec/${PKG_NAME}/fake-cli.dll "\$@"
 EOF
+
+dotnet-project-licenses -e --input . -f license-files
