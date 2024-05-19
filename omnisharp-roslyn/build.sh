6b5a7a4d2b83b6061cac42c262bdf227be715f8d
danielnachun <daniel.nachun@gmail.com>
2024-05-19 15:31:07 -0700

    omnisharp-roslyn: update licenses

--- omnisharp-roslyn/build.sh
+++ omnisharp-roslyn/build.sh
@@ -20,3 +20,5 @@ tee ${PREFIX}/bin/OmniSharp << EOF
 #!/bin/sh
 DOTNET_ROOT=${DOTNET_ROOT} exec ${DOTNET_ROOT}/dotnet exec ${PREFIX}/libexec/${PKG_NAME}/OmniSharp.dll "\$@"
 EOF
+
+dotnet-project-licenses -e --input . -f license-files
