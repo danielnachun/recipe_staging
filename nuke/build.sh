d5aeae6e24616f5eb1d2aa2c1b5efb407d4c565f
Daniel Nachun <daniel.nachun@gmail.com>
2024-05-19 18:51:44 -0400

    nuke: update licenses (#2274)

--- nuke/build.sh
+++ nuke/build.sh
@@ -13,3 +13,5 @@ tee ${PREFIX}/bin/nuke << EOF
 #!/bin/sh
 DOTNET_ROOT=${DOTNET_ROOT} exec ${DOTNET_ROOT}/dotnet exec ${PREFIX}/libexec/${PKG_NAME}/Nuke.GlobalTool.dll "\$@"
 EOF
+
+dotnet-project-licenses -e --input . -f license-files
