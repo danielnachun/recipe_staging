d7b79d60711e65c8629a187a64b6a872bc640c47
Daniel Nachun <daniel.nachun@gmail.com>
2024-05-19 18:24:29 -0400

    ilspy: update licenses (#2271)

--- ilspy/build.sh
+++ ilspy/build.sh
@@ -21,3 +21,5 @@ tee ${PREFIX}/bin/ilspycmd << EOF
 #!/bin/sh
 DOTNET_ROOT=${DOTNET_ROOT} exec ${DOTNET_ROOT}/dotnet exec ${PREFIX}/libexec/${PKG_NAME}/ilspycmd.dll "\$@"
 EOF
+
+dotnet-project-licenses -e --input . -f license-files
