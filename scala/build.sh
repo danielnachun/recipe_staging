8c973d022a1c872a7bd7eb6bd0402d10cdf7ffa2
Daniel Nachun <daniel.nachun@gmail.com>
2024-05-21 14:54:59 -0400

    scala: update licenses (#2286)
    * scala: update licenses
    
    * scala: update licenses
    
    * scala: update licenses
    
    * scala: update licenses

--- scala/build.sh
+++ scala/build.sh
@@ -2,7 +2,25 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+download_licenses() {
+    pom_file=$1
+    pom_xml=$(dirname ${pom_file})/pom.xml
+    mv ${pom_file} ${pom_xml}
+    sed -i 's:[\+|\-].*\-SNAPSHOT.*</version>:</version>:g' ${pom_xml}
+    pushd $(dirname ${pom_xml})
+    mvn license:download-licenses -Dgoal=download-licenses
+    popd
+}
+
+export -f download_licenses
+
 sbt dist/packArchive
+
+sbt makePom
+
+find -name "*.pom" | xargs -I % bash -c 'download_licenses %'
+mkdir -p ${SRC_DIR}/target/generated-resources/licenses
+find -type d -name "licenses" | grep generated-resources | grep -v "^./target" | xargs -I % bash -c 'cp %/* ./target/generated-resources/licenses'
 
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
