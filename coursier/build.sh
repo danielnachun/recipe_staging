3e963af647abd5ff7df5baff84f49be020d6eeb9
danielnachun <daniel.nachun@gmail.com>
2024-05-22 11:48:20 -0700

    coursier: update licenses

--- coursier/build.sh
+++ coursier/build.sh
@@ -9,6 +9,7 @@ mkdir -p ${PREFIX}/bin
 export GRAALVM_HOME=${PREFIX}/lib/jvm
 export JAVA_HOME=${GRAALVM_HOME}
 sed -i "s?--no-fallback ?--no-fallback --native-compiler-options=-L${PREFIX}/lib ?" modules/cli/src/main/resources/META-INF/native-image/io.get-coursier/coursier-cli/native-image.properties
-./mill -i show cli.nativeImage
+./mill -i show cli[].nativeImage
+./mill -i show cli[].publishM2Local ${SRC_DIR}/m2
 
 install -m 755 out/cli/base-image/nativeImage.dest/cs ${PREFIX}/bin/cs
