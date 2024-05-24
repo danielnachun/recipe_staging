b58854fa3f87a86323968cc13c59946929e5e5ad
danielnachun <daniel.nachun@gmail.com>
2024-05-24 00:18:57 -0700

    babashka: update license

--- babashka/build.sh
+++ babashka/build.sh
@@ -4,9 +4,12 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 mkdir -p ${PREFIX}/bin
 
-export GRAALVM_HOME=${BUILD_PREFIX}/lib/jvm
+export GRAALVM_HOME=${PREFIX}/lib/jvm
 
 script/uberjar
 script/compile --native-compiler-options="-L${PREFIX}/lib"
+
+lein pom
+mvn license:download-licenses -Dgoal=download-licenses
 
 install -m 755 bb ${PREFIX}/bin
