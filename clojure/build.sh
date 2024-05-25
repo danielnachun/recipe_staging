7749426271cc5ee96610e05849a7651609e17eb9
danielnachun <daniel.nachun@gmail.com>
2024-05-25 15:45:29 -0700

    clojure: update licenses

--- clojure/build.sh
+++ clojure/build.sh
@@ -5,11 +5,11 @@ set -o xtrace -o nounset -o pipefail -o errexit
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
-mvn package
+mvn -Plocal -Dmaven.test.skip=true package
 
 mvn license:download-licenses -Dgoal=download-licenses
 
-cp target/${PKG_NAME}-${PKG_VERSION}.jar ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME}.jar
+cp ${PKG_NAME}.jar ${PREFIX}/libexec/${PKG_NAME}
 
 tee ${PREFIX}/bin/clojure << EOF
 #!/bin/sh
