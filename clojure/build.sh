edf6028b7b3be0f0ebd533e9e3d0080f6107c5a6
danielnachun <daniel.nachun@gmail.com>
2024-05-25 14:38:28 -0700

    clojure: update licenses

--- clojure/build.sh
+++ clojure/build.sh
@@ -8,3 +8,12 @@ mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 mvn package
 
 mvn license:download-licenses -Dgoal=download-licenses
+
+cp target/${PKG_NAME}-${PKG_VERSION}.jar ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME}.jar
+
+tee ${PREFIX}/bin/clojure << EOF
+#!/bin/sh
+JAVA_HOME=${JAVA_HOME} exec ${JAVA_HOME}/bin/java -jar ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME}.jar "\$@"
+EOF
+
+ln -sf ${PREFIX}/bin/clojure ${PREFIX}/bin/clj
