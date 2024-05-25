a842b7bc5462bd1e246f1f6b1c198aef612597c1
danielnachun <daniel.nachun@gmail.com>
2024-05-25 14:34:54 -0700

    clojure: update licenses

--- clojure/build.sh
+++ clojure/build.sh
@@ -5,10 +5,6 @@ set -o xtrace -o nounset -o pipefail -o errexit
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
-sed -i "s?\${HOMEBREW_RUBY_PATH}?${BUILD_PREFIX}/bin/ruby?g" install.sh
-sed -i "s?libexec?libexec/${PKG_NAME}?g" install.sh
-sed -i "s?libexec?libexec/${PKG_NAME}?g" clojure
-./install.sh ${PREFIX}
+mvn package
 
 mvn license:download-licenses -Dgoal=download-licenses
-exit 1
