f07eab51c5ea5ce3f1f34f97c1138e3ceb035c6b
Daniel Nachun <daniel.nachun@gmail.com>
2024-05-24 20:17:44 -0400

    cljstyle: update licenses (#2293)
    * cljstyle: update licenses
    
    * cljstyle: update licenses

--- cljstyle/build.sh
+++ cljstyle/build.sh
@@ -2,10 +2,12 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-lein uberjar
+export GRAALVM_HOME=${PREFIX}/lib/jvm
+
+clojure -T:build uberjar
 
 native-image \
-    -jar target/uberjar/cljstyle.jar \
+    -jar target/cljstyle.jar \
     --native-image-info \
     --native-compiler-options="-L${PREFIX}/lib" \
     --initialize-at-build-time \
@@ -19,6 +21,13 @@ native-image \
     -J-Dclojure.compiler.direct-linking=true \
     -J-Xmx4500m \
     --verbose
+
+clojure -T:build pom
+
+pushd target/classes/META-INF/maven/mvxcvi/cljstyle
+    mvn license:download-licenses -Dgoal=download-licenses
+    mv target/generated-resources ${SRC_DIR}/target
+popd
 
 mkdir -p ${PREFIX}/bin
 install -m 755 cljstyle ${PREFIX}/bin
