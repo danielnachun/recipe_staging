43b0bf178dda0339ba66da4292111a19ffd653a4
Daniel Nachun <daniel.nachun@gmail.com>
2024-05-25 19:20:51 -0400

    clojure: update licenses (#2297)
    * clojure: update licenses
    
    * clojure: update licenses
    
    * clojure: update licenses
    
    * clojure: update licenses
    
    * clojure: update licenses

--- clojure/build.sh
+++ clojure/build.sh
@@ -5,7 +5,15 @@ set -o xtrace -o nounset -o pipefail -o errexit
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
-sed -i "s?\${HOMEBREW_RUBY_PATH}?${BUILD_PREFIX}/bin/ruby?g" install.sh
-sed -i "s?libexec?libexec/${PKG_NAME}?g" install.sh
-sed -i "s?libexec?libexec/${PKG_NAME}?g" clojure
-./install.sh ${PREFIX}
+mvn -Plocal -Dmaven.test.skip=true package
+
+mvn license:download-licenses -Dgoal=download-licenses
+
+cp ${PKG_NAME}.jar ${PREFIX}/libexec/${PKG_NAME}
+
+tee ${PREFIX}/bin/clojure << EOF
+#!/bin/sh
+JAVA_HOME=${JAVA_HOME} exec ${JAVA_HOME}/bin/java -jar ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME}.jar "\$@"
+EOF
+
+ln -sf ${PREFIX}/bin/clojure ${PREFIX}/bin/clj
