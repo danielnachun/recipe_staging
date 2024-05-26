248db2c319f75a65fb1efc48bad20834ccb8702c
danielnachun <daniel.nachun@gmail.com>
2024-05-25 21:25:34 -0700

    groovy: update licenses

--- groovy/build.sh
+++ groovy/build.sh
@@ -2,6 +2,17 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+download_licenses() {
+    pom_file=$1
+    pom_xml=$(dirname ${pom_file})/pom.xml
+    mv ${pom_file} ${pom_xml}
+    pushd $(dirname ${pom_xml})
+    mvn license:download-licenses -Dgoal=download-licenses
+    popd
+}
+
+export -f download_licenses
+
 ./gradlew clean dist
 mv subprojects/groovy-binary/build/distributions/apache-groovy-binary-${PKG_VERSION}.zip .
 unzip apache-groovy-binary-${PKG_VERSION}.zip
@@ -21,3 +32,8 @@ EOF
 export -f env_script
 
 find ${PREFIX}/libexec/${PKG_NAME}/bin -type f | grep -v ".bat" | grep -v ".ico" | sort -u | xargs -I % bash -c "env_script %"
+./gradlew publishMavenPublicationToLocalFileRepository --no-build-cache --no-scan --refresh-dependencies
+
+find -name "*.pom" | xargs -I % bash -c 'download_licenses %'
+mkdir -p ${SRC_DIR}/target/generated-resources/licenses
+find -type d -name "licenses" | grep generated-resources | grep -v "^./target" | xargs -I % bash -c 'cp %/* ./target/generated-resources/licenses'
