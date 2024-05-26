93f42d77118da796b1819af29d18307f110cd269
danielnachun <daniel.nachun@gmail.com>
2024-05-26 14:55:44 -0700

    add cabal-plan

---
+++ cabal-plan/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CABAL_DIR=${SRC_DIR}/.cabal
+cabal v2-update
+cabal v2-install \
+    --extra-lib-dirs=${PREFIX}/lib \
+    --allow-newer=base,aeson \
+	--jobs=${CPU_COUNT} \
+	--max-backjumps=100000 \
+	--install-method=copy \
+	--installdir=${PREFIX}/bin
+${PREFIX}/bin/cabal-plan license-report --licensedir=${SRC_DIR}/license-files exe:${PKG_NAME}
