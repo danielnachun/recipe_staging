dcc46a1d366ad58f91ff2dffcc9b080d720151c4
danielnachun <daniel.nachun@gmail.com>
2024-05-26 23:53:56 -0700

    comby: update licenses

--- comby/build.sh
+++ comby/build.sh
@@ -2,6 +2,15 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+copy_license() {
+    license_file=$1
+    license_file_dirname=$(basename $(dirname ${license_file}))
+    mkdir -p license-files/${license_file_dirname}
+    cp ${license_file} license-files/${license_file_dirname}
+}
+
+export -f copy_license
+
 export OPAMROOT=${SRC_DIR}/.opam
 export OPAMYES=1
 export OPAMDOWNLOADJOBS=1
@@ -15,3 +24,7 @@ opam exec -- make release
 
 mkdir -p ${PREFIX}/bin
 install -m 755 _build/default/src/main.exe ${PREFIX}/bin/comby
+
+mkdir -p license-files
+odig show license-files --lib-dir=${OPAMROOT}/default/lib --doc-dir=${OPAMROOT}/default/doc -l | cut -f 2 -d ' ' | \
+    xargs -I % bash -c 'copy_license %'
