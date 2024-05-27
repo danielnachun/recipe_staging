87d51bf322ba0f8c75ca30609a11278b52fc689a
Daniel Nachun <daniel.nachun@gmail.com>
2024-05-27 13:44:19 -0400

    ocaml-lsp: update licenses (#2314)
    * ocaml-lsp: update licenses
    
    * ocaml-lsp: update licenses
    
    * ocaml-lsp: update licenses

--- ocaml-lsp/build.sh
+++ ocaml-lsp/build.sh
@@ -2,6 +2,15 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+copy_license() {
+    license_file=$1
+    license_file_dirname=$(basename $(dirname ${license_file}))
+    mkdir -p license-files/${license_file_dirname}
+    cp ${license_file} license-files/${license_file_dirname}
+}
+
+export -f copy_license
+
 export OPAMROOT=${SRC_DIR}/.opam
 export OPAMYES=1
 export OPAMDOWNLOADJOBS=${CPU_COUNT}
@@ -13,3 +22,9 @@ opam exec -- make install
 
 mkdir -p ${PREFIX}/bin
 install -m 755 _build/default/ocaml-lsp-server/bin/main.exe ${PREFIX}/bin/ocamllsp
+
+opam switch default
+eval $(opam env)
+mkdir -p license-files
+odig show license-files --lib-dir=${OPAMROOT}/default/lib --doc-dir=${OPAMROOT}/default/doc -l | cut -f 2 -d ' ' | \
+    xargs -I % bash -c 'copy_license %'
