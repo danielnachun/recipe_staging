1b50e49f8de4dd972763c9160e2ad2d2d23a828f
danielnachun <daniel.nachun@gmail.com>
2024-05-26 23:34:07 -0700

    stylish-haskell: update licenses

--- stylish-haskell/build.sh
+++ stylish-haskell/build.sh
@@ -2,8 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CABAL_DIR=${SRC_DIR}/.cabal
 cabal v2-update
-cabal --store-dir=${SRC_DIR}/.cabal v2-install \
+cabal v2-install \
     --allow-newer=ghc-lib-parser,text,bytestring,aeson \
     --extra-lib-dirs=${PREFIX}/lib \
     --flags=+ghc-lib \
@@ -11,3 +12,4 @@ cabal --store-dir=${SRC_DIR}/.cabal v2-install \
 	--max-backjumps=100000 \
 	--install-method=copy \
 	--installdir=${PREFIX}/bin
+cabal-plan license-report --licensedir=${SRC_DIR}/license-files exe:${PKG_NAME}
