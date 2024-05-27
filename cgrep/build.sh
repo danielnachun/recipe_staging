f8867de614bfde3bd7949b18e48e8a966228f50c
Daniel Nachun <daniel.nachun@gmail.com>
2024-05-27 13:33:42 -0400

    cgrep: update licenses (#2311)
    * cgrep: update licenses
    
    * cgrep: update licenses

--- cgrep/build.sh
+++ cgrep/build.sh
@@ -4,10 +4,12 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 cp ${RECIPE_DIR}/cabal.project ${SRC_DIR}
 
+export CABAL_DIR=${SRC_DIR}/.cabal
 cabal v2-update
-cabal --store-dir=${SRC_DIR}/.cabal v2-install \
+cabal v2-install \
     --extra-lib-dirs=${PREFIX}/lib \
 	--jobs=${CPU_COUNT} \
 	--max-backjumps=100000 \
 	--install-method=copy \
 	--installdir=${PREFIX}/bin
+cabal-plan license-report --licensedir=${SRC_DIR}/license-files exe:${PKG_NAME}
