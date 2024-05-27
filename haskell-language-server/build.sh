b6da550a5f35e94d1cd48430829435cef63ea3c1
Daniel Nachun <daniel.nachun@gmail.com>
2024-05-27 18:05:18 -0400

    haskell-language-server: update licenses (#2302)

--- haskell-language-server/build.sh
+++ haskell-language-server/build.sh
@@ -2,11 +2,14 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CABAL_DIR=${SRC_DIR}/.cabal
+
 cabal v2-update
-cabal --store-dir=${SRC_DIR}/.cabal v2-install \
+cabal v2-install \
     --extra-lib-dirs=${PREFIX}/lib \
     --flags=-dynamic \
 	--jobs=${CPU_COUNT} \
 	--max-backjumps=100000 \
 	--install-method=copy \
 	--installdir=${PREFIX}/bin
+cabal-plan license-report --licensedir=${SRC_DIR}/license-files exe:${PKG_NAME}
