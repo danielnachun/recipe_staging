2c2fa24c87bf856f13f1dba11da275e61a03b176
danielnachun <daniel.nachun@gmail.com>
2024-05-26 22:40:09 -0700

    shake: update licenses

--- shake/build.sh
+++ shake/build.sh
@@ -2,6 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CABAL_DIR=${SRC_DIR}/.cabal
 cabal v2-update
 cabal --store-dir=${SRC_DIR}/.cabal v2-install \
     --extra-lib-dirs=${PREFIX}/lib \
@@ -9,3 +10,4 @@ cabal --store-dir=${SRC_DIR}/.cabal v2-install \
 	--max-backjumps=100000 \
 	--install-method=copy \
 	--installdir=${PREFIX}/bin
+cabal-plan license-report --licensedir=${SRC_DIR}/license-files exe:${PKG_NAME}
