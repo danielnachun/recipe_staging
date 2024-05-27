a8ed84d653907155bafeeb93c5e0f9236f94a7a3
danielnachun <daniel.nachun@gmail.com>
2024-05-26 22:32:51 -0700

    alex: update licenses

--- alex/build.sh
+++ alex/build.sh
@@ -3,6 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 export PATH=${SRC_DIR}/binary/bin:${PATH}
+export CABAL_DIR=${SRC_DIR}/.cabal
 
 cabal v2-update
 cabal v2-install \
@@ -21,9 +22,11 @@ alex -g src/Scan.x -o src/Scan.hs
 mv src/Parser.y src/Parser.y.boot
 mv src/Scan.x src/Scan.x.boot
 
-cabal --store-dir=${SRC_DIR}/.cabal v2-install \
+cabal v2-install \
     --extra-lib-dirs=${PREFIX}/lib \
 	--jobs=${CPU_COUNT} \
 	--max-backjumps=100000 \
 	--install-method=copy \
 	--installdir=${PREFIX}/bin
+
+cabal-plan license-report --licensedir=${SRC_DIR}/license-files exe:${PKG_NAME}
