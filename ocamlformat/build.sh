7d63b17eda43494dc8dfd88b37d539a602a5fa75
danielnachun <daniel.nachun@gmail.com>
2024-05-26 23:51:04 -0700

    ocamlformat: update licenses

--- ocamlformat/build.sh
+++ ocamlformat/build.sh
@@ -2,6 +2,16 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+
+copy_license() {
+    license_file=$1
+    license_file_dirname=$(basename $(dirname ${license_file}))
+    mkdir -p license-files/${license_file_dirname}
+    cp ${license_file} license-files/${license_file_dirname}
+}
+
+export -f copy_license
+
 export OPAMROOT=${SRC_DIR}/.opam
 export OPAMYES=1
 export OPAMDOWNLOADJOBS=${CPU_COUNT}
@@ -14,3 +24,7 @@ opam exec -- make exe
 mkdir -p ${PREFIX}/bin
 install -m 755 _build/default/bin/ocamlformat/main.exe ${PREFIX}/bin/ocamlformat
 install -m 755 _build/default/bin/ocamlformat/main.exe ${PREFIX}/bin/ocamlformat-rpc
+
+mkdir -p license-files
+odig show license-files --lib-dir=${OPAMROOT}/default/lib --doc-dir=${OPAMROOT}/default/doc -l | cut -f 2 -d ' ' | \
+    xargs -I % bash -c 'copy_license %'
