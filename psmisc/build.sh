98641d43672156132bbeff2b9c9dbe58a82b59ee
danielnachun <daniel.nachun@gmail.com>
2024-06-10 00:04:15 -0700

    add psmisc

--- psmisc/build.sh
+++ psmisc/build.sh
@@ -3,7 +3,8 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 ./autogen.sh
-./configure --disable-silent \
+./configure --disable-statx \
+    --disable-silent \
     --disable-dependency-tracking \
     --prefix=${PREFIX} 
 make install
