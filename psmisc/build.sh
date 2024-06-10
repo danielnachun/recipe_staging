1f671c35172fea89cc5e254e144576d5082b1d45
danielnachun <daniel.nachun@gmail.com>
2024-06-10 00:21:08 -0700

    add psmisc

--- psmisc/build.sh
+++ psmisc/build.sh
@@ -2,6 +2,8 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+sed -i 's/AT_NO_AUTOMOUNT|AT_STATX_DONT_SYNC/0/' src/statx.c
+
 ./autogen.sh
 ./configure --disable-statx \
     --disable-silent \
