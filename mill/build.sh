1196b7d2a2d3fe987d58f4389b9b7da15a4b9780
Daniel Nachun <daniel.nachun@gmail.com>
2024-06-17 09:33:36 -0700

    mill: delete unneede djar (#2403)

--- mill/build.sh
+++ mill/build.sh
@@ -6,7 +6,6 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
 ./mill -i show dev.assembly
-install -m 644 out/dev/assembly.dest/out-tmp.jar ${PREFIX}/libexec/${PKG_NAME}/mill.jar
 install -m 755 out/dev/assembly.dest/mill ${PREFIX}/libexec/${PKG_NAME}/mill
 
 tee ${PREFIX}/bin/mill << EOF
