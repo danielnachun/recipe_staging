5950110c0d70b8d11ee2891fed2cc6d2afff3d89
danielnachun <daniel.nachun@gmail.com>
2024-06-22 12:46:51 -0700

    add msbuild-project-tools-server

---
+++ msbuild-project-tools-server/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+dotnet publish --no-self-contained src/LanguageServer/LanguageServer.csproj --output ${PREFIX}/libexec/${PKG_NAME} 
+
+mkdir -p ${PREFIX}/bin
+rm -rf ${PREFIX}/libexec/${PKG_NAME}/NugetUtility
+tee ${PREFIX}/bin/msbuild-project-tools-server << EOF
+#!/bin/sh
+DOTNET_ROOT=${DOTNET_ROOT} exec ${DOTNET_ROOT}/dotnet exec ${PREFIX}/libexec/${PKG_NAME}/MSBuildProjectTools.LanguageServer.dll "\$@"
+EOF
+
+dotnet-project-licenses -e --input . -f license-files
