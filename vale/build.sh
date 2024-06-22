8d09bba7263d1e9117937d6114559327f144e62a
Daniel Nachun <daniel.nachun@gmail.com>
2024-06-22 14:26:56 -0700

    vale 3.5.0 (#2398)
    * vale 3.5.0
    
    * vale 3.5.0
    
    * vale 3.5.0
    
    * vale 3.5.0
    
    * vale 3.5.0

--- vale/build.sh
+++ vale/build.sh
@@ -2,7 +2,8 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-export CGO_ENABLED=0
-export LDFLAGS="-s -w -X main.version=${PKG_VERSION}"
+unset GOPATH
+export CGO_ENABLED=1
+export LDFLAGS="-s -w"
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}" ./cmd/vale
 go-licenses save ./cmd/vale --save_path=license-files --ignore github.com/xi2/xz
