a8e390a3a898647a4e620924efebff2f49a98f99
danielnachun <daniel.nachun@gmail.com>
2024-06-24 00:55:02 -0700

    add mesonlsp

--- mesonlsp/build.sh
+++ mesonlsp/build.sh
@@ -7,10 +7,14 @@ sed -i "s/'-Wl,-ld_classic'//" meson.build
 sed -i 's/and !defined(__x86_64__)//g' src/polyfill/polyfill.hpp
 
 export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+if [[ ${target_platform} =~ .*linux.* ]]; then
+    export CFLAGS="${CFLAGS} -pthread"
+    export CXXFLAGS="${CXXFLAGS} -pthread"
+fi
 
 meson --prefix=${PREFIX} \
     --libdir=${PREFIX}/lib \
     --buildtype=release \
-    build 
+    build
 meson compile -C build -v
 meson install -C build
