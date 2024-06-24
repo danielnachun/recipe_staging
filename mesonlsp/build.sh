ba09333810b91aa7e775589da7ad272707dfa91c
danielnachun <daniel.nachun@gmail.com>
2024-06-24 00:00:14 -0700

    add mesonlsp

--- mesonlsp/build.sh
+++ mesonlsp/build.sh
@@ -2,10 +2,15 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+sed -i '/Requires.private/d' ${PREFIX}/lib/pkgconfig/libarchive.pc
+sed -i "s/'-Wl,-ld_classic'//" meson.build
+sed -i 's/and !defined(__x86_64__)//g' src/polyfill/polyfill.hpp
+
+export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+
 meson --prefix=${PREFIX} \
     --libdir=${PREFIX}/lib \
     --buildtype=release \
-    --wrap-mode=nofallback \
     build 
 meson compile -C build -v
 meson install -C build
