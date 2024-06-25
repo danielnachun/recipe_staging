1beb0967b742f19ad526dc2923ff646d137b4e20
Daniel Nachun <daniel.nachun@gmail.com>
2024-06-24 20:29:24 -0700

    add haskell-debug-adapter (#2429)
    * add haskell-debug-adapter
    
    * add haskell-debug-adapter
    
    * add haskell-debug-adapter

---
+++ haskell-debug-adapter/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+sed -i '/..\/haskell-dap/d' cabal.project
+sed -i '/..\/ghci-dap/d' cabal.project
+
+export CABAL_DIR=${SRC_DIR}/.cabal
+cabal v2-update
+cabal v2-install \
+    --extra-lib-dirs=${PREFIX}/lib \
+	--jobs=${CPU_COUNT} \
+	--max-backjumps=100000 \
+	--install-method=copy \
+	--installdir=${PREFIX}/bin
+cabal-plan license-report --licensedir=${SRC_DIR}/license-files exe:${PKG_NAME}
