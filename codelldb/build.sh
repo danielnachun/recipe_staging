89b5504e8bd3fd7ebda4218bd672fbee9c7cc898
Daniel Nachun <daniel.nachun@gmail.com>
2024-06-25 19:47:56 -0700

    add codelldb (#2445)
    * add codelldb
    
    * add codelldb
    
    * add codelldb
    
    * add codelldb
    
    * add codelldb
    
    * add codelldb

---
+++ codelldb/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+if [[ ${target_platform} == "osx-arm64" ]]; then
+    export CMAKE_ARGS="-DLLVM_TRIPLE='aarch64-apple-darwin' ${CMAKE_ARGS}"
+fi
+
+cmake -S . -B build \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -Wno-dev \
+    -DBUILD_TESTING=OFF \
+    -DLLDB_PACKAGE=${BUILD_PREFIX} \
+    ${CMAKE_ARGS}
+
+cmake --build build
+
+mkdir -p ${PREFIX}/bin
+install -m 755 ./build/target/**/release/codelldb ${PREFIX}/bin
+mkdir -p ${PREFIX}/lib
+install -m 644 ./build/adapter/libcodelldb${SHLIB_EXT} ${PREFIX}/lib
