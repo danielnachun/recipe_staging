819e329a77d8428f11750feb3a616d15f757b06d
danielnachun <daniel.nachun@gmail.com>
2024-06-26 16:36:57 -0700

    add indent

---
+++ indent/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+./configure --disable-debug \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} \
+    --mandir=${PREFIX}/share/man
+make install
