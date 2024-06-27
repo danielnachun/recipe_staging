72a5c3595f82371d1d57431c8aaa8738389e1f3a
danielnachun <daniel.nachun@gmail.com>
2024-06-26 17:38:41 -0700

    add indent

--- indent/build.sh
+++ indent/build.sh
@@ -2,6 +2,8 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CFLAGS="${CFLAGS} -Wimplicit-function-declaration"
+
 ./configure --disable-debug \
     --disable-dependency-tracking \
     --prefix=${PREFIX} \
