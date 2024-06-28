878678f26bdb3b320af7ec040295c3fcbf71c5f3
Daniel Nachun <daniel.nachun@gmail.com>
2024-06-27 17:51:14 -0700

    update scqers (#2460)

--- scqers/build.sh
+++ scqers/build.sh
@@ -3,8 +3,10 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 mkdir -p ${PREFIX}/bin
-install -m 755 scripts/MPRA_Snakemake_Pipeline/workflow/scripts/clean_up_UMI_counts_v3_20220126.R ${PREFIX}/bin/clean_up_umi_counts
-install -m 755 scripts/MPRA_Snakemake_Pipeline/workflow/scripts/get_barcode_v2_fixed_pos_w_seq_check_20220201.py ${PREFIX}/bin/get_barcode_fixed_pos
+install -m 755 scripts/get_barcode_counts/workflow/scripts/clean_up_UMI_counts_v3_20220126.R ${PREFIX}/bin/clean_up_umi_counts
+install -m 755 scripts/get_barcode_counts/workflow/scripts/get_barcode_v2_fixed_pos_w_seq_check_20220201.py ${PREFIX}/bin/get_barcode_fixed_pos
+install -m 755 scripts/gene_expression_processing/workflow/scripts/seurat.R ${PREFIX}/bin/seurat
 
 sed -i '1i #!/usr/bin/env Rscript' ${PREFIX}/bin/clean_up_umi_counts
+sed -i '1i #!/usr/bin/env Rscript' ${PREFIX}/bin/seurat
 sed -i '1i #!/usr/bin/env python3' ${PREFIX}/bin/get_barcode_fixed_pos
