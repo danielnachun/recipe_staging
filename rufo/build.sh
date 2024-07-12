61e663ea96428f2b7bd8613dab55ec9662cb28eb
Daniel Nachun <daniel.nachun@gmail.com>
2024-07-11 19:30:10 -0700

    rufo: try to fix on Windows (#2469)

--- rufo/build.sh
+++ rufo/build.sh
@@ -4,3 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 gem install -N -l -V --ignore-dependencies --bindir "${PREFIX}/bin" ${PKG_NAME}-${PKG_VERSION}.gem
 gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
+
+tee ${PREFIX}/bin/rufo.cmd << EOF
+call %CONDA_PREFIX%\bin\ruby %CONDA_PREFIX%\bin\rufo %*
+EOF
