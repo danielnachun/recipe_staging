17f359863f233bccac64f5ba160c5970b754d567
danielnachun <daniel.nachun@gmail.com>
2024-07-11 19:38:58 -0700

    add bibtex-tidy

--- bibtex-tidy/build.sh
+++ bibtex-tidy/build.sh
@@ -2,7 +2,21 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-npm run build
+# Don't use pre-built gyp packages
+export npm_config_build_from_source=true
 
-mkdir -p ${PREFIX}/bin
-install -m 755 bin/bibtex-tidy ${PREFIX}/bin/bibtex-tidy
+# source.url in meta.yaml references a tgz file which contains a fully-built package
+# version, we now want to turn this back into a tgz file using pnpm pack and install it
+# globally from that.
+# as we are doing pnpm pack we still need to include the node_modules which we retrieve
+# using pnpm install
+pnpm install
+pnpm pack
+
+# install package globally from file (as opposed to from a registry as you'd do normally)
+NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
+npm pack
+npm install -g ${PKG_NAME}-${PKG_VERSION}.tgz
+
+# generate license disclaimer for the package
+# pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
