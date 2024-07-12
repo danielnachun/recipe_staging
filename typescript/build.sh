e832bc8b14a7bdd3970f3202d11f30232d34a55b
danielnachun <daniel.nachun@gmail.com>
2024-07-11 21:03:34 -0700

    typescript 5.5.3

--- typescript/build.sh
+++ typescript/build.sh
@@ -7,9 +7,4 @@ npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source \
-    --prefix=${PREFIX}/libexec/${PKG_NAME} \
     ${PKG_NAME}-${PKG_VERSION}.tgz
-
-mkdir -p ${PREFIX}/bin
-ln -sf ${PREFIX}/libexec/${PKG_NAME}/bin/tsc ${PREFIX}/bin
-ln -sf ${PREFIX}/libexec/${PKG_NAME}/bin/tsserver ${PREFIX}/bin
