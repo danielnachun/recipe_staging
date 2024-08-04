e01c633914d803bbcbaac8655552a79e2f9e5ea3
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-04 12:31:00 -0700

    add nx

--- nx/build.sh
+++ nx/build.sh
@@ -2,8 +2,12 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cd packages/nx
+pnpm install
+pnpm run build
+
+cd build/packages/nx
 # Run pnpm so that pnpm-licenses can create report
+sed -i "s/0.0.1/${PKG_VERSION}/" package.json
 pnpm install --ignore-scripts
 pnpm pack
 
@@ -12,7 +16,7 @@ npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source \
-    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
+    ${SRC_DIR}/build/packages/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
-pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
+pnpm-licenses generate-disclaimer --prod --output-file=${SRC_DIR}/third-party-licenses.txt
