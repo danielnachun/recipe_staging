c65aae54875a70721e5389adf613fdd503c8220b
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-04 21:36:53 -0700

    add docusaurus

--- docusaurus/build.sh
+++ docusaurus/build.sh
@@ -2,20 +2,16 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-yarn build:packages
-
 # Run pnpm so that pnpm-licenses can create report
-cd packages/${PKG_NAME}
 pnpm install
 pnpm pack
 
 # Create package archive and install globally
-npm run build
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source \
-    ${SRC_DIR}/packages/${PKG_NAME}/${PKG_NAME}-core-${PKG_VERSION}.tgz
+    ${SRC_DIR}/${PKG_NAME}-core-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
 pnpm-licenses generate-disclaimer --prod --output-file=${SRC_DIR}/third-party-licenses.txt
