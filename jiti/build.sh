b31dfcdaf06439b2e2587328f7fd337d12b1d7ad
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-04 19:13:51 -0700

    add jiti (#2519)
    * add jiti
    
    * add jiti

---
+++ jiti/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+# Create package archive and install globally
+npm install --ignore-scripts
+npm run build
+npm pack --ignore-scripts
+npm install -ddd \
+    --global \
+    --build-from-source \
+    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
+
+tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
+call %CONDA_PREFIX%\bin\node %PREFIX%\bin\jiti %*
+EOF
