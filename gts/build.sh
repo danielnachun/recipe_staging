9b16a71dec47c4f588b4fe89fbe90813557d0524
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-04 22:00:32 -0700

    add gts

--- gts/build.sh
+++ gts/build.sh
@@ -11,11 +11,11 @@ npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source \
-    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
+    ${SRC_DIR}/gts-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
+tee ${PREFIX}/bin/gts.cmd << EOF
 call %CONDA_PREFIX%\bin\node %PREFIX%\bin\gts %*
 EOF
