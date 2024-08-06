deb5b1f851355a7e6b0f4e64f70c4d7ebc800baf
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-05 23:43:38 -0700

    add funfair-build-version

--- funfair-build-version/build.sh
+++ funfair-build-version/build.sh
@@ -8,22 +8,27 @@ mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 # Build package with dotnet publish
 rm -rf global.json
 framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
-dotnet publish --no-self-contained src/FunFair.BuildVersion/FunFair.BuildVersion.csproj --output ${PREFIX}/libexec/${PKG_NAME} --framework "net${framework_version}"
-rm ${PREFIX}/libexec/${PKG_NAME}/buildversion
+
+ln -sf ${SRC_DIR}/src/FunFair.props ${SRC_DIR}/src/FunFair.BuildVersion
+ln -sf ${SRC_DIR}/src/FunFair.props ${SRC_DIR}/src/FunFair.BuildVersion.Detection
+ln -sf ${SRC_DIR}/src/FunFair.props ${SRC_DIR}/src/FunFair.BuildVersion.Publishers
+ln -sf ${SRC_DIR}/src/FunFair.props ${SRC_DIR}/src/FunFair.BuildVersion.Interfaces
+
+dotnet publish --no-self-contained src/FunFair.BuildVersion/FunFair.BuildVersion.csproj --output ${PREFIX}/libexec/${PKG_NAME} --framework "net${framework_version}" /p:RunAnalyzers=False -c Release
+rm ${PREFIX}/libexec/${PKG_NAME}/FunFair.BuildVersion
 
 # Create bash and batch wrappers
 tee ${PREFIX}/bin/buildversion << EOF
 #!/bin/sh
-exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/${PKG_NAME}/buildversion.dll "\$@"
+exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/${PKG_NAME}/FunFair.BuildVersion.dll "\$@"
 EOF
 
 tee ${PREFIX}/bin/buildversion.cmd << EOF
-exec %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\\${PKG_NAME}\buildversion.dll %*
+exec %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\\${PKG_NAME}\FunFair.BuildVersion.dll %*
 EOF
 
 # Download dependency licenses wtih dotnet-project-licenses
-# tee ignored_packages.json << EOF
-# ["Microsoft.DotNet.PlatformAbstractions"]
-# EOF
-# dotnet-project-licenses --input src/FunFair.BuildVersion/FunFair.BuildVersion.csproj -t -d license-files -ignore ignored_packages.json
-dotnet-project-licenses --input src/FunFair.BuildVersion/FunFair.BuildVersion.csproj -t -d license-files
+tee ignored_packages.json << EOF
+["Nullable.Extended.Analyzer", "LibGit2Sharp*", "Credfeto.Enumeration.Source.Generation*", "CommandLineParser", "FunFair.CodeAnalysis"]
+EOF
+dotnet-project-licenses --input src/FunFair.BuildVersion/FunFair.BuildVersion.csproj -t -d license-files -ignore ignored_packages.json
