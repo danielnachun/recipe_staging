c14bad52b9d89bade3824c2cae5f120c0fbfcdd4
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-06 00:40:53 -0700

    add changelog-manager

--- changelog-manager/build.sh
+++ changelog-manager/build.sh
@@ -8,22 +8,11 @@ mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 # Build package with dotnet publish
 rm -rf global.json
 framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
-dotnet publish --no-self-contained src/Credfeto.ChangeLog.Cmd/Credfeto.ChangeLog.Cmd.csproj --output ${PREFIX}/libexec/${PKG_NAME} --framework "net${framework_version}"
-rm ${PREFIX}/libexec/${PKG_NAME}/changelog
+dotnet publish --no-self-contained src/Credfeto.ChangeLog.Cmd/Credfeto.ChangeLog.Cmd.csproj --output ${PREFIX}/libexec/${PKG_NAME} --framework "net${framework_version}" /p:RunAnalyzers=False -c Release
+ln -sf ${PREFIX}/libexec/${PKG_NAME}/Credfeto.ChangeLog.Cmd ${PREFIX}/bin/changelog
 
-# Create bash and batch wrappers
-tee ${PREFIX}/bin/changelog << EOF
-#!/bin/sh
-exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/changelog/changelog.dll "\$@"
+# Download dependency licenses wtih dotnet-project-licenses
+tee ignored_packages.json << EOF
+["CommandLineParser", "Credfeto.Enumeration.Source.Generation*", "FunFair.CodeAnalysis", "LibGit2Sharp*", "Nullable.Extended.Analyzer"]
 EOF
-
-tee ${PREFIX}/bin/changelog.cmd << EOF
-exec %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\changelog\changelog.dll %*
-EOF
-
-# # Download dependency licenses wtih dotnet-project-licenses
-# tee ignored_packages.json << EOF
-# ["Microsoft.DotNet.PlatformAbstractions"]
-# EOF
-# dotnet-project-licenses --input src/Dotnet.Script/Dotnet.Script.csproj -t -d license-files -ignore ignored_packages.json
-dotnet-project-licenses --input src/Dotnet.Script/Dotnet.Script.csproj -t -d license-files
+dotnet-project-licenses --input src/Credfeto.ChangeLog.Cmd/Credfeto.ChangeLog.Cmd.csproj -t -d license-files -ignore ignored_packages.json
