ac25d58ccd7421b7f13b804a27171909ae8af7f8
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-05 22:06:37 -0700

    add pickles

---
+++ pickles/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+
+# Build package with dotnet publish
+rm -rf global.json
+framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
+sed -i "s?<TargetFramework>.*</TargetFramework>?<TargetFramework>net${framework_version}</TargetFramework>?" \
+    src/Pickles.CommandLine/Pickles.CommandLine.csproj
+dotnet publish --no-self-contained src/Pickles.CommandLine/Pickles.CommandLine.csproj --output ${PREFIX}/libexec/${PKG_NAME}
+rm ${PREFIX}/libexec/${PKG_NAME}/pickles
+
+# Create bash and batch wrappers
+tee ${PREFIX}/bin/pickles << EOF
+#!/bin/sh
+exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/pickles/Pickles.dll "\$@"
+EOF
+
+tee ${PREFIX}/bin/pickles.cmd << EOF
+exec %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\pickles\Pickles.dll %*
+EOF
+
+# Download dependency licenses wtih dotnet-project-licenses
+dotnet-project-licenses --input src/Pickles.CommandLine/Pickles.CommandLine.csproj -t -d license-files
