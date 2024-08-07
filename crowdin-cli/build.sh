6ac077f6af3b7ec2df408a1498e1134327b6bdc2
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-06 21:02:26 -0700

    add crowdin-cli

--- crowdin-cli/build.sh
+++ crowdin-cli/build.sh
@@ -2,6 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+mkdir -p ${PREFIX}/bin
+
 # Add plugin for dependency licenses
 sed -i "s/id 'java'/id 'java'\nid('com.github.jk1.dependency-license-report') version 'latest.release'/" build.gradle
 
@@ -11,10 +14,12 @@ sed -i "s/id 'java'/id 'java'\nid('com.github.jk1.dependency-license-report') ve
 # Download dependency licenses
 ./gradlew generateLicenseReport
 
+cp build/libs/${PKG_NAME}-${PKG_VERSION}.jar ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME}.jar
+
 tee ${PREFIX}/bin/${PKG_NAME} << EOF
-call \${CONDA_PREFIX}/bin/java -jar \${CONDA_PREFIX}/libexec/crowdin-cli.jar \$@
+exec \${JAVA_HOME}/bin/java -jar \${CONDA_PREFIX}/libexec/crowdin-cli/crowdin-cli.jar \$@
 EOF
 
 tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %JAVA_HOME%\bin\java %PREFIX%\bin\crowdin %*
+call %JAVA_HOME%\bin\java -jar %CONDA_PREFIX%\libexec\crowdin-cli\crowdin-cli.jar %*
 EOF
