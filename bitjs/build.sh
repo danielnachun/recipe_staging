9bd4fb282bcb40520a477a92fa8387c181716901
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-07 11:47:26 -0700

    add bitjs

--- bitjs/build.sh
+++ bitjs/build.sh
@@ -3,19 +3,17 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 # Run pnpm so that pnpm-licenses can create report
+pnpm config set '@teambit:registry' https://node-registry.bit.cloud
+pnpm config set '@bitdev:registry' https://node-registry.bit.cloud
 pnpm install
 pnpm pack
 
 # Create package archive and install globally
-npm pack --ignore-scripts
-npm install -ddd \
+pnpm install -ddd \
     --global \
-    --build-from-source \
+    --global-bin-dir ${PREFIX}/bin \
+    --global-dir ${PREFIX} \
     ${SRC_DIR}/teambit-bit-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
-pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
-
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\bit %*
-EOF
+# pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
