0f9d41218904e71298cd3c2b18ef191c267479d1
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-07 13:10:47 -0700

    add gatsby-cli

--- gatsby/build.sh
+++ gatsby/build.sh
@@ -4,10 +4,11 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 cd packages/gatsby
 # Run pnpm so that pnpm-licenses can create report
-pnpm install --ignore-scripts
-pnpm pack
+# pnpm install --ignore-scripts
+# pnpm pack
 
 # Create package archive and install globally
+npm install
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
