131cde9648a18f0f3cf113afd6cd4afa703a0e90
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-11 10:59:06 -0700

    add agda

--- agda/build.sh
+++ agda/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CABAL_DIR=${SRC_DIR}/.cabal
 cabal v2-update
 cabal v2-install \
+    --extra-include-dirs=${PREFIX}/include \
     --extra-lib-dirs=${PREFIX}/lib \
 	--jobs=${CPU_COUNT} \
 	--max-backjumps=100000 \
