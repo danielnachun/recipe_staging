80efe988481394635d1fbef646fcd1a82df2b0df
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-13 01:32:16 -0700

    add menhir (#2614)
    * add menhir
    
    * add menhir
    
    * add menhir

---
+++ menhir/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export OPAMROOT=${SRC_DIR}/.opam
+export OPAMYES=1
+export OPAMDOWNLOADJOBS=${CPU_COUNT}
+export OPAMJOBS=${CPU_COUNT}
+
+opam init --no-setup --disable-sandboxing
+opam exec -- opam install ${PKG_NAME} --deps-only -y --no-depexts
+opam exec -- dune build --profile release
+opam exec -- dune install --prefix ${PREFIX}
