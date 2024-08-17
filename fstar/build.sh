dbaf1b47d2269ceba6172ef457683e27cd00600a
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-16 20:10:15 -0500

    add fstar

---
+++ fstar/build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+
+copy_license() {
+    license_file=$1
+    license_file_dirname=$(basename $(dirname ${license_file}))
+    mkdir -p license-files/${license_file_dirname}
+    cp ${license_file} license-files/${license_file_dirname}
+}
+
+export -f copy_license
+
+export OPAMROOT=${SRC_DIR}/.opam
+export OPAMYES=1
+export OPAMDOWNLOADJOBS=${CPU_COUNT}
+export OPAMJOBS=${CPU_COUNT}
+
+opam init --no-setup --disable-sandboxing
+opam exec -- opam install . -y --deps-only --no-depexts
+pushd ocaml
+opam exec -- dune build --profile release @install
+opam exec -- dune install --prefix ${PREFIX}
+popd
+
+mkdir -p license-files
+odig show license-files --lib-dir=${OPAMROOT}/default/lib --doc-dir=${OPAMROOT}/default/doc -l | cut -f 2 -d ' ' | \
+    xargs -I % bash -c 'copy_license %'
