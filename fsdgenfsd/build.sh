96908d9eda972a363af73f07ffd3899a8a80685a
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-18 17:55:33 -0500

    add fsdgenfsd

---
+++ fsdgenfsd/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+
+# Build package with dotnet publish
+rm -rf global.json
+framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
+dotnet publish --no-self-contained src/fsdgenfsd/fsdgenfsd.csproj --output ${PREFIX}/libexec/${PKG_NAME} --framework "net${framework_version}"
+rm ${PREFIX}/libexec/${PKG_NAME}/fsdgenfsd
+
+# Create bash and batch wrappers
+tee ${PREFIX}/bin/fsdgenfsd << EOF
+#!/bin/sh
+exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/fsdgenfsd/fsdgenfsd.dll "\$@"
+EOF
+
+tee ${PREFIX}/bin/fsdgenfsd.cmd << EOF
+exec %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\fsdgenfsd\fsdgenfsd.dll %*
+EOF
+
+# Download dependency licenses wtih dotnet-project-licenses
+dotnet-project-licenses --input src/fsdgenfsd/fsdgenfsd.csproj -t -d license-files
