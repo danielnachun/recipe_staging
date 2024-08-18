f948cbde2d635de48ecc22f2f537db24302f1e23
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-18 01:51:45 -0500

    add purescript

--- purescript/build.sh
+++ purescript/build.sh
@@ -6,6 +6,7 @@ export CABAL_DIR=${SRC_DIR}/.cabal
 cabal v2-update
 cabal v2-install \
     --allow-newer=base,template-haskell,pretty,deepseq,ghc-prim,th-abstraction,memory,lens \
+    --constraint "zlib +pkg-config" \
     --extra-lib-dirs=${PREFIX}/lib \
 	--jobs=${CPU_COUNT} \
 	--max-backjumps=100000 \
