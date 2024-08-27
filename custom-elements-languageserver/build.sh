2984615047bd23d44c9e49025c7fba0643d858e8
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-27 13:08:31 -0500

    add custom-elements-languageserver

--- custom-elements-languageserver/build.sh
+++ custom-elements-languageserver/build.sh
@@ -2,18 +2,12 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-# Run pnpm so that pnpm-licenses can create report
-cd lib/server
-version="$(jq '.version' package.json | tr -d '\"')"
-
 # Create package archive and install globally
-npm install
-npm run build
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source \
-    ${SRC_DIR}/lib/server/${PKG_NAME}-${version}.tgz
+    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
 
 tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
 call %CONDA_PREFIX%\bin\node %PREFIX%\bin\custom-elements-languageserver %*
