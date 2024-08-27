668d8d0c183fbbe1b781b11279dd3a62c91b13cc
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-27 14:13:50 -0500

    add lean-language-server

--- lean-language-server/build.sh
+++ lean-language-server/build.sh
@@ -3,21 +3,17 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 # Run pnpm so that pnpm-licenses can create report
-cd lean-language-server
 pnpm install
-pnpm run build
-pnpm pack
 
 # Create package archive and install globally
-version=$(jq '.version' package.json | tr -d '"')
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source \
-    ${SRC_DIR}/${PKG_NAME}/${PKG_NAME}-${version}.tgz
+    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
-pnpm-licenses generate-disclaimer --prod --output-file=${SRC_DIR}/third-party-licenses.txt
+pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
 tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
 call %CONDA_PREFIX%\bin\node %PREFIX%\bin\lean-language-server %*
