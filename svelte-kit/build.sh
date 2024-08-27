83b85806529480e9863679ea2afbe2c42c7d3539
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-27 11:27:53 -0500

    add svelte-kit (#2661)
    * add svelte-kit
    
    * add svelte-kit

--- svelte-kit/build.sh
+++ svelte-kit/build.sh
@@ -2,24 +2,15 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-pnpm install
-pnpm run build
-
 # Run pnpm so that pnpm-licenses can create report
-cd packages/kit
 pnpm install
-pnpm pack
 
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source \
-    ${SRC_DIR}/packages/kit/sveltejs-kit-${PKG_VERSION}.tgz
+    ${SRC_DIR}/sveltejs-kit-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
 pnpm-licenses generate-disclaimer --prod --output-file=${SRC_DIR}/third-party-licenses.txt
-
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\svelte-kit %*
-EOF
