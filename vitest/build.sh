343253959f21e92d431d1f29bc0caeebccd5873d
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-27 11:28:12 -0500

    add vitest (#2664)

--- vitest/build.sh
+++ vitest/build.sh
@@ -2,20 +2,15 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-pnpm install
-pnpm run build
-cd packages/vitest
-
 # Run pnpm so that pnpm-licenses can create report
 pnpm install
-pnpm pack
 
 # Create package archive and install globally
-pnpm install -ddd \
-    --global-bin-dir=${PREFIX}/bin \
-    --global-dir=${PREFIX} \
+npm pack --ignore-scripts
+npm install -ddd \
     --global \
-    ${SRC_DIR}/packages/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tgz
+    --build-from-source \
+    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
-# pnpm-licenses generate-disclaimer --prod --output-file=${SRC_DIR}/third-party-licenses.txt
+pnpm-licenses generate-disclaimer --prod --output-file=${SRC_DIR}/third-party-licenses.txt
