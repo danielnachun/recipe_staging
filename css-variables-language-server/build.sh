bcfc8831e9f74cca72196b5c696e7f994123ccef
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-27 12:59:26 -0500

    add css-variables-language-server

--- css-variables-language-server/build.sh
+++ css-variables-language-server/build.sh
@@ -3,20 +3,17 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 # Run pnpm so that pnpm-licenses can create report
-cd packages/css-variables-language-server
 pnpm install
-pnpm run build
-pnpm pack
 
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source \
-    ${SRC_DIR}/packages/css-variables-language-server/${PKG_NAME}-${PKG_VERSION}.tgz
+    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
-pnpm-licenses generate-disclaimer --prod --output-file=${SRC_DIR}/third-party-licenses.txt
+pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
 tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
 call %CONDA_PREFIX%\bin\node %PREFIX%\bin\css-variables-language-server %*
