9d8cbc4353d975301aca3f5d7dac5423d43ec1d4
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-27 11:28:08 -0500

    add vitepress (#2663)

--- vitepress/build.sh
+++ vitepress/build.sh
@@ -4,10 +4,8 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 # Run pnpm so that pnpm-licenses can create report
 pnpm install
-pnpm pack
 
 # Create package archive and install globally
-npm run build
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
