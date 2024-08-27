fa198934128dfc27450fa5d457e5b178cc4e86e7
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-27 13:49:49 -0500

    add emmet-ls

--- emmet-ls/build.sh
+++ emmet-ls/build.sh
@@ -4,16 +4,13 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 # Run pnpm so that pnpm-licenses can create report
 pnpm install
-pnpm run build
-pnpm pack
 
 # Create package archive and install globally
-version=$(jq '.version' package.json | tr -d '"')
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source \
-    ${SRC_DIR}/${PKG_NAME}-${version}.tgz
+    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
