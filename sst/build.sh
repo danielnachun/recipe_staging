4973407b02672d1fbb4ea6eb2bca1b046a04ed00
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-27 11:28:45 -0500

    add sst (#2668)

--- sst/build.sh
+++ sst/build.sh
@@ -2,24 +2,15 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-pnpm install
-pnpm run build
-
 # Run pnpm so that pnpm-licenses can create report
-cd packages/sst/dist
 pnpm install
-pnpm pack
 
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source \
-    ${SRC_DIR}/packages/${PKG_NAME}/dist/${PKG_NAME}-*.tgz
+    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
-pnpm-licenses generate-disclaimer --prod --output-file=${SRC_DIR}/third-party-licenses.txt
-
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\sst %*
-EOF
+pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
