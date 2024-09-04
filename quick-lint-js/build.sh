3f4a6479bc265a10ebff232c4e37ce23cb7cccc5
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-04 01:47:59 -0700

    add quick-lint-js

--- quick-lint-js/build.sh
+++ quick-lint-js/build.sh
@@ -3,6 +3,9 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 cmake -S . -B build \
+    -DQUICK_LINT_JS_ENABLE_BENCHMARKS=OFF \
+    -DQUICK_LINT_JS_INSTALL_VIM_NEOVIM_TAGS=ON \
+    -DQUICK_LINT_JS_USE_BUNDLED_SIMDJSON=OFF \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_VERBOSE_MAKEFILE=ON \
     -Wno-dev \
