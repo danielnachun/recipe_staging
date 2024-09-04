6dffd1817f2eba3509782ae3e4dbe1fd2f7e9247
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-04 02:29:02 -0700

    add lerna (#2741)

--- lerna/build.sh
+++ lerna/build.sh
@@ -2,20 +2,15 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-npm install
-npm run build
-
 # Run pnpm so that pnpm-licenses can create report
-cd dist/packages/lerna
 pnpm install
-pnpm pack
 
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source \
-    ${SRC_DIR}/dist/packages/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tgz
+    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
 pnpm-licenses generate-disclaimer --prod --output-file=${SRC_DIR}/third-party-licenses.txt
