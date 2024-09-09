7ad2be892ea415d003c8dd686bb376b3b694feac
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-09 12:16:28 -0700

    add fish-lsp

--- fish-lsp/build.sh
+++ fish-lsp/build.sh
@@ -2,10 +2,13 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-# Run pnpm so that pnpm-licenses can create report
-# mv package.json package.json.bak
-# jq 'del(.scripts.preinstall)' package.json.bak > package.json
-pnpm install --ignore-scripts
+mv package.json package.json.bak
+jq 'del(.scripts.preinstall)' package.json.bak > package.json
+npm install --ignore-scripts
+npm install yarn@1
+npm uninstall tsc
+npm install typescript
+npm run compile
 
 # Create package archive and install globally
 npm pack --ignore-scripts
@@ -15,6 +18,7 @@ npm install -ddd \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
+pnpm install --ignore-scripts
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
 tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
