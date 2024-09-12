08ecc875f1c883d2431d199d596c4859efa94d19
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-12 14:09:13 -0700

    add kotlin-language-server

--- kotlin-language-server/build.sh
+++ kotlin-language-server/build.sh
@@ -8,9 +8,9 @@ mkdir -p ${PREFIX}/bin
 
 sed -i 's/kotlin("jvm")/kotlin("jvm")\nid("com.github.jk1.dependency-license-report") version "latest.release"/' build.gradle.kts
 
-./gradlew :server:build -PjavaVersion=21
+./gradlew :server:build -PjavaVersion=21 -x test
 
-./gradlew generateLicenseReport -PjavaVersion=21
+./gradlew generateLicenseReport -PjavaVersion=21 -x test
 
 cp -r ${SRC_DIR}/server/build/install/server/lib ${PREFIX}/libexec/${PKG_NAME}
 cp -r ${SRC_DIR}/server/build/install/server/bin ${PREFIX}/libexec/${PKG_NAME}
