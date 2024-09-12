dbe47639f067ab316db253c1f421290868f6dad3
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-11 23:59:46 -0700

    add spago

--- spago/build.sh
+++ spago/build.sh
@@ -2,12 +2,19 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-export CABAL_DIR=${SRC_DIR}/.cabal
-cabal v2-update
-cabal v2-install \
-    --extra-lib-dirs=${PREFIX}/lib \
-	--jobs=${CPU_COUNT} \
-	--max-backjumps=100000 \
-	--install-method=copy \
-	--installdir=${PREFIX}/bin
-cabal-plan license-report --licensedir=${SRC_DIR}/license-files exe:${PKG_NAME}
+# Run pnpm so that pnpm-licenses can create report
+pnpm install
+
+# Create package archive and install globally
+npm pack --ignore-scripts
+npm install -ddd \
+    --global \
+    --build-from-source \
+    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
+
+# Create license report for dependencies
+pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
+
+tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
+call %CONDA_PREFIX%\bin\node %PREFIX%\bin\spago %*
+EOF
