4eed691ad8c9482a2368740e6349dd5ad4f84d0f
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-19 01:01:31 -0600

    add fennel

--- fennel/build.sh
+++ fennel/build.sh
@@ -2,8 +2,11 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+sed -i 's/env $(LUA)/lua/' Makefile
+
 make PREFIX=${PREFIX}
 make install
+
 
 tee ${PREFIX}/bin/fennel.cmd << EOF
 call %PREFIX%\bin\lua %PREFIX%\bin\fennel %*
