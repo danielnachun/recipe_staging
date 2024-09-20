1518bd4a3381b70d0f645d2b330bb7bd2a017e5c
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-20 12:50:11 -0600

    add fennel (#2830)
    * add fennel
    
    * add fennel
    
    * add fennel
    
    * add fennel

---
+++ fennel/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+make PREFIX=${PREFIX}
+make install
+
+sed -i "s|${PREFIX}/bin/||" ${PREFIX}/bin/fennel
+
+tee ${PREFIX}/bin/fennel.cmd << EOF
+call %PREFIX%\bin\lua %PREFIX%\bin\fennel %*
+EOF
