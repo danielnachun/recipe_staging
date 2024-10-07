d5a8dbad442630ae7d12504001dbc50cfb451b69
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-07 01:51:39 -0700

    add neu (#2881)
    * add neu
    
    * add neu
    
    * add neu
    
    * add neu

---
+++ neu/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+if [[ ${target_platform} == "osx-64" ]]; then
+    export CFLAGS=${CFLAGS//-march=core2/}
+    export CFLAGS=${CFLAGS//-mtune=haswell/}
+    export CXXFLAGS=${CXXFLAGS//-march=core2/}
+    export CXXFLAGS=${CXXFLAGS//-mtune=haswell/}
+fi
+
+# Create package archive and install globally
+npm pack --ignore-scripts
+npm install -ddd \
+    --global \
+    --build-from-source \
+    ${SRC_DIR}/neutralinojs-${PKG_NAME}-${PKG_VERSION}.tgz
+
+# Create license report for dependencies
+pnpm install
+pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
