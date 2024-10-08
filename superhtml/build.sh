aac03b141b1a0d50839d84271305f691a843081d
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-07 17:59:32 -0700

    add superhtml

---
+++ superhtml/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+unset HTTP_PROXY
+unset HTTPS_PROXY
+if [[ ${target_platform} == "linux-64" ]]; then
+    zig build --prefix ${PREFIX} -Drelease-fast=true -Dcpu=core2 -Dtarget=native-native-gnu.2.17
+fi
+if [[ ${target_platform} == "osx-64" ]]; then
+    zig build --prefix ${PREFIX} -Doptimize=ReleaseFast -Dcpu=core2
+fi
+if [[ ${target_platform} == "osx-arm64" ]]; then
+    zig build --prefix ${PREFIX} -Doptimize=ReleaseFast -Dcpu=apple_m1
+fi
