56e9a8d92cc8ca4a6b6f74cf5025b72157f6b85c
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-07 18:13:53 -0700

    ncdu 2.6

--- ncdu/build.sh
+++ ncdu/build.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+unset HTTP_PROXY
+unset HTTPS_PROXY
+
 if [[ ${target_platform} == "linux-64" ]]; then
     zig build --prefix ${PREFIX} -Drelease-fast=true -Dcpu=core2 -Dtarget=native-native-gnu.2.17
 fi
@@ -11,5 +14,6 @@ fi
 if [[ ${target_platform} == "osx-arm64" ]]; then
     zig build --prefix ${PREFIX} -Dpie=true -Doptimize=ReleaseFast -Dcpu=apple_m1
 fi
+
 mkdir -p ${PREFIX}/share/man/man1
 install -m 755 ncdu.1 ${PREFIX}/share/man/man1/ncdu.1
