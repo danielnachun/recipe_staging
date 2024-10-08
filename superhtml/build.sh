87f9d53a4bb551287ca7540d886e8388db950ea0
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-07 18:54:33 -0700

    add superhtml

--- superhtml/build.sh
+++ superhtml/build.sh
@@ -5,7 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 unset HTTP_PROXY
 unset HTTPS_PROXY
 if [[ ${target_platform} == "linux-64" ]]; then
-    zig build --prefix ${PREFIX} -Drelease-fast=true -Dcpu=baseline -Dtarget=x86_64-linux-gnu.2.17
+    zig build --prefix ${PREFIX} -Drelease-fast=true -Dcpu=core2 -Dtarget=x86_64-linux-gnu.2.17
 fi
 if [[ ${target_platform} == "osx-64" ]]; then
     zig build --prefix ${PREFIX} -Doptimize=ReleaseFast -Dcpu=core2
