eeeb342d85e84ca4c36a0c1cf5b9108ff6fc207d
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-11 17:39:21 -0700

    add go-git

--- go-git/bld.bat
+++ go-git/bld.bat
@@ -1,5 +1,6 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" .\cli\go-git || goto :error
-go-licenses save .\cli\go-git --save_path=license-files || goto :error
+cd cli\go-git || goto :error
+go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" || goto :error
+go-licenses save . --save_path=license-files --ignore=github.com/go-git || goto :error
 
 goto :EOF
 
