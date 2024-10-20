28d98f1f83a6c2a81bc6e4c01179834dfa253d12
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-20 15:59:17 -0700

    add retroseq

--- retroseq/build.sh
+++ retroseq/build.sh
@@ -2,9 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-mkdir -p ${PREFIX}/lib/perl5/vendor_perl
+mkdir -p ${PREFIX}/lib/perl5/site_perl/RetroSeq
 
-install -m 644 ${SRC_DIR}/RetroSeq/Utilities.pm ${PREFIX}/lib/perl5/vendor_perl/RetroSeq/Utilities.pm
-install -m 644 ${SRC_DIR}/RetroSeq/Vcf.pm ${PREFIX}/lib/perl5/vendor_perl/RetroSeq/Vcf.pm
+install -m 644 ${SRC_DIR}/RetroSeq/Utilities.pm ${PREFIX}/lib/perl5/site_perl/RetroSeq/Utilities.pm
+install -m 644 ${SRC_DIR}/RetroSeq/Vcf.pm ${PREFIX}/lib/perl5/site_perl/RetroSeq/Vcf.pm
 
 install -m 755 ${SRC_DIR}/RetroSeq/retroseq.prun.pl ${PREFIX}/bin/retroseq
