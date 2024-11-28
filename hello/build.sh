6c59bee39a3e92cbdc24dd905716ef5e65d5d4ea
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-28 12:11:46 -0800

    hello: add missing deps (#2970)
    * hello: add missing deps
    
    * hello: add missing deps
    
    * hello: add missing deps

--- hello/build.sh
+++ hello/build.sh
@@ -6,6 +6,9 @@ if [[ $target_platform =~ .*osx.* ]]; then
     LDFLAGS="-liconv"
 fi
 
+sed -i 's/AM_GNU_GETTEXT_VERSION/AM_GNU_GETTEXT_REQUIRE_VERSION/' configure.ac
+
+autoreconf --force --install --verbose
 ./configure --disable-silent \
     --disable-dependency-tracking \
     --prefix=${PREFIX} 
