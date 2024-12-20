60b0af2490e098603b205961a1ec0459d50d7e3b
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-20 14:50:56 -0800

    add mcscanx

--- mcscanx/build.sh
+++ mcscanx/build.sh
@@ -2,15 +2,49 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-if [[ ${target_platform} =~ .*linux.* ]]; then
-    cxx_stdlib="stdc++"
-else
-    cxx_stdlib="c++"
-fi
+create_java_wrapper() {
+    exe_name=$(basename $1)
 
-make CC=${CC} \
-    CFLAGS="-fopenmp -D_USE_OMP -O3 -fomit-frame-pointer -funroll-loops ${CXXFLAGS} " \
-    LFLAGS="-std=c++11 -I ./ ${LDFLAGS} -lm -l${cxx_stdlib}"
+tee ${PREFIX}/bin/${exe_name//.class/} << EOF
+#!/bin/sh
+exec \${JAVA_HOME}/bin/java -classpath \${CONDA_PREFIX}/libexec/MCScanX ${exe_name//.class/}
+EOF
+    chmod +x ${PREFIX}/bin/${exe_name//.class/}
+}
+
+export -f create_java_wrapper
+
+add_perl_shebang() {
+    exe_name=$(basename $1)
+
+    sed -i '/#!\/usr\/bin\/perl -w/d' ${SRC_DIR}/downstream_analyses/${exe_name}
+    sed -i '1i #!/usr/bin/env perl' ${SRC_DIR}/downstream_analyses/${exe_name}
+}
+
+export -f add_perl_shebang
+
+make CXX="${CXX} ${CXXFLAGS} ${LDFLAGS}"
 
 mkdir -p ${PREFIX}/bin
-install -m 755 cnef ${PREFIX}/bin/cnef
+install -m 755 MCScanX ${PREFIX}/bin
+install -m 755 MCScanX_h ${PREFIX}/bin
+install -m 755 duplicate_gene_classifier ${PREFIX}/bin
+install -m 755 downstream_analyses/detect_collinear_tandem_arrays ${PREFIX}/bin
+install -m 755 downstream_analyses/dissect_multiple_alignment ${PREFIX}/bin
+
+mkdir -p ${PREFIX}/libexec/MCScanX
+cp downstream_analyses/*.class ${PREFIX}/libexec/MCScanX
+java_exes=(
+    bar_plotter
+    circle_plotter
+    dot_plotter
+    dual_synteny_plotter
+    family_circle_plotter
+    family_tree_plotter
+    family_tree_plotter_chr
+    family_tree_plotter_show_length
+)
+find ${PREFIX}/libexec/MCScanX -name *.class | xargs -I % bash -c "create_java_wrapper %"
+
+find ${SRC_DIR}/downstream_analyses -name *.pl | xargs -I % bash -c "add_perl_shebang %"
+install -m 755 downstream_analyses/*.pl ${PREFIX}/bin
