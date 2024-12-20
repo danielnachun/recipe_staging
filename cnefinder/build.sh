140b39302c5a3dced63e07f18c6835112cd13eb6
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-20 15:02:59 -0800

    add cnefinder (#3017)
    * add cnefinder
    
    * add cnefinder

---
+++ cnefinder/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+if [[ ${target_platform} =~ .*linux.* ]]; then
+    cxx_stdlib="stdc++"
+else
+    cxx_stdlib="c++"
+fi
+
+make CC=${CC} \
+    CFLAGS="-fopenmp -D_USE_OMP -O3 -fomit-frame-pointer -funroll-loops ${CXXFLAGS} " \
+    LFLAGS="-std=c++11 -I ./ ${LDFLAGS} -lm -l${cxx_stdlib}"
+
+mkdir -p ${PREFIX}/bin
+install -m 755 cnef ${PREFIX}/bin/cnef
