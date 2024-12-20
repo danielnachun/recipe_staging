f66000cc1ff37803dafc16bcf375f66eabe6bd60
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-20 00:11:19 -0800

    add cnefinder

--- cnefinder/build.sh
+++ cnefinder/build.sh
@@ -10,7 +10,7 @@ fi
 
 make CC=${CC} \
     CFLAGS="-fopenmp -D_USE_OMP -O3 -fomit-frame-pointer -funroll-loops ${CXXFLAGS} " \
-    LFLAGS="-std=c++11 -I ./ ${LDFLAGS} -l${cxx_stdlib}"
+    LFLAGS="-std=c++11 -I ./ ${LDFLAGS} -lm -l${cxx_stdlib}"
 
 mkdir -p ${PREFIX}/bin
 install -m 755 cnef ${PREFIX}/bin/cnef
