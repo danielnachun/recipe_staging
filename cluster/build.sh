7407fecaa55eaa501a5d27400378991a4019bea7
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-20 00:34:28 -0800

    add cluster

---
+++ cluster/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+./configure --disable-silent \
+    --disable-dependency-tracking \
+    --without-x \
+    --prefix=${PREFIX}
+make
+make install
