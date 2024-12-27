73400cb7605dcd355bd61cc2fffe5ff26a24c175
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-26 23:31:23 -0600

    perl-xml-libxml: fix recipe

--- perl-xml-libxml/build.sh
+++ perl-xml-libxml/build.sh
@@ -15,22 +15,11 @@ fi
 
 # If it has Build.PL use that, otherwise use Makefile.PL
 export LD_LIBRARY_PATH="${PREFIX}/lib"
-if [ -f Build.PL ]; then
-    perl Build.PL
-    ./Build
-    ./Build test
-    # Make sure this goes in site
-    ./Build install --installdirs site
-elif [ -f Makefile.PL ]; then
-    # Disable check_lib, it incorrectly tries -lnsl, which perl is not actually compiled againt
-    sed -i.bak 's/ check_lib/    print $conf_LIBS;\
-    check_lib/g' Makefile.PL
-    # Make sure this goes in site
-    perl Makefile.PL INSTALLDIRS=site LDFLAGS="$LDFLAGS" CCFLAGS="${CFLAGS}" LIBS="-L${PREFIX}/lib -lxml2 -lz -llzma -liconv -licui18n -licuuc -licudata -lm -ldl" INC="-I$PREFIX/include/libxml2 -I$PREFIX/include"
-    make
-    #make test
-    make install
-else
-    echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
-    exit 1
-fi
+# Disable check_lib, it incorrectly tries -lnsl, which perl is not actually compiled againt
+sed -i.bak 's/ check_lib/    print $conf_LIBS;\
+check_lib/g' Makefile.PL
+# Make sure this goes in site
+perl Makefile.PL INSTALLDIRS=site LDFLAGS="$LDFLAGS" CCFLAGS="${CFLAGS}" LIBS="-L${PREFIX}/lib -lxml2 -lz -llzma -liconv -licui18n -licuuc -licudata -lm -ldl" INC="-I$PREFIX/include/libxml2 -I$PREFIX/include"
+make
+#make test
+make install
