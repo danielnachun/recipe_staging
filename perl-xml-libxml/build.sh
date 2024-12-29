8e907231900a7dcb04699891dbbf321e43d4be53
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-28 18:00:32 -0600

    add perl-xml-libxml

--- perl-xml-libxml/build.sh
+++ perl-xml-libxml/build.sh
@@ -7,10 +7,11 @@ if [ `uname -s` == "Darwin" ]; then
 
     # Give install_name_tool enough room to work its magic
     LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
-    CFLAGS="${CFLAGS} -Wno-incompatible-pointer-types -Doff64_t=__off64_t"
+    CFLAGS="${CFLAGS} -Wno-incompatible-function-pointer-types"
 else
     # Force use of conda's libxml instead of the system one
     export LD_LIBRARY_PATH="${PREFIX}/lib"
+    CFLAGS="${CFLAGS} -Wno-incompatible-pointer-types -Doff64_t=__off64_t"
 fi
 
 # If it has Build.PL use that, otherwise use Makefile.PL
@@ -19,8 +20,8 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 sed -i.bak 's/ check_lib/    print $conf_LIBS;\
 check_lib/g' Makefile.PL
 # Make sure this goes in site
-perl Makefile.PL INSTALLDIRS=site 
-make LDFLAGS="$LDFLAGS" CCFLAGS="${CFLAGS}" LIBS="-L${PREFIX}/lib -lxml2 -lz -llzma -liconv -licui18n -licuuc -licudata -lm -ldl" INC="-I$PREFIX/include/libxml2 -I$PREFIX/include"
+perl Makefile.PL INSTALLDIRS=site LDFLAGS="$LDFLAGS" CCFLAGS="${CFLAGS}" LIBS="-L${PREFIX}/lib -lxml2 -lz -llzma -liconv -licui18n -licuuc -licudata -lm -ldl" INC="-I$PREFIX/include/libxml2 -I$PREFIX/include"
+make
 #make test
 make install
 
