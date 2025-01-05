4c39a29158f9333a7b58dea4369f1e80c46146e3
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-05 10:49:39 -0600

    add tectonic

---
+++ tectonic/build.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -ex
+
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+export PKG_CONFIG_ALLOW_CROSS=1
+
+# Backport of https://github.com/tectonic-typesetting/tectonic/commit/6b49ca8db40aaca29cb375ce75add3e575558375
+cargo add time@0.3.36
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+cargo install --bins --no-track --locked --features external-harfbuzz --root $PREFIX --path .
