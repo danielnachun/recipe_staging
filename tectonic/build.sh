a32f315b2f5200cbb139e95c9886ca8b853b231d
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-05 11:00:10 -0600

    add tectonic

--- tectonic/build.sh
+++ tectonic/build.sh
@@ -9,5 +9,7 @@ export PKG_CONFIG_ALLOW_CROSS=1
 # Backport of https://github.com/tectonic-typesetting/tectonic/commit/6b49ca8db40aaca29cb375ce75add3e575558375
 cargo add time@0.3.36
 
+export CXXFLAGS="${CXXFLAGS} -std=c++17"
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 cargo install --bins --no-track --locked --features external-harfbuzz --root $PREFIX --path .
