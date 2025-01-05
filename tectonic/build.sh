6ee182493f9dd6bf5ba06ef99561bdfc6606f7e5
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-05 11:43:24 -0600

    add tectonic

--- tectonic/build.sh
+++ tectonic/build.sh
@@ -9,7 +9,7 @@ export PKG_CONFIG_ALLOW_CROSS=1
 # Backport of https://github.com/tectonic-typesetting/tectonic/commit/6b49ca8db40aaca29cb375ce75add3e575558375
 cargo add time@0.3.36
 
-sed -i '/"-std=c++14",/d' crates/engine_xetex/build.rs
+sed -i 's/c++14/c++17/' crates/engine_xetex/build.rs
 
 # export CXXFLAGS="${CXXFLAGS} -std=c++17"
 
