aef50ba7ce4144de54dcf4e6277a2bde2a0178be
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 20:00:36 -0800

    add gcal (#3104)
    * add gcal
    
    * add gcal

---
+++ gcal/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CFLAGS="${CFLAGS} -Wno-implicit-function-declaration"
+
+autoreconf --force --verbose --install
+./configure --disable-silent \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} 
+make install
