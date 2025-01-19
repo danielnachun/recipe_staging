a3a21a4a08fb08d1be76198cd8f7085dc2355779
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 17:30:09 -0800

    libpaper: fix recipe

--- libpaper/build.sh
+++ libpaper/build.sh
@@ -2,6 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+autoreconf --force --verbose --install
 ./configure --disable-silent \
     --disable-dependency-tracking \
     --prefix=${PREFIX} \
