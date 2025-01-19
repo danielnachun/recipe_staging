bc29e81020d6944898363117c05ca60ca62843c9
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 21:37:56 -0800

    add cflow

---
+++ cflow/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+./configure --disable-silent-rules \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX}
+make install
