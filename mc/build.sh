b77a6c3626411aa37bd6cc1c56809b53721b9d6e
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 21:31:56 -0800

    add mc

---
+++ mc/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+./configure --disable-silent-rules \
+    --without-x \
+    --with-screen=slang \
+    --disable-dependency-tracking \
+    --enable-vfs-sftp \
+    --prefix=${PREFIX}
+make install
