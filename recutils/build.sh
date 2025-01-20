119c11c835a821d811e2c419e0f51ee18d673c98
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-20 00:09:48 -0800

    autoreconf

--- recutils/build.sh
+++ recutils/build.sh
@@ -3,6 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 export CFLAGS="${CFLAGS} -Wno-implicit-function-declaration"
+autoreconf --force --verbose --install
 ./configure --disable-silent-rules \
     --disable-dependency-tracking \
     --prefix=${PREFIX}
