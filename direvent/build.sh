51c638b8063b315a7bb8ec152d0e8dc483c58903
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-20 20:50:31 -0800

    add direvent

---
+++ direvent/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CFLAGS="${CFLAGS} -Wno-implicit-function-declaration"
+./configure --disable-silent-rules \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} 
+make install
