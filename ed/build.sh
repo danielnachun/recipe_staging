c41bb8fba61e59702b48b4e5530c3f6fd73f8eea
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-20 20:38:28 -0800

    add ed

---
+++ ed/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+tar --lzip --extract --verbose --file=ed-${PKG_VERSION}.tar.lz
+cd ${SRC_DIR}/ed-${PKG_VERSION}
+
+./configure --disable-silent-rules \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} 
+make install
