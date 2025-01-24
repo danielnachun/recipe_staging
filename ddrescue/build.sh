9ccdbf1a9ac9d1f53c9c2ca55bee289050509de5
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-24 09:51:58 -0800

    add ddrescue (#3112)
    * add ddrescue
    
    * add ddrescue

---
+++ ddrescue/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+tar --lzip --extract --verbose --file=ddrescue-${PKG_VERSION}.tar.lz
+cd ${SRC_DIR}/ddrescue-${PKG_VERSION}
+
+./configure --disable-silent-rules \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX}
+
+make install \
+    CXX="${CXX}" \
+    CXXFLAGS="${CXXFLAGS}" \
+    LDFLAGS="${LDFLAGS}"
