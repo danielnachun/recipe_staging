b3d186ab3360c8c8307773d77be790a379c45f72
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-24 08:07:20 -0800

    add cppi (#3105)
    * add cppi
    
    * add xz

---
+++ cppi/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+autoreconf --force --verbose --install
+./configure --disable-silent-rules \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} 
+make install
