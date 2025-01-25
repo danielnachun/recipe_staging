233d4f4d105899ef551d0e1d63016089fa2ff2fc
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-25 10:58:09 -0800

    fix build

--- recode/build.sh
+++ recode/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export PYTHON=${BUILD_PREFIX}/bin/python
 export ACLOCAL_PATH=${PREFIX}/share/aclocal
+export LEX=${BUILD_PREFIX}/bin/flex
 
 ./bootstrap
 ./configure --disable-silent-rules \
