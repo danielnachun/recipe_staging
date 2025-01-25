cb87f26be8a7012868d33b572c08c31c95a22d1f
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-25 00:40:55 -0800

    add recode

---
+++ recode/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export PYTHON=${BUILD_PREFIX}/bin/python
+./configure --disable-silent-rules \
+    --disable-dependency-tracking \
+    --without-included-gettext \
+    --prefix=${PREFIX}
+make install
