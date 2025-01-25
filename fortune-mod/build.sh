d72bf8f3a2c378e96d1f76af2ec1802c834ddc3c
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-25 11:52:50 -0800

    fix build

--- fortune-mod/build.sh
+++ fortune-mod/build.sh
@@ -2,6 +2,11 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+sed -i 's/"games"/"bin"/' fortune-mod/CMakeLists.txt
+sed -i 's/"manpages"/"--basepath",\n$ENV{DOCBOOK_BASE_PATH},\n"manpages"/' fortune-mod/util/FortuneMod/ManPage/Generate/App.pm
+
+export DOCBOOK_BASE_PATH="${BUILD_PREFIX}/share/docbook-xsl"
+
 cmake -S fortune-mod -B build \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_VERBOSE_MAKEFILE=ON \
