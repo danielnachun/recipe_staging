3ad271afdf10c9b50bbd8956e74bc24b044f22f9
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-24 23:28:35 -0800

    add fortune-mod

---
+++ fortune-mod/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+cmake -S . -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -DNO_OFFENSIVE=ON \
+    -DLOCALDIR=${PREFIX}/share/fortune \
+    -DCOOKIEDIR=${PREFIX}/share/fortune \
+    -Wno-dev \
+    -DBUILD_TESTING=OFF \
+    ${CMAKE_ARGS}
+
+cmake --build build -j${CPU_COUNT}
+cmake --install build
