588bc113dd1a66eee179e489b04be7e7fd496e35
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-24 23:40:53 -0800

    add fortune-mod

--- fortune-mod/build.sh
+++ fortune-mod/build.sh
@@ -2,7 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cmake -S . -B build \
+cmake -S fortune-mod -B build \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_VERBOSE_MAKEFILE=ON \
     -DNO_OFFENSIVE=ON \
