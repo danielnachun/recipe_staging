d35a8f77fe8895190db3ad4802533956a035cb0b
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-24 23:11:22 -0800

    add ueberzugpp (#3124)
    * add ueberzugpp
    
    * add deps
    
    * add deps
    
    * add deps
    
    * add deps
    
    * add deps
    
    * add deps
    
    * add deps

---
+++ ueberzugpp/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+cmake -S . -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -DENABLE_WAYLAND=OFF \
+    -DENABLE_X11=OFF \
+    -DENABLE_XCB_ERRORS=OFF \
+    -DENABLE_OPENGL=OFF \
+    -Wno-dev \
+    -DBUILD_TESTING=OFF \
+    ${CMAKE_ARGS}
+
+cmake --build build -j${CPU_COUNT}
+cmake --install build
