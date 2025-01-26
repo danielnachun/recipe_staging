c05f5eb11f6b5bba01a3c275a845c1d65c063b74
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-25 17:35:05 -0800

    fix build

--- combine/build.sh
+++ combine/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 sed -i '/AM_C_PROTOTYPES/d' configure.ac
 sed -i '/AUTOMAKE_OPTIONS = ansi2knr/d' src/Makefile.am
 sed -i '/AUTOMAKE_OPTIONS = ansi2knr/d' src/combine_scm/Makefile.am
+sed -i '/@include fdl.texi/d' doc/combine.texinfo
 
 export CFLAGS="${CFLAGS} -Wno-implicit-function-declaration -Wno-implicit-int"
 export INFOPATH="${SRC_DIR}/doc"
