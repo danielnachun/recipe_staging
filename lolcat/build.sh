91b50702eb9730a055cc44cc3b450ab187346939
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-25 16:23:32 -0800

    add lolcat

--- lolcat/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-gem install -N -l -V --ignore-dependencies --bindir "${PREFIX}/bin" ${PKG_NAME}-${PKG_VERSION}.gem
-gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
-
