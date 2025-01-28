5e8b59ebd12061683ee45a7d70988e3577478c71
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-28 01:47:21 -0800

    bioconductor-rgraphviz 1.66.0 (#3200)

--- bioconductor-rdisop/build.sh
+++ bioconductor-rdisop/build.sh
@@ -8,8 +8,4 @@ CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
 CXX14=$CXX" > ~/.R/Makevars
-if [[ ${target_platform} =~ .*osx.* ]]; then
-    sed -i 's/unary_function/__unary_function/' src/imslib/src/ims/utils/compose_f_gx_t.h
-    sed -i 's/binary_function/__binary_function/' src/imslib/src/ims/utils/compose_f_gx_hy_t.h
-fi
-$R CMD INSTALL --build .
+R CMD INSTALL --build .
