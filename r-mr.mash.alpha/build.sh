e9830b1b7465e203555952675916918449c64c6e
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-28 12:32:57 -0800

    r-mr.ash.alpha 0.3.36

--- r-mr.mash.alpha/build.sh
+++ r-mr.mash.alpha/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export DISABLE_AUTOBREW=1
-export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
 
 # shellcheck disable=SC2086
-${R} CMD INSTALL --build . ${R_ARGS}
+R CMD INSTALL --build . ${R_ARGS}
