60ea34e505bd53e3eafba4872e5870459b1544d5
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-28 16:46:56 -0800

    r-pgenlibr 0.4.0 (#3202)
    * r-pgenlibr 0.4.0
    
    * fix build

--- r-pgenlibr/build.sh
+++ r-pgenlibr/build.sh
@@ -3,4 +3,5 @@
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
-${R} CMD INSTALL --build . ${R_ARGS}
+R CMD INSTALL --build . ${R_ARGS}
+rm -rf ${PREFIX}/lib/R/library/pgenlibr/include/include/*.o
