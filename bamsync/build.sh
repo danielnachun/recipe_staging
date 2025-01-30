dd9cee8b4249a6227ce0471cf6161eb630615de9
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-29 21:18:57 -0800

    add bamsync

--- bamsync/build.sh
+++ bamsync/build.sh
@@ -3,6 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 cd rnaseq/bamsync
+sed -i 's/g++/${CXX}/' Makefile
 make CCFLAGS="${CXXFLAGS} -O3 -Wall -Wextra -I${PREFIX}/include/bamtools" LDFLAGS="${LDFLAGS}"
 
 mkdir -p ${PREFIX}/bin
