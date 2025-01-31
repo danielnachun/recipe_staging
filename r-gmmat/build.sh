e1339056fae592ab8b7535b010819d74d2f5bdf0
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-30 16:52:00 -0800

    r-gmmat 1.4.2

--- r-gmmat/build.sh
+++ r-gmmat/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export DISABLE_AUTOBREW=1
-sed -i 's?-Izstd/lib/common?-Izstd/lib/common -DHAVE_WORKING_LOG1P?' configure
 
 # shellcheck disable=SC2086
-${R} CMD INSTALL --build . ${R_ARGS}
+R CMD INSTALL --build . ${R_ARGS}
