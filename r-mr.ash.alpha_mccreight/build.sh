6b2fbad4e26d64f8793085e171632fb3d64711f2
Daniel Nachun <daniel.nachun@gmail.com>
2025-02-20 19:21:50 -0800

    fix mr.ash.alpha.mccreight (#3242)

--- r-mr.ash.alpha_mccreight/build.sh
+++ r-mr.ash.alpha_mccreight/build.sh
@@ -4,7 +4,7 @@ export DISABLE_AUTOBREW=1
 
 sed -i 's/mr.ash.alpha/mr.ash.alpha.mccreight/g' DESCRIPTION
 sed -i 's/mr.ash.alpha/mr.ash.alpha.mccreight/g' NAMESPACE
-sed -i 's/mr.ash.alpha/mr.ash.alpha.mccreight/g' R/RcppExports.R
+sed -i 's/mr\.ash\.alpha/mr.ash.alpha.mccreight/g' R/RcppExports.R
 sed -i 's/mr.ash.alpha/mr.ash.alpha.mccreight/g' R/mr.ash.R
 
 # shellcheck disable=SC2086
