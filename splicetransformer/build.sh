f9daadbe215d1049f6ffb4cb973408cfbbc3675a
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-09 21:42:50 -0700

    add splicetransformer

---
+++ splicetransformer/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+PY_VER=$(python -V | cut -d ' ' -f 2 | cut -d '.' -f 1,2)
+SP_DIR=${PREFIX}/lib/python${PY_VER}/site-packages
+mkdir -p ${SP_DIR}/sptransformer
+
+sed -i 's/from model/from sptransformer.model/' tasks_annotate_mutations.py
+sed -i 's/from tasks_annotate_mutations/from sptransformer.tasks_annotate_mutations/' tasks_annotate_mutations.py
+
+cp -r ${SRC_DIR}/model ${SP_DIR}/sptransformer
+install -m 644 tasks_annotate_mutations.py ${SP_DIR}/sptransformer
+
+mkdir -p ${PREFIX}/bin
+install -m 755 sptransformer.py ${PREFIX}/bin
