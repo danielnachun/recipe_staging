9556883c8fe4d78b7f6668cf5e77a308dd8c6328
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-09 23:24:05 -0700

    add libsrtp

---
+++ libsrtp/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+cmake -S . -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_STATIC_LIBS=OFF \
+    -Wno-dev \
+    -DBUILD_TESTING=OFF \
+    ${CMAKE_ARGS}
+
+cmake --build build -j${CPU_COUNT}
+cmake --install build
