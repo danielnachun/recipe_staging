ce82d68bc1eedee89499a403a8892e6780db096c
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-10 21:02:55 -0700

    add qsv (#3296)
    * add qsv
    
    * add qsv
    
    * add qsv

--- qsv/build.sh
+++ qsv/build.sh
@@ -2,19 +2,26 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
+export PATH=$(echo ${PATH} | tr ':' '\n' | grep -v "pixi" | tr '\n' ':')
+
 cargo-bundle-licenses \
     --format yaml \
     --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
-export CXXFLAGS="${CXXFLAGS} -include stdint.h"
+export LIBCLANG_PATH="${PREFIX}/lib"
 if [[ ${target_platform} =~ .*osx.* ]]; then
     export CFLAGS="${CFLAGS} -fno-define-target-os-macros"
 fi
-cargo install --features all_features --locked --root ${PREFIX} --path .
 
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/${PKG_NAME}"
+# If we are NOT cross compiling, supply the path to the python interpreter given by ${PYTHON}
+# If we are cross compiling, the cross-python package will provide these variables instead.
+if [[ ${build_platform} == ${target_platform} ]]; then
+    export PYO3_PYTHON=${PYTHON}
+fi
 
-# remove extra build file
-rm -f "${PREFIX}/.crates.toml"
+export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
+cargo install --features all_features --bins --no-track --locked --root ${PREFIX} --path .
