d6dc2251df9cbe2bfe1fce87bd89a90fae967c74
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-10 21:02:46 -0700

    add keepassxc (#3295)
    * add keepassxc
    
    * add keepassxc
    
    * add keepassxc
    
    * add keepassxc
    
    * add keepassxc
    
    * add keepassxc
    
    * add keepassxc
    
    * add keepassxc
    
    * add keepassxc
    
    * add keepassxc
    
    * add keepassxc
    
    * add keepassxc
    
    * add keepassxc

---
+++ keepassxc/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+cmake -S . -B build \
+    -DKEEPASSXC_BUILD_TYPE=Release \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -Wno-dev \
+    -DBUILD_TESTING=OFF \
+    -DWITH_GUI_TESTS=OFF \
+    -DWITH_XC_UPDATECHECK=OFF \
+    ${CMAKE_ARGS}
+
+cmake --build build -j${CPU_COUNT}
+
+mkdir -p ${PREFIX}/bin
+install -m 755 build/src/cli/keepassxc-cli ${PREFIX}/bin
