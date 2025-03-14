7b1251231692de1c3cf7c3fdaaed012b77b89499
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-14 10:47:13 -0700

    add quantas_cu (#3306)
    * add quantas_cu
    
    * add quantas_cu
    
    * add quantas_cu
    
    * add quantas_cu

---
+++ quantas_cu/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+mkdir -p ${PREFIX}/bin
+sed -i 's?/usr/bin/perl -w?/usr/bin/env perl?' scripts/tsu_add_splice_site_usage_CU.pl
+chmod +x scripts/*.pl
+cp scripts/*.pl ${PREFIX}/bin
