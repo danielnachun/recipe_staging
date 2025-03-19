a28f387ee2a7fc57a0b6afb7ce79f4e229da85ad
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-19 16:52:49 -0700

    update gtex-pipeline-qtl

--- gtex-pipeline-qtl/build.sh
+++ gtex-pipeline-qtl/build.sh
@@ -3,6 +3,21 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 mkdir -p ${PREFIX}/bin
+
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/qtl/src/ase_aggregate_by_individual.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/qtl/src/ase_calculate_lamp.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/qtl/src/combine_covariates.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/qtl/src/combine_signif_pairs.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/qtl/src/convert_vep.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/qtl/src/eqtl_prepare_expression.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/qtl/src/metasoft_postprocess.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/qtl/src/metasoft_prepare_input.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/qtl/src/run_GATK_ASEReadCounter.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/qtl/src/run_PEER.R"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/qtl/src/run_metasoft.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/qtl/leafcutter/src/cluster_prepare_fastqtl.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/qtl/torus/src/run_torus.py"
+
 install -m 755 "${SRC_DIR}/qtl/src/ase_aggregate_by_individual.py" "${PREFIX}/bin/ase_aggregate_by_individual.py"
 install -m 755 "${SRC_DIR}/qtl/src/ase_calculate_lamp.py" "${PREFIX}/bin/ase_calculate_lamp.py"
 install -m 755 "${SRC_DIR}/qtl/src/combine_covariates.py" "${PREFIX}/bin/combine_covariates.py"
@@ -16,6 +31,6 @@ install -m 755 "${SRC_DIR}/qtl/src/run_PEER.R" "${PREFIX}/bin/run_PEER.R"
 install -m 755 "${SRC_DIR}/qtl/src/run_metasoft.py" "${PREFIX}/bin/run_metasoft.py"
 install -m 755 "${SRC_DIR}/qtl/leafcutter/src/cluster_prepare_fastqtl.py" "${PREFIX}/bin/cluster_prepare_fastqtl.py"
 install -m 755 "${SRC_DIR}/qtl/torus/src/run_torus.py" "${PREFIX}/bin/run_torus.py"
-sed -i '1i #!/usr/bin/env python3' "${PREFIX}/bin/ase_aggregate_by_individual.py"
-sed -i '1i #!/usr/bin/env python3' "${PREFIX}/bin/cluster_prepare_fastqtl.py"
-sed -i '1i #!/usr/bin/env python3' "${PREFIX}/bin/run_torus.py"
+sed -i "1i #!${PREFIX}/bin/python3" "${PREFIX}/bin/ase_aggregate_by_individual.py"
+sed -i "1i #!${PREFIX}/bin/python3" "${PREFIX}/bin/cluster_prepare_fastqtl.py"
+sed -i "1i #!${PREFIX}/bin/python3" "${PREFIX}/bin/run_torus.py"
