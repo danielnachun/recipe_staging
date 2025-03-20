93910b228d2444a7fb0a51777f38a3b8d45f222b
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-19 17:56:44 -0700

    update quantas

--- quantas/build.sh
+++ quantas/build.sh
@@ -3,5 +3,9 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 mkdir -p ${PREFIX}/bin
+
+sed -i "s?/usr/bin/env perl?${PREFIX}/bin/perl?" _countit/*.pl
+sed -i "s?/usr/bin/env perl?${PREFIX}/bin/perl?" _gapless/*.pl
+
 cp _countit/*.pl ${PREFIX}/bin
 cp _gapless/*.pl ${PREFIX}/bin
