f7e642558cb1ac01a49c371216bb2619eb7abc87
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-19 18:07:25 -0700

    update quantas_cu

--- quantas_cu/build.sh
+++ quantas_cu/build.sh
@@ -3,6 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 mkdir -p ${PREFIX}/bin
-sed -i 's?/usr/bin/perl -w?/usr/bin/env perl?' scripts/tsu_add_splice_site_usage_CU.pl
+sed -i "s?/usr/bin/perl -w?${PREFIX}/bin/perl?" scripts/tsu_add_splice_site_usage_CU.pl
+sed -i "s?/usr/bin/perl?${PREFIX}/bin/perl?" scripts/*.pl
 chmod +x scripts/*.pl
 cp scripts/*.pl ${PREFIX}/bin
