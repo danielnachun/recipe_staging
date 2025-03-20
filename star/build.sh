a9b9cb273ed1fc8313796d4a2f1b1407bb473ede
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-20 16:20:59 -0700

    update star (#3321)
    * update star
    
    * add star
    
    * add star

--- star/build.sh
+++ star/build.sh
@@ -1,32 +1,34 @@
-#!/bin/bash
-if [ "$(uname)" == "Darwin" ]; then
-    echo "Installing STAR for OSX."
-    mkdir -p $PREFIX/bin
-    cp bin/MacOSX_x86_64/* $PREFIX/bin
-else 
-    echo "Building STAR for Linux"
-    mkdir -p $PREFIX/bin
-    cd source
+#!/usr/bin/env bash
 
-    AMD64_SIMD_LEVELS=("avx2" "avx" "sse4.1" "ssse3" "sse3")
+set -o xtrace -o nounset -o pipefail -o errexit
 
-    if [ "$(arch)" == "x86_64" ]; then
-        for SIMD in ${AMD64_SIMD_LEVELS[@]}; do
-            make -j "$(nproc)" CXXFLAGSextra="-m$SIMD" STAR STARlong
-            cp STAR $PREFIX/bin/STAR-$SIMD
-            cp STARlong $PREFIX/bin/STARlong-$SIMD
-            make clean
-        done
-        make STAR STARlong
-        cp STAR $PREFIX/bin/STAR-plain
-        cp STARlong $PREFIX/bin/STARlong-plain
-        cp ../simd-dispatch.sh $PREFIX/bin/STAR
-        cp ../simd-dispatch.sh $PREFIX/bin/STARlong
-    else
-        make pj "$(nproc)" STAR STARlong
-        cp STAR $PREFIX/bin/STAR
-        cp STARlong $PREFIX/bin/STARlong
-    fi
+echo "Building STAR"
+mkdir -p $PREFIX/bin
+cd source
+
+AMD64_SIMD_LEVELS=("avx2" "avx" "sse4.1" "ssse3" "sse3")
+
+export CXXFLAGS="${CXXFLAGS} -Wno-register"
+if [[ ${target_platform} =~ .*osx.* ]]; then
+    export CXXFLAGS="${CXXFLAGS} -DCOMPILE_FOR_MAC"
+fi
+
+if [ "$(arch)" == "x86_64" ]; then
+    for SIMD in ${AMD64_SIMD_LEVELS[@]}; do
+        make -j${CPU_COUNT} CXXFLAGSextra="-m$SIMD" STAR STARlong CXX="${CXX} ${CXXFLAGS}"
+        cp STAR $PREFIX/bin/STAR-$SIMD
+        cp STARlong $PREFIX/bin/STARlong-$SIMD
+        make clean
+    done
+    make STAR STARlong
+    cp STAR $PREFIX/bin/STAR-plain
+    cp STARlong $PREFIX/bin/STARlong-plain
+    cp ${RECIPE_DIR}/simd-dispatch.sh $PREFIX/bin/STAR
+    cp ${RECIPE_DIR}/simd-dispatch.sh $PREFIX/bin/STARlong
+else
+    make -j${CPU_COUNT} STAR STARlong CXX="${CXX} ${CXXFLAGS}"
+    cp STAR $PREFIX/bin/STAR
+    cp STARlong $PREFIX/bin/STARlong
 fi
 
 chmod +x $PREFIX/bin/STAR
