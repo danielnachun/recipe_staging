2aa1d04a37263cfc6a688d8dbe7129f102e52cac
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-20 14:39:49 -0700

    add rsem

--- rsem/build.sh
+++ rsem/build.sh
@@ -27,26 +27,3 @@ make \
     BOOST=${PREFIX}/include \
     prefix="$PREFIX" \
     install
-
-# EBSeq R scripts and one binary
-sed -i.bak 's|#!/usr/bin/env Rscript|#!/opt/anaconda1anaconda2anaconda3/bin/Rscript|' EBSeq/rsem-*
-rm EBSeq/rsem-*.bak
-make -C EBSeq CXX="$CXX $CXXFLAGS $LDFLAGS" rsem-for-ebseq-calculate-clustering-info
-cp EBSeq/rsem-* $PREFIX/bin
-
-# Fix perl scripts and module
-# move all perl stuff into a separate dir
-mkdir -p perl-build/lib
-mv $PREFIX/bin/rsem*.pm perl-build/lib
-for n in $PREFIX/bin/rsem-*; do
-    if head -n1 "$n" | grep -q "env perl"; then
-	mv -v "$n" perl-build
-    fi
-done
-mv rsem-control-fdr rsem-run-ebseq perl-build
-cp ${RECIPE_DIR}/Build.PL perl-build
-cd perl-build
-# now run perl install
-perl ./Build.PL
-perl ./Build manifest
-perl ./Build install --installdirs site
