1004cf4d94dd73710a51c2a24ce7f83e689f0202
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-19 17:19:45 -0700

    update gtex-pipeline-gene_model

--- gtex-pipeline-gene_model/build.sh
+++ gtex-pipeline-gene_model/build.sh
@@ -3,4 +3,5 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 mkdir -p ${PREFIX}/bin
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/gene_model/collapse_annotation.py"
 install -m 755 "${SRC_DIR}/gene_model/collapse_annotation.py" "${PREFIX}/bin/collapse_annotation.py"
