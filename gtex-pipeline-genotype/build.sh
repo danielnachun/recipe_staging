f2018a0dd08af8b80d219c2c8d9b1bb83d81a7af
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-19 17:32:27 -0700

    update gtex-pipeline-genotype

--- gtex-pipeline-genotype/build.sh
+++ gtex-pipeline-genotype/build.sh
@@ -3,11 +3,11 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 mkdir -p ${PREFIX}/bin
-sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3" "${SRC_DIR}/genotype/compute_genotype_pcs.py"
-sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3" "${SRC_DIR}/genotype/shapeit2/src/aggregate_pirs.py"
-sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3" "${SRC_DIR}/genotype/shapeit2/src/bam_to_pir.py"
-sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3" "${SRC_DIR}/genotype/shapeit2/src/run_shapeit.py"
-sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3" "${SRC_DIR}/genotype/shapeit2/src/shapeit_postprocess.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/genotype/compute_genotype_pcs.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/genotype/shapeit2/src/aggregate_pirs.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/genotype/shapeit2/src/bam_to_pir.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/genotype/shapeit2/src/run_shapeit.py"
+sed -i "s?/usr/bin/env python3?${PREFIX}/bin/python3?" "${SRC_DIR}/genotype/shapeit2/src/shapeit_postprocess.py"
 
 install -m 755 "${SRC_DIR}/genotype/compute_genotype_pcs.py" "${PREFIX}/bin/compute_genotype_pcs.py"
 install -m 755 "${SRC_DIR}/genotype/shapeit2/src/aggregate_pirs.py" "${PREFIX}/bin/aggregate_pirs.py"
