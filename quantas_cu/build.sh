2d96d9fa13aa6de48e83b797f8fdf27382c9ba72
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-20 17:13:23 -0700

    update quantas_cu

--- quantas_cu/build.sh
+++ quantas_cu/build.sh
@@ -5,7 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 mkdir -p ${PREFIX}/bin
 sed -i "s?/usr/bin/perl -w?${PREFIX}/bin/perl?" scripts/tsu_add_splice_site_usage_CU.pl
 sed -i "s?/usr/bin/perl?${PREFIX}/bin/perl?" scripts/*.pl
-sed -i "s/tagoverlap.pl/tagoverlap_CU.pl/" scripts/summarise_splice_site_usage_CU.pl
-sed -i "s/tag2profile.pl/tag2profile_CU.pl/" scripts/summarise_splice_site_usage_CU.pl
+sed -i "s/tagoverlap.pl/tagoverlap_CU.pl/" scripts/summarize_splice_site_usage_CU.pl
+sed -i "s/tag2profile.pl/tag2profile_CU.pl/" scripts/summarize_splice_site_usage_CU.pl
 chmod +x scripts/*.pl
 cp scripts/*.pl ${PREFIX}/bin
