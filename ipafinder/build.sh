84be2fc26d70534a944f32a4dbf3ae972d408b74
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-22 21:53:08 -0700

    add ipafinder

--- ipafinder/build.sh
+++ ipafinder/build.sh
@@ -6,3 +6,6 @@ mkdir -p ${PREFIX}/bin
 
 install -m 755 IPAFinder_DetectIPA.py ${PREFIX}/bin/IPAFinder_DetectIPA
 install -m 755 IPAFinder_GetAnno.py ${PREFIX}/bin/IPAFinder_GetAnno
+
+sed -i '1i #!/usr/bin/env python3' ${PREFIX}/bin/IPAFinder_DetectIPA
+sed -i '1i #!/usr/bin/env python3' ${PREFIX}/bin/IPAFinder_GetAnno
