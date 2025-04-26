0c19047e9fa895bbf1efe4adf6e6947dd2f85537
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-25 18:27:12 -0700

    add zix

--- zix/build.sh
+++ zix/build.sh
@@ -2,6 +2,8 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+
 meson --prefix=${PREFIX} \
     --libdir=${PREFIX}/lib \
     --buildtype=release \
