161ef2c7a53ab1059a1dbd5ef5718037e7201b9b
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-25 23:18:16 -0700

    add roc-toolkit

--- roc-toolkit/build.sh
+++ roc-toolkit/build.sh
@@ -5,10 +5,14 @@ set -o xtrace -o nounset -o pipefail -o errexit
 scons --prefix=${PREFIX} \
     --libdir=${PREFIX}/lib \
     --enable-examples \
-    --enable-tests
+    --enable-tests \
+    CC=${CC} \
+    CXX=${CXX}
 
 scons --prefix=${PREFIX} \
     --libdir=${PREFIX}/lib \
     --enable-examples \
     --enable-tests \
     install
+    CC=${CC} \
+    CXX=${CXX}
