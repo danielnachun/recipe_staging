d46b9b57678ca9bfabea1c64214fe4570c66de14
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-25 18:03:22 -0700

    add liblc3

--- liblc3/build.sh
+++ liblc3/build.sh
@@ -2,6 +2,8 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CFLAGS="${CFLAGS} -Wno-implicit-function-declaration"
+
 meson --prefix=${PREFIX} \
     --libdir=${PREFIX}/lib \
     --buildtype=release \
