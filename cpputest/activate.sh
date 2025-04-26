27df240041f94e1dc8dcfe53c89b74bdfe506b1f
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-25 20:41:42 -0700

    add cpputest (#3392)

---
+++ cpputest/activate.sh
@@ -0,0 +1,3 @@
+# CppUTest will look for this variable to find the library
+# https://cpputest.github.io/manual.html
+export CPPUTEST_HOME="$CONDA_PREFIX"
---
+++ cpputest/build.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
+
+cmake -S . -B build \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -Wno-dev \
+    -DBUILD_TESTING=OFF \
+    ${CMAKE_ARGS}
+
+cmake --build build -j${CPU_COUNT}
+cmake --install build
