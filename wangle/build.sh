aad60ffe0e017027b398b82866370285056cbb35
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-26 23:17:16 -0700

    add wangle

---
+++ wangle/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CXXFLAGS="${CXXFLAGS} -DGLOG_USE_GLOG_EXPORT"
+
+cmake -S wangle -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -Wno-dev \
+    -DBUILD_TESTING=OFF \
+    -DBUILD_SHARED_LIBS=ON \
+    ${CMAKE_ARGS}
+
+cmake --build build -j${CPU_COUNT}
+cmake --install build
