93ccffb1a33fda378e9794bc2bb3e50d8b698eba
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-27 01:23:19 -0700

    add fbthrift

---
+++ fbthrift/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CXXFLAGS="${CXXFLAGS} -DGLOG_USE_GLOG_EXPORT -D_LIBCPP_HAS_NO_ASAN"
+export LDFLAGS="${LDFLAGS} -Wl,-undefined,dynamic_lookup -Wl,-dead_strip_dylibs"
+
+sed -i 's/compiler_base/shared/g' thrift/shared/CMakeLists.txt
+sed -i 's/TARGETS thrift1/TARGETS thrift1\nshared/' thrift/compiler/CMakeLists.txt
+sed -i 's?whisker/dsl.cc?whisker/dsl.cc\nwhisker/detail/string.cc?' thrift/compiler/CMakeLists.txt
+
+cmake -S . -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -Wno-dev \
+    -Denable_tests=OFF \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_CXX_STANDARD=20 \
+    -Dthriftpy3=OFF \
+    -Dthriftpy=OFF \
+    ${CMAKE_ARGS}
+
+cmake --build build -j${CPU_COUNT}
+cmake --install build
