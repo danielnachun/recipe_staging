483decd7f816ad76ee1eefc64a2aed3a2e77d2ac
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-27 02:12:09 -0700

    add edencommon

---
+++ edencommon/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CXXFLAGS="${CXXFLAGS} -DGLOG_USE_GLOG_EXPORT"
+export LDFLAGS="${LDFLAGS} -Wl,-undefined,dynamic_lookup -Wl,-dead_strip_dylibs"
+
+sed -i 's/COMPONENTS cpp2 py/COMPONENTS cpp2/' CMakeLists.txt
+
+cmake -S . -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -Wno-dev \
+    -DBUILD_SHARED_LIBS=ON \
+    ${CMAKE_ARGS}
+
+cmake --build build -j${CPU_COUNT}
+cmake --install build
