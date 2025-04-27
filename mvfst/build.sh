774163fb9fe82974abcf20b3d8dec148ad29bedf
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-27 12:18:48 -0700

    add mvfst (#3408)
    * add mvfst
    
    * add mvfst
    
    * add mvfst
    
    * add mvfst
    
    * add mvfst
    
    * add mvfst
    
    * add mvfst
    
    * add mvfst

---
+++ mvfst/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CXXFLAGS="${CXXFLAGS} -DGLOG_USE_GLOG_EXPORT -DANDROID"
+
+if [[ ${target_platform} =~ .*osx.* ]]; then
+    export LDFLAGS="${LDFLAGS} -Wl,-undefined,dynamic_lookup -Wl,-dead_strip_dylibs"
+fi
+
+cmake -S . -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -Wno-dev \
+    -DBUILD_SHARED_LIBS=ON \
+    ${CMAKE_ARGS}
+
+cmake --build build -j${CPU_COUNT}
+cmake --install build
