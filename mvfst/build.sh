b0e8da7155d859cf11a1aa4db4f9810fd32cc554
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-27 12:03:53 -0700

    add mvfst

--- mvfst/build.sh
+++ mvfst/build.sh
@@ -8,7 +8,9 @@ if [[ ${target_platform} =~ .*osx.* ]]; then
     export LDFLAGS="${LDFLAGS} -Wl,-undefined,dynamic_lookup -Wl,-dead_strip_dylibs"
 fi
 
-sed -i '/#include <linux\/ipv6.h>/d' quic/xsk/XskSender.h
+sed -i '/add_subdirectory(xsk)/d' quic/CMakeLists.txt
+sed -i '/mvfst_xsk/d' quic/dsr/CMakeLists.txt
+sed -i 's/defined(__linux__) && !defined(ANDROID)/defined(ANDRFOID)/' quic/dsr/backend/DSRPacketizer.h
 
 cmake -S . -B build \
     -DCMAKE_BUILD_TYPE=Release \
