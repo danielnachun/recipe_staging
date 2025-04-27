0b82fc9cdc39265c0dbe00ac8d3fa0c330765b84
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-27 12:11:27 -0700

    add mvfst

--- mvfst/build.sh
+++ mvfst/build.sh
@@ -2,15 +2,11 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-export CXXFLAGS="${CXXFLAGS} -DGLOG_USE_GLOG_EXPORT"
+export CXXFLAGS="${CXXFLAGS} -DGLOG_USE_GLOG_EXPORT -DANDROID"
 
 if [[ ${target_platform} =~ .*osx.* ]]; then
     export LDFLAGS="${LDFLAGS} -Wl,-undefined,dynamic_lookup -Wl,-dead_strip_dylibs"
 fi
-
-sed -i '/add_subdirectory(xsk)/d' quic/CMakeLists.txt
-sed -i '/mvfst_xsk/d' quic/dsr/CMakeLists.txt
-sed -i 's/defined(__linux__) && !defined(ANDROID)/defined(ANDRFOID)/' quic/dsr/backend/DSRPacketizer.h
 
 cmake -S . -B build \
     -DCMAKE_BUILD_TYPE=Release \
