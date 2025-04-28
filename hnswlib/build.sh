def63e54c78581a2f28a695add3bf9fa3c391f19
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-28 16:03:39 -0700

    add hnswlib

--- hnswlib/build.sh
+++ hnswlib/build.sh
@@ -2,12 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cmake -S . -B build \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_VERBOSE_MAKEFILE=ON \
-    -Wno-dev \
-    -DHNSWLIB_EXAMPLES=OFF \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}
-
-cmake --build build -j${CPU_COUNT}
-cmake --install build
+mkdir -p ${PREFIX}/include
+cp -r hnswlib ${PREFIX}/include
