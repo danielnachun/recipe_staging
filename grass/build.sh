7e8ea24ffb1131f33d1a3fbe164f47cf1f89f9fe
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-28 13:53:58 -0700

    add grass

--- grass/build.sh
+++ grass/build.sh
@@ -2,6 +2,8 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CFLAGS="${CFLAGS} -Wno-implicit-function-declaration -Wno-int-conversion"
+
 autoreconf --force --install --verbose
 ./configure --disable-silent-rules \
     --disable-dependency-tracking \
