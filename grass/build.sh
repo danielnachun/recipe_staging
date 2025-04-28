04bd3adb7a81a044b486a2fc2457988ff1c7781a
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-28 12:37:34 -0700

    add grass

---
+++ grass/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+autoreconf --force --install --verbose
+./configure --disable-silent-rules \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} \
+    --with-freetype-includes=${PREFIX}/include/freetype2 \
+    --without-widgets \
+    --without-opengl \
+    --with-readline \
+    --with-pthread \
+    --with-netcdf \
+    --with-nls \
+    --with-geos \
+    --with-postgres \
+    --with-pdal=${PREFIX}/include/pdal \
+    --with-bzlib \
+    --with-zstd
+make
+make install
