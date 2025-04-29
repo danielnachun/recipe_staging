78f9e2b51a0539609ceaa9b9baff641221f4e9ef
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-29 14:20:58 -0700

    add covfie (#3431)

---
+++ covfie/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+cmake -S . -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -Wno-dev \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}
+
+cmake --build build -j${CPU_COUNT}
+cmake --install build
