d74f50f8a7c572ffb8804c2ecc71e43bc9124821
Daniel Nachun <daniel.nachun@gmail.com>
2025-05-01 00:28:09 -0700

    fix LDFLAGS

--- grass/build.sh
+++ grass/build.sh
@@ -3,6 +3,9 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 export CFLAGS="${CFLAGS} -Wno-implicit-function-declaration -Wno-int-conversion"
+if [[ ${target_platform} =~ .*linux.* ]]; then
+    export LDFLAGS="${LDFLAGS} -liconv"
+fi
 
 autoreconf --force --install --verbose
 ./configure --disable-silent-rules \
