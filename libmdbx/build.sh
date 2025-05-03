6fa5b030672ecd6bf833579b1f0c40395aa44fd0
Daniel Nachun <daniel.nachun@gmail.com>
2025-05-03 12:35:55 -0700

    add libmdbx

---
+++ libmdbx/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+sed -i 's/0x04000000/0x3100000/' src/api-env.c
+
+cmake -S . -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -DCMAKE_OSX_ARCHITECTURES=arm64 \
+    -Wno-dev \
+    ${CMAKE_ARGS}
+
+cmake --build build -j${CPU_COUNT}
+cmake --install build
