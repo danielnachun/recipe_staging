11e89c1ed28d1a505b86ddc6650c43ac131efdda
Daniel Nachun <daniel.nachun@gmail.com>
2025-05-03 11:59:30 -0700

    add tarantool

--- tarantool/build.sh
+++ tarantool/build.sh
@@ -2,15 +2,18 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+# export CFLAGS="${CFLAGS} -Wno-implicit-function-declaration -Wno-int-conversion"
+
 cmake -S . -B build \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_VERBOSE_MAKEFILE=ON \
     -Wno-dev \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
     -DENABLE_BUNDLED_ICONV=OFF \
     -DENABLE_BUNDLED_ICU=OFF \
     -DENABLE_BUNDLED_LIBCURL=OFF \
     -DENABLE_BUNDLED_LIBYAML=OFF \
-    -DENABLE_BUNDLED_MSGPUCK=OFF \
     -DENABLE_BUNDLED_OPENSSL=OFF \
     -DENABLE_BUNDLED_READLINE=OFF \
     -DENABLE_BUNDLED_ZLIB=OFF \
