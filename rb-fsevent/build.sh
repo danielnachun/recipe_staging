8513fe8feba5385400fabd8558f387fbcf553d28
Daniel Nachun <daniel.nachun@gmail.com>
2025-05-17 11:08:08 -0700

    rb-fsevent: rebuild for ruby 3.4 (#3583)

--- rb-fsevent/build.sh
+++ rb-fsevent/build.sh
@@ -2,13 +2,19 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-gem install -N -l -V --ignore-dependencies ${PKG_NAME}-${PKG_VERSION}.gem
-gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
+export ARCHFLAGS="-arch x86_64"
+if [[ ${target_platform} == "osx-arm64" ]]; then
+    export ARCHFLAGS="-arch arm64"
+fi
+
+gem install -N -l -V --ignore-dependencies rb-fsevent-${PKG_VERSION}.gem
+gem unpack rb-fsevent-${PKG_VERSION}.gem
 if [[ ${target_platform} =~ .*osx.* ]]; then
-    cd ${PREFIX}/share/rubygems/gems/${PKG_NAME}-${PKG_VERSION}/ext
+    cd ${PREFIX}/share/rubygems/gems/rb-fsevent-${PKG_VERSION}/ext
     sed -i "s?\$SDK_INFO\['Path'\]?\"${CONDA_BUILD_SYSROOT}\"?" rakefile.rb
     sed -i '/$CFLAGS = "#{$CFLAGS} -mmacosx-version-min=#{$MACOSX_DEPLOYMENT_TARGET}"/d' rakefile.rb
     rake replace_exe
+    rm -rf ${PREFIX}/share/rubygems/gems/rb-fsevent-${PKG_VERSION}/ext/build
 else
-    rm -rf ${PREFIX}/share/rubygems/gems/${PKG_NAME}-${PKG_VERSION}/ext
+    rm -rf ${PREFIX}/share/rubygems/gems/rb-fsevent-${PKG_VERSION}/ext
 fi
