241149639b1c9614db1fe060904df9c3d5079041
Daniel Nachun <daniel.nachun@gmail.com>
2025-06-20 19:17:17 -0700

    try to fix build

--- gmato/build.sh
+++ gmato/build.sh
@@ -2,7 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-sed -i "s?/usr/bin/perl -w?/usr/bin/env perl?" *.pl
+sed -i "s?/usr/bin/perl -w\r?/usr/bin/env perl?" *.pl
 sed -i 's/system ("perl /system ("/' *.pl
 
 mkdir -p ${PREFIX}/bin
