e1970ed3759ed1e0ba3e2f5a92aa3d9e56a29698
Daniel Nachun <daniel.nachun@gmail.com>
2025-07-08 19:21:55 -0700

    add pacvim

--- pacvim/build.sh
+++ pacvim/build.sh
@@ -2,9 +2,10 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-sed -i 's/cursesw.h/ncurses.h/' src/globals.h
-sed -i 's/cursesw.h/ncurses.h/' src/helperFns.h
+# sed -i 's/cursesw.h/ncurses.h/' src/globals.h
+# sed -i 's/cursesw.h/ncurses.h/' src/helperFns.h
 sed -i 's/CFLAGS =/CFLAGS +=/' Makefile
+export CFLAGS=${CXXFLAGS}
 
 make install PREFIX=${PREFIX} \
     CXX="${CXX}" \
