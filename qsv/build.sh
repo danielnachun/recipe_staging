be89559ca70c9e0597edb7a7d6339f125f5f6ce1
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-04 21:59:58 -0800

    qsv 10.0.0

--- qsv/build.sh
+++ qsv/build.sh
@@ -15,4 +15,5 @@ if [[ ${target_platform} =~ .*osx.* ]]; then
     export CFLAGS="${CFLAGS} -fno-define-target-os-macros"
 fi
 
+export PYO3_PYTHON=${PYTHON}
 cargo install --features all_features --bins --no-track --locked --root ${PREFIX} --path .
