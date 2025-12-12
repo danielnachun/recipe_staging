3fd6408949848caec242167865dd22bc7cbf619f
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-12 15:37:02 -0800

    add scripts to dxda

--- dxda/build.sh
+++ dxda/build.sh
@@ -4,3 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 go build -o=${PREFIX}/bin/dx-download-agent -ldflags="-s -w" ./cmd/dx-download-agent
 go-licenses save ./cmd/dx-download-agent --save_path=license-files
+
+chmod +x scripts/*
+sed -i '1i #!/usr/bin/env python3' scripts/*
+cp scripts/* ${PREFIX}/bin
