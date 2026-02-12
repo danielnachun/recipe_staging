908f4207f4f60aac776053085c0c7ccf56557b45
Daniel Nachun <daniel.nachun@gmail.com>
2026-02-12 15:45:04 -0800

    add fusera

---
+++ fusera/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+go mod init fusera
+go mod tidy -e
+rm -rf vendor
+go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
+go-licenses save . --save_path=license-files
