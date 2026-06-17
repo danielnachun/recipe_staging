0ed731e382361b42fc55dab872ae9f4087de2fe1
Daniel Nachun <daniel.nachun@gmail.com>
2026-06-17 14:11:23 -0700

    add mca_wgs (#3777)
    * add mca_wgs
    
    * add mca_wgs

---
+++ mca_wgs/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CXXFLAGS="${CXXFLAGS} -O3 -fopenmp -Wall -Wextra -Wno-sign-compare -Wno-unused-local-typedefs -Wno-deprecated -Wno-unused-parameter"
+export LDFLAGS="${LDFLAGS} -O3 -fopenmp"
+
+rm -rf obj/*.o
+make CXX=${CXX} all \
+    CXXFLAGS="${CXXFLAGS}" \
+    LDFLAGS="${LDFLAGS}" \
+    LDLIBS="-lhts -ldeflate -lboost_iostreams -lboost_program_options -lpthread -lz" \
+    BFILE=${PREFIX}/bin/mCAs_WGS \
+    BOOST_LIB=${PREFIX}/lib \
+    BOOST_INC=${PREFIX}/include \
+    HTSLD_LIB=${PREFIX}/lib \
+    HTSLD_INC=${PREFIX}/include \
+    LIBDEFLATE=${PREFIX}/lib \
+    EIGEN_INC=${PREFIX}/include/eigen
+
+install -m 755 bin/generateRefMasks ${PREFIX}/bin
+install -m 755 bin/countReadsSaveDiscordant ${PREFIX}/bin
+install -m 755 bin/computeDepthProfiles ${PREFIX}/bin
+install -m 755 bin/computeRegionDepthsPCadj ${PREFIX}/bin
