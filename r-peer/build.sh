8166aa5304a644e8d8d63a9ac89d4705dfd943b0
Daniel Nachun <daniel.nachun@gmail.com>
2026-07-24 00:15:11 -0700

    update r-peer

--- r-peer/build.sh
+++ r-peer/build.sh
@@ -2,5 +2,9 @@
 
 export DISABLE_AUTOBREW=1
 
+rm -rf src/Eigen src/libpeer.so
+echo 'LinkingTo: RcppEigen' >> DESCRIPTION
+echo 'CXX_STD = CXX14' >> src/Makevars
+
 # shellcheck disable=SC2086
-${R} CMD INSTALL --build . ${R_ARGS}
+R CMD INSTALL --build . ${R_ARGS}
