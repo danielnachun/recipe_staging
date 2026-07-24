89e2c951020a3da49155c8298979cdf0b449f4d8
Daniel Nachun <daniel.nachun@gmail.com>
2026-07-23 21:24:49 -0700

    patch for modern build

--- gcta/build.sh
+++ gcta/build.sh
@@ -8,21 +8,25 @@ export BOOST_LIB="${PREFIX}"
 export LDFLAGS="${LDFLAGS} -lgsl"
 export LDFLAGS=$(echo $LDFLAGS | sed 's/-Wl,-dead_strip_dylibs//g')
 
+sed -i 's/SET(CMAKE_CXX_STANDARD 11)/SET(CMAKE_CXX_STANDARD 14)/' CMakeLists.txt
+sed -i 's#INCLUDE_DIRECTORIES("submods/plink-ng/2.0")#&\nINCLUDE_DIRECTORIES("submods/plink-ng/2.0/include")#' CMakeLists.txt
 sed -i 's/CMAKE_EXE_LINKER_FLAGS "/CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /g' CMakeLists.txt
 sed -i 's/zstd_s/zstd/' CMakeLists.txt
 sed -i '/link_directories(\/usr\/local\/lib)/d' CMakeLists.txt
+# Replace the bundled (too-old) plink-ng with the newer plink2 on all platforms
+rm -rf submods/plink-ng/2.0
+mv plink2 submods/plink-ng/2.0
+
 if [[ ${target_platform} == "osx-arm64" ]]; then
     sed -i 's/"aarch64"/"arm64"/' CMakelists.txt
     export OPENBLAS="${PREFIX}"
     export CXXFLAGS="${CXXFLAGS} -DIGNORE_BUNDLED_SIMDE"
     export CFLAGS="${CFLAGS} -DIGNORE_BUNDLED_SIMDE"
-    rm -rf submods/plink-ng/2.0
-    mv plink2 submods/plink-ng/2.0
 else
     export MKLROOT="${PREFIX}"
 fi
 
-cmake -S . -B build ${CMAKE_ARGS}
+cmake -S . -B build ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 cmake --build build
 cmake --install build
 
