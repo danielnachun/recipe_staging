87ef68c600ee8ba8abbc406d41d05c69847a63fc
Daniel Nachun <daniel.nachun@gmail.com>
2026-07-24 00:58:20 -0700

    update apex (#3792)

--- apex/build.sh
+++ apex/build.sh
@@ -2,11 +2,15 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+sed -i 's/Eigen::all/Eigen::placeholders::all/' src/fitModels.hpp
+sed -i -E 's/Spectra::SymEigsSolver< double, Spectra::LARGEST_ALGE, (Spectra::DenseSymMatProd<double>) >([^(]*)\(&op,/Spectra::SymEigsSolver< \1 >\2(op,/' src/fitModels.cpp
+sed -i 's/eigs\.compute();/eigs.compute(Spectra::SortRule::LargestAlge);/' src/fitModels.cpp
+
 # Symlink args.hxx from taywee-args into source tree
 mkdir "${SRC_DIR}/src/args"
-ln -sf "${BUILD_PREFIX}/include/args.hxx" "${SRC_DIR}/src/args"
+ln -sf "${PREFIX}/include/args.hxx" "${SRC_DIR}/src/args"
 
-export CXXFLAGS="${CXXFLAGS} -I${BUILD_PREFIX}/include -I${BUILD_PREFIX}/include/eigen3" 
+export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include/eigen3" 
 make CXX="${CXX}"
 
 mkdir -p ${PREFIX}/bin
