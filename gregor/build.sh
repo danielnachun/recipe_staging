54a2f6a5b0fe26d8fdc2f53ac8ed9d9336caf956
Daniel Nachun <daniel.nachun@gmail.com>
2026-07-28 21:55:37 -0700

    gregor update

--- gregor/build.sh
+++ gregor/build.sh
@@ -2,7 +2,10 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cp -R lib/* ${PREFIX}/lib/perl5/site_perl
-cp -R script/* ${PREFIX}/bin
+tar xzf GREGOR.v${PKG_VERSION}.tar.gz
+cp -R GREGOR/lib/* ${PREFIX}/lib/perl5/site_perl
+cp -R GREGOR/script/* ${PREFIX}/bin
 cd ${PREFIX}/bin
 ls | xargs -I % sed -i "s?/usr/bin/perl -w?/usr/bin/env perl?" %
+
+sed -i 's#"\$Bin/\.\./script/"#"$Bin/"#' $PREFIX/lib/perl5/site_perl/GREGOR.pm
