8d54fbd09e369090dacb5aab8d1fc580d0181737
Daniel Nachun <daniel.nachun@gmail.com>
2026-07-31 12:18:36 -0700

    fix build

--- plinkseq/build.sh
+++ plinkseq/build.sh
@@ -29,7 +29,7 @@ ln -sf ${PREFIX}/include/sqlite3.h sources/plinkseq/sources/include/plinkseq
 #sed -i 's/$(PROTOBUF_LIB_FULL_PATH)//g' Makefile
 #sed -i 's/-lz/-lprotobuf -labsl_log_internal_message -labsl_log_internal_check_op -lsqlite3 -lz /g' Makefile
 export CXXFLAGS=$(echo ${CXXFLAGS} | sed 's/-O2/-O0/g')
-make CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -Wno-register" STATIC_FLAG="${LDFLAGS}"
+make CXX="${CXX}" AR="${AR}" RANLIB="${RANLIB}" CXXFLAGS="${CXXFLAGS} -Wno-register -fno-strict-return" STATIC_FLAG="${LDFLAGS}"
 
 mkdir -p ${PREFIX}/bin
 install -m 755 build/execs/behead ${PREFIX}/bin
