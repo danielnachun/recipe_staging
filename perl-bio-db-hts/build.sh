ba9372ffcca87b230262d8da0b1a58a386d7184e
Daniel Nachun <daniel.nachun@gmail.com>
2026-08-02 20:17:34 -0700

    update perl-bio-db-hts

--- perl-bio-db-hts/build.sh
+++ perl-bio-db-hts/build.sh
@@ -3,6 +3,6 @@ export HTSLIB_DIR=$PREFIX
 export CC=${BUILD_PREFIX}/bin/${CC}
 export LD=$CC
 perl Build.PL --extra_compiler_flags "-I$PREFIX/include"
-perl ./Build CC=${CC}
+perl ./Build
 # Make sure this goes in site
 perl ./Build install --installdirs site
